31b76d1b8e57827b919c5fef90394e429c0dfd45
Andy Nonaka <AJNonaka@lbl.gov>
2018-04-03 09:30:28 -0700

    setup directory

--- src_common/main.cpp
+++ src_common/main.cpp
@@ -1,45 +1,19 @@
 
-#include <FHD.H>
+#include "common.H"
 
 using namespace amrex;
 
-int main(int argc, char* argv[])
+int main (int argc, char* argv[])
 {
+    amrex::Initialize(argc,argv);
+    
+    main_driver();
 
-    // in AMReX.cpp
-    Initialize(argc,argv);
+    amrex::Finalize();
 
-    // timer for profiling
-    BL_PROFILE_VAR("main()", main);
+    return 0;
+}
 
-    // wallclock time
-    const Real strt_total = ParallelDescriptor::second();
-
-    {
-        // declare an FHD object to manage data
-        FHD fhd;
-
-        // read in C++/F90 parameters
-        // define global C++/F90 variables
-        // set up boundary conditions
-        // set istep, t_new, t_old
-        // allocate MultiFabs and initialize MultiFab data
-        fhd.Init();
-
-        // advance solution to final time
-        fhd.Evolve();
-	
-        // wallclock time
-        Real end_total = ParallelDescriptor::second() - strt_total;
-	
-        // print wallclock time
-        ParallelDescriptor::ReduceRealMax(end_total ,ParallelDescriptor::IOProcessorNumber());
-        Print() << "\nTotal Time: " << end_total << '\n';
-    }
-
-    // destroy timer for profiling
-    BL_PROFILE_VAR_STOP(main);
-
-    // in AMReX.cpp
-    Finalize();
+void main_main ()
+{
 }
---
+++ src_common/main_driver.cpp
@@ -0,0 +1,7 @@
+
+#include "common.H"
+
+void main_driver()
+{
+    Abort("main_driver: write a custom routine and put in local source directory");
+}
