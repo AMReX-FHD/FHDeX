0f36297decd31794fd4abf7ec19607e4b9decbac
Andy Nonaka <AJNonaka@lbl.gov>
2018-04-03 17:36:43 -0700

    use the parameters in the namespace when appropriate

--- src_hydro/main_driver.cpp
+++ src_hydro/main_driver.cpp
@@ -9,12 +9,8 @@ using namespace parameters;
 void main_driver()
 {
 
-    // What time is it now?  We'll use this to compute total run time.
+    // store the current time so we can later compute total run time.
     Real strt_time = ParallelDescriptor::second();
-
-    // AMREX_SPACEDIM: number of dimensions
-    int n_cell, max_grid_size, nsteps;
-    Vector<int> is_periodic(AMREX_SPACEDIM,1);  // periodic in all direction by default
 
     // inputs parameters
     {
@@ -32,46 +28,58 @@ void main_driver()
 
     }
 
+    // is the problem periodic?
+    Vector<int> is_periodic(AMREX_SPACEDIM,0);  // set to 0 (not periodic) by default
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        if (bc_lo[i] == 0 && bc_hi[i] == 0) {
+            is_periodic[i] = 1;
+        }
+    }
+
     // make BoxArray and Geometry
     BoxArray ba;
     Geometry geom;
     {
-        IntVect dom_lo(AMREX_D_DECL(       0,        0,        0));
-        IntVect dom_hi(AMREX_D_DECL(n_cell-1, n_cell-1, n_cell-1));
+        IntVect dom_lo(AMREX_D_DECL(           0,            0,            0));
+        IntVect dom_hi(AMREX_D_DECL(n_cells[0]-1, n_cells[1]-1, n_cells[2]-1));
         Box domain(dom_lo, dom_hi);
 
         // Initialize the boxarray "ba" from the single box "bx"
         ba.define(domain);
+
         // Break up boxarray "ba" into chunks no larger than "max_grid_size" along a direction
-        ba.maxSize(max_grid_size);
+        // note we are converting "Vector<int> max_grid_size" to an IntVect
+        ba.maxSize(IntVect(max_grid_size));
 
        // This defines the physical box, [-1,1] in each direction.
-        RealBox real_box({AMREX_D_DECL(-1.0,-1.0,-1.0)},
-                         {AMREX_D_DECL( 1.0, 1.0, 1.0)});
+        RealBox real_box({AMREX_D_DECL(prob_lo[0],prob_lo[1],prob_lo[2])},
+                         {AMREX_D_DECL(prob_hi[0],prob_hi[1],prob_hi[2])});
 
         // This defines a Geometry object
         geom.define(domain,&real_box,CoordSys::cartesian,is_periodic.data());
     }
-
-    // Nghost = number of ghost cells for each array 
-    int Nghost = 1;
-    
-    // Ncomp = number of components for each array
-    int Ncomp  = 1;
   
     // How Boxes are distrubuted among MPI processes
     DistributionMapping dm(ba);
 
     // we allocate two phi multifabs; one will store the old state, the other the new.
-    MultiFab phi_old(ba, dm, Ncomp, Nghost);
-    MultiFab phi_new(ba, dm, Ncomp, Nghost);
+    MultiFab phi_old(ba, dm, 1, 1);
+    MultiFab phi_new(ba, dm, 1, 1);
 
     phi_old.setVal(0.);
     phi_new.setVal(0.);
 
+    // build the flux multifabs
+    std::array<MultiFab, AMREX_SPACEDIM> flux;
+    for (int dir=0; dir<AMREX_SPACEDIM; ++dir) {
+        // flux(dir) has one component, zero ghost cells, and is nodal in direction dir
+        BoxArray edge_ba = ba;
+        edge_ba.surroundingNodes(dir);
+        flux[dir].define(edge_ba, dm, 1, 0);
+    }
+
     // compute the time step
-    const Real* dx = geom.CellSize();
-    Real dt = 0.9*dx[0]*dx[0] / (2.0*AMREX_SPACEDIM);
+    Real dt = fixed_dt;
 
     // time = starting time in the simulation
     Real time = 0.0;
@@ -82,17 +90,7 @@ void main_driver()
 
     }
 
-    // build the flux multifabs
-    std::array<MultiFab, AMREX_SPACEDIM> flux;
-    for (int dir = 0; dir < AMREX_SPACEDIM; dir++)
-    {
-        // flux(dir) has one component, zero ghost cells, and is nodal in direction dir
-        BoxArray edge_ba = ba;
-        edge_ba.surroundingNodes(dir);
-        flux[dir].define(edge_ba, dm, 1, 0);
-    }
-
-    for (int n = 1; n <= nsteps; ++n)
+    for (int n = 1; n <= max_step; ++n)
     {
         MultiFab::Copy(phi_old, phi_new, 0, 0, 1, 0);
 
@@ -113,12 +111,9 @@ void main_driver()
         }
     }
 
-    // Call the timer again and compute the maximum difference between the start time and stop time
-    //   over all processors
+    // Call the timer again and compute the maximum difference between the start time 
+    // and stop time over all processors
     Real stop_time = ParallelDescriptor::second() - strt_time;
-    const int IOProc = ParallelDescriptor::IOProcessorNumber();
-    ParallelDescriptor::ReduceRealMax(stop_time,IOProc);
-
-    // Tell the I/O Processor to write out the "run time"
+    ParallelDescriptor::ReduceRealMax(stop_time);
     amrex::Print() << "Run time = " << stop_time << std::endl;
 }
