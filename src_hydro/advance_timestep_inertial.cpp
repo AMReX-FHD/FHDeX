274563f6539c9b5b6adc648347e92e3069d2539e
Andy Nonaka <AJNonaka@lbl.gov>
2018-04-27 15:22:28 -0700

    new file

---
+++ src_hydro/advance_timestep_inertial.cpp
@@ -0,0 +1,14 @@
+#include "common_functions.H"
+#include "common_functions_F.H"
+
+#include "hydro_functions.H"
+
+#include "common_params.H"
+
+using namespace common;
+
+void advance_timestep_inertial() {
+
+    Print() << "HERE advance_timestep_inertial()" << std::endl;
+
+}
