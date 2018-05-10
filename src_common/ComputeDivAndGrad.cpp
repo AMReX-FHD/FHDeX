be4d7eda34429245f7d6e5efa6a6b1c434ff3c0a
Daniel Ladiges <drladiges@lbl.gov>
2018-05-09 17:28:41 -0700

    git added missing files

---
+++ src_common/ComputeDivAndGrad.cpp
@@ -0,0 +1,44 @@
+#include "INS_functions.H"
+#include "common_functions.H"
+#include "common_functions_F.H"
+
+
+//Computes divergence at cell centres from velcocities at cell faces
+void ComputeDiv(MultiFab& div, std::array<MultiFab, AMREX_SPACEDIM>& umac, const Geometry geom, int increment)
+{
+    int inc = increment; 
+
+    for ( MFIter mfi(div); mfi.isValid(); ++mfi )
+    {
+        const Box& bx = mfi.validbox();
+
+		compute_div(BL_TO_FORTRAN_BOX(bx),
+					BL_TO_FORTRAN_ANYD(umac[0][mfi]), 
+                    BL_TO_FORTRAN_ANYD(umac[1][mfi]),
+#if AMREX_SPACEDIM == 3
+                    BL_TO_FORTRAN_ANYD(umac[2][mfi]),
+#endif
+                    BL_TO_FORTRAN_ANYD(div[mfi]),					
+                    geom.CellSize(), &inc);
+    }
+}
+
+//Computes gradient at cell faces of cell centred scalar
+void ComputeGrad(MultiFab& phi, std::array<MultiFab, AMREX_SPACEDIM>& umac, const Geometry geom)
+{
+    for ( MFIter mfi(phi); mfi.isValid(); ++mfi )
+    {
+        const Box& bx = mfi.validbox();
+
+		compute_grad(BL_TO_FORTRAN_BOX(bx),
+					BL_TO_FORTRAN_ANYD(umac[0][mfi]), 
+                    BL_TO_FORTRAN_ANYD(umac[1][mfi]),
+#if AMREX_SPACEDIM == 3
+                    BL_TO_FORTRAN_ANYD(umac[2][mfi]),
+#endif
+                    BL_TO_FORTRAN_ANYD(phi[mfi]),					
+                    geom.CellSize());
+
+    }
+}
+
