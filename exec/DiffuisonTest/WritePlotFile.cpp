c8a3c371aa39a22cbaa4426d04782b254ecb4e9c
Andy Nonaka <AJNonaka@lbl.gov>
2018-06-18 11:22:11 -0700

    clean up diffusion test driver

--- exec/DiffuisonTest/WritePlotFile.cpp
+++ exec/DiffuisonTest/WritePlotFile.cpp
@@ -1,24 +1,25 @@
 #include "DiffusionTest_functions.H"
-#include "common_functions.H"
 
 #include "AMReX_PlotFileUtil.H"
+
+#include "common_functions.H"
+
+#include "common_namespace.H"
+
+using namespace common;
 
 void WritePlotFile(int step,
                    const amrex::Real time,
                    const amrex::Geometry geom,
-                   const MultiFab& rhotot,
-                   const std::array< MultiFab, AMREX_SPACEDIM >& umac,
-	           const MultiFab& div)
+                   const std::array< MultiFab, AMREX_SPACEDIM >& umac)
 {
+    const std::string plotfilename = Concatenate("plt",step,7);
 
-    std::string plotfilename = Concatenate("plt",step,7);
+    BoxArray ba = umac[0].boxArray();
+    DistributionMapping dmap = umac[0].DistributionMap();
 
-    BoxArray ba = rhotot.boxArray();
-    DistributionMapping dmap = rhotot.DistributionMap();
-
-    // rhotot plus velocity
-    //int nPlot = 1+AMREX_SPACEDIM;
-	int nPlot = 2+AMREX_SPACEDIM;
+    // plot all the velocity variables
+    int nPlot = AMREX_SPACEDIM;
 
     MultiFab plotfile(ba, dmap, nPlot, 0);
 
@@ -27,30 +28,20 @@ void WritePlotFile(int step,
     // keep a counter for plotfile variables
     int cnt = 0;
 
-    varNames[cnt++] = "rhotot";
-
     for (int i=0; i<AMREX_SPACEDIM; ++i) {
         std::string x = "vel";
         x += (120+i);
         varNames[cnt++] = x;
     }
 
-	varNames[cnt++] = "div";
-
     // reset plotfile variable counter
     cnt = 0;
-
-    // copy rhotot into plotfile
-    plotfile.copy(rhotot,0,cnt,1);
-    cnt++;
 
     // average staggered velocities to cell-centers and copy into plotfile
     for (int i=0; i<AMREX_SPACEDIM; ++i) {
         AverageFaceToCC(umac[i],0,plotfile,cnt,1);
         cnt++;
     }
-
-    plotfile.copy(div,0,cnt,1);
 
     // write a plotfile
     WriteSingleLevelPlotfile(plotfilename,plotfile,varNames,geom,time,step);
