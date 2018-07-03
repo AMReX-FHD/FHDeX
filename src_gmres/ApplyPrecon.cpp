70b67f69ce8e9fea2bd7bfbc1169fa4005fbd4fa
Andy Nonaka <AJNonaka@lbl.gov>
2018-07-03 16:49:27 -0700

    more preconditioner progress

--- src_gmres/ApplyPrecon.cpp
+++ src_gmres/ApplyPrecon.cpp
@@ -18,36 +18,84 @@ void ApplyPrecon(const std::array<MultiFab, AMREX_SPACEDIM>& b_u,
                  const std::array<MultiFab, NUM_EDGE>& beta_ed,
                  const MultiFab& gamma,
                  const Real& theta_alpha,
-                 const Real* dx)
+                 const Real* dx,
+                 const Geometry& geom)
 {
+    BoxArray ba = b_p.boxArray();
+    DistributionMapping dmap = b_p.DistributionMap();
+
+    MultiFab phi     (ba,dmap,1,1);
+    MultiFab mac_rhs (ba,dmap,1,0);
+    MultiFab zero_fab(ba,dmap,1,0);
+    MultiFab x_p_tmp (ba,dmap,1,1);
+
+    std::array< MultiFab, AMREX_SPACEDIM > alphainv_fc;
+    std::array< MultiFab, AMREX_SPACEDIM > one_fab_fc;
+    std::array< MultiFab, AMREX_SPACEDIM > zero_fab_fc;
+    std::array< MultiFab, AMREX_SPACEDIM > b_u_tmp;
+
+    // build alphainv_fc, one_fab_fc, zero_fab_fc, and b_u_tmp
+    AMREX_D_TERM(alphainv_fc[0].define(convert(ba,nodal_flag_x), dmap, 1, 0);,
+                 alphainv_fc[1].define(convert(ba,nodal_flag_y), dmap, 1, 0);,
+                 alphainv_fc[2].define(convert(ba,nodal_flag_z), dmap, 1, 0););
+    AMREX_D_TERM(one_fab_fc[0]. define(convert(ba,nodal_flag_x), dmap, 1, 0);,
+                 one_fab_fc[1]. define(convert(ba,nodal_flag_y), dmap, 1, 0);,
+                 one_fab_fc[2]. define(convert(ba,nodal_flag_z), dmap, 1, 0););
+    AMREX_D_TERM(zero_fab_fc[0].define(convert(ba,nodal_flag_x), dmap, 1, 0);,
+                 zero_fab_fc[1].define(convert(ba,nodal_flag_y), dmap, 1, 0);,
+                 zero_fab_fc[2].define(convert(ba,nodal_flag_z), dmap, 1, 0););
+    AMREX_D_TERM(b_u_tmp[0].    define(convert(ba,nodal_flag_x), dmap, 1, 0);,
+                 b_u_tmp[1].    define(convert(ba,nodal_flag_y), dmap, 1, 0);,
+                 b_u_tmp[2].    define(convert(ba,nodal_flag_z), dmap, 1, 0););
+    
+    // set alphainv_fc to 1/alpha_fc
+    // set one_fab_fc to 1.
+    // set zero_fab_fc to 0.
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        alphainv_fc[i].setVal(1.);
+        alphainv_fc[i].divide(alpha_fc[i],0,1,0);
+        one_fab_fc[i].setVal(1.);
+        zero_fab_fc[i].setVal(0.);
+    }
+
+    // set the initial guess for Phi in the Poisson solve to 0
+    // set x_u = 0 as initial guess
+    phi.setVal(0.);
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        x_u[i].setVal(0.);
+    }
+
+    // 1 = projection preconditioner
+    // 2 = lower triangular preconditioner
+    // 3 = upper triangular preconditioner
+    // 4 = block diagonal preconditioner
+    // 5 = Uzawa-type approximation (see paper)
+    // 6 = upper triangular + viscosity-based BFBt Schur complement (from Georg Stadler)
 
     // projection preconditioner
     if (abs(precon_type) == 1) {
 
-        /*
-          STEP 1: Solve for an intermediate state, x_u^star, using an implicit viscous solve
-                  x_u^star = A^{-1} b_u
-        */
+        ////////////////////
+        // STEP 1: Solve for an intermediate state, x_u^star, using an implicit viscous solve
+        //         x_u^star = A^{-1} b_u
+        ////////////////////
 
         // x_u^star = A^{-1} b_u
-        //
-        //
+        StagMGSolver(alpha_fc,beta,beta_ed,gamma,x_u,b_u,theta_alpha,geom);
 
-        /*
-          STEP 2: Construct RHS for pressure Poisson problem
-        */
+        ////////////////////
+        // STEP 2: Construct RHS for pressure Poisson problem
+        ////////////////////
 
         // set mac_rhs = D(x_u^star)
-        //
-        //
+        ComputeDiv(mac_rhs,x_u,0,0,1,geom,0);
 
         // add b_p to mac_rhs
-        //
-        //
+        MultiFab::Add(mac_rhs,b_p,0,0,1,0);
 
-        /*
-          STEP 3: Compute x_u
-        */
+        ////////////////////
+        // STEP 3: Compute x_u
+        ////////////////////
 
         // use multigrid to solve for Phi
         // x_u^star is only passed in to get a norm for absolute residual criteria
@@ -58,9 +106,9 @@ void ApplyPrecon(const std::array<MultiFab, AMREX_SPACEDIM>& b_u,
         //
         //
 
-        /*
-          STEP 4: Compute x_p by applying the Schur complement approximation
-        */
+        ////////////////////
+        // STEP 4: Compute x_p by applying the Schur complement approximation
+        ////////////////////
 
         if (visc_schur_approx == 0) {
             // if precon_type = +1, or theta_alpha=0 then x_p = theta_alpha*Phi - c*beta*(mac_rhs)
