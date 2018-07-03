ee1e59c323a6b6d218e6c273f4e204989370c74f
Andy Nonaka <AJNonaka@lbl.gov>
2018-07-03 16:16:10 -0700

    generalize ComputeDiv to take in start_incomp, start_outcomp, and ncomp.
    also fixed a bug in the 3d increment divergence function

--- src_common/src_F90/compute_div_and_grad.F90
+++ src_common/src_F90/compute_div_and_grad.F90
@@ -1,16 +1,25 @@
-
-!Computes divergence at cell centres from velcocities at cell faces
-#if AMREX_SPACEDIM == 2
-subroutine compute_div(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, div, divlo, divhi, dx, increment) bind(C, name="compute_div")
+module compute_div_and_grad_module
 
   use amrex_fort_module, only : amrex_real
   implicit none
 
-	integer, intent(in) :: lo(2), hi(2), velxlo(2), velxhi(2), velylo(2), velyhi(2), divlo(2), divhi(2), increment
-  real(amrex_real), intent(inout) :: velx(velxlo(1):velxhi(1),velxlo(2):velxhi(2))
-  real(amrex_real), intent(inout) :: vely(velylo(1):velyhi(1),velylo(2):velyhi(2))
+  private
+
+  public :: compute_div, compute_grad
+
+contains
+
+  ! Computes divergence at cell centres from face values
+#if (AMREX_SPACEDIM == 2)
+subroutine compute_div(lo, hi, phix, phixlo, phixhi, phiy, phiylo, phiyhi, &
+                       div, divlo, divhi, dx, increment) bind(C, name="compute_div")
+
+  integer         , intent(in   ) :: lo(2), hi(2), phixlo(2), phixhi(2), phiylo(2), phiyhi(2)
+  integer         , intent(in   ) :: divlo(2), divhi(2), increment
+  real(amrex_real), intent(inout) :: phix(phixlo(1):phixhi(1),phixlo(2):phixhi(2))
+  real(amrex_real), intent(inout) :: phiy(phiylo(1):phiyhi(1),phiylo(2):phiyhi(2))
   real(amrex_real), intent(inout) :: div(divlo(1):divhi(1),divlo(2):divhi(2))
-	real(amrex_real), intent(in   ) :: dx(2)
+  real(amrex_real), intent(in   ) :: dx(2)
 
   ! local variables
   integer i,j
@@ -20,36 +29,38 @@ subroutine compute_div(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, div, 
   dyInv = 1.d0/dx(2)
 
   if (increment .eq. 0) then
-  	!compute divergence
-    do j = lo(2), hi(2)
-    	do i = lo(1), hi(1)
-       div(i,j) = (-velx(i,j) + velx(i+1,j))*dxInv + (-vely(i,j) + vely(i,j+1))*dyInv
-    	end do
-    end do
+     ! compute divergence
+     do j = lo(2), hi(2)
+     do i = lo(1), hi(1)
+        div(i,j) = (-phix(i,j) + phix(i+1,j))*dxInv + &
+                   (-phiy(i,j) + phiy(i,j+1))*dyInv
+     end do
+     end do
   else
-  	!increment divergence
-    do j = lo(2), hi(2)
-    	do i = lo(1), hi(1)
-       div(i,j) = div(i,j) + (-velx(i,j) + velx(i+1,j))*dxInv + (-vely(i,j) + vely(i,j+1))*dyInv
-    	end do
-    end do
+     ! increment divergence
+     do j = lo(2), hi(2)
+     do i = lo(1), hi(1)
+        div(i,j) = div(i,j) + (-phix(i,j) + phix(i+1,j))*dxInv + &
+                              (-phiy(i,j) + phiy(i,j+1))*dyInv
+     end do
+     end do
   endif
 
 end subroutine compute_div
 #endif
 
 #if AMREX_SPACEDIM == 3
-subroutine compute_div(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, velz, velzlo, velzhi, div, divlo, divhi, dx, increment) bind(C, name="compute_div")
+subroutine compute_div(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, velz, velzlo, velzhi, &
+                       div, divlo, divhi, dx, increment) bind(C, name="compute_div")
 
-  use amrex_fort_module, only : amrex_real
-  implicit none
-
-	integer, intent(in) :: lo(3), hi(3), velxlo(3), velxhi(3), velylo(3), velyhi(3), velzlo(3), velzhi(3), divlo(3), divhi(3), increment
+  integer         , intent(in   ) :: lo(3), hi(3)
+  integer         , intent(in   ) :: velxlo(3), velxhi(3), velylo(3), velyhi(3), velzlo(3), velzhi(3)
+  integer         , intent(in   ) :: divlo(3), divhi(3), increment
   real(amrex_real), intent(inout) :: velx(velxlo(1):velxhi(1),velxlo(2):velxhi(2),velxlo(3):velxhi(3))
   real(amrex_real), intent(inout) :: vely(velylo(1):velyhi(1),velylo(2):velyhi(2),velylo(3):velyhi(3))
   real(amrex_real), intent(inout) :: velz(velzlo(1):velzhi(1),velzlo(2):velzhi(2),velzlo(3):velzhi(3))
   real(amrex_real), intent(inout) :: div(divlo(1):divhi(1),divlo(2):divhi(2),divlo(3):divhi(3))
-	real(amrex_real), intent(in   ) :: dx(3) 
+  real(amrex_real), intent(in   ) :: dx(3) 
 
   ! local variables
   integer i,j,k
@@ -60,23 +71,27 @@ subroutine compute_div(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, velz,
   dzInv = 1.d0/dx(3)
 
   if (increment .eq. 0) then
-	  !compute divergence
-	  do k = lo(3), hi(3)
-		  do j = lo(2), hi(2)
-			  do i = lo(1), hi(1)
-		     div(i,j,k) = (-velx(i,j,k) + velx(i+1,j,k))*dxInv + (-vely(i,j,k) + vely(i,j+1,k))*dyInv + (-velz(i,j,k) + vely(i,j,k+1))*dzInv
-			  end do
-		  end do
-	  end do
+     ! compute divergence
+     do k = lo(3), hi(3)
+     do j = lo(2), hi(2)
+     do i = lo(1), hi(1)
+        div(i,j,k) = (-velx(i,j,k) + velx(i+1,j,k))*dxInv + &
+                     (-vely(i,j,k) + vely(i,j+1,k))*dyInv + &
+                     (-velz(i,j,k) + vely(i,j,k+1))*dzInv
+     end do
+     end do
+     end do
   else
-	  !compute divergence
-	  do k = lo(3), hi(3)
-		  do j = lo(2), hi(2)
-			  do i = lo(1), hi(1)
-		     div(i,j,k) = (-velx(i,j,k) + velx(i+1,j,k))*dxInv + (-vely(i,j,k) + vely(i,j+1,k))*dyInv + (-velz(i,j,k) + vely(i,j,k+1))*dzInv
-			  end do
-		  end do
-	  end do
+     ! increment divergence
+     do k = lo(3), hi(3)
+     do j = lo(2), hi(2)
+     do i = lo(1), hi(1)
+        div(i,j,k) = div(i,j,k) + (-velx(i,j,k) + velx(i+1,j,k))*dxInv + &
+                                  (-vely(i,j,k) + vely(i,j+1,k))*dyInv + &
+                                  (-velz(i,j,k) + vely(i,j,k+1))*dzInv
+     end do
+     end do
+     end do
   endif
 
 end subroutine compute_div
@@ -86,14 +101,11 @@ end subroutine compute_div
 #if AMREX_SPACEDIM == 2
 subroutine compute_grad(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, phi, philo, phihi, dx) bind(C, name="compute_grad")
 
-  use amrex_fort_module, only : amrex_real
-  implicit none
-
-	integer, intent(in) :: lo(2), hi(2), velxlo(2), velxhi(2), velylo(2), velyhi(2), philo(2), phihi(2)
+  integer, intent(in) :: lo(2), hi(2), velxlo(2), velxhi(2), velylo(2), velyhi(2), philo(2), phihi(2)
   real(amrex_real), intent(inout) :: velx(velxlo(1):velxhi(1),velxlo(2):velxhi(2))
   real(amrex_real), intent(inout) :: vely(velylo(1):velyhi(1),velylo(2):velyhi(2))
   real(amrex_real), intent(inout) :: phi(philo(1):phihi(1),philo(2):phihi(2))
-	real(amrex_real), intent(in   ) :: dx(2) 
+  real(amrex_real), intent(in   ) :: dx(2) 
 
   ! local variables
   integer i,j
@@ -123,9 +135,6 @@ end subroutine compute_grad
 
 #if AMREX_SPACEDIM == 3
 subroutine compute_grad(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, velz, velzlo, velzhi, phi, philo, phihi, dx) bind(C, name="compute_grad")
-
-  use amrex_fort_module, only : amrex_real
-  implicit none
 
 	integer, intent(in) :: lo(3), hi(3), velxlo(3), velxhi(3), velylo(3), velyhi(3), velzlo(3), velzhi(3), philo(3), phihi(3)
   real(amrex_real), intent(inout) :: velx(velxlo(1):velxhi(1),velxlo(2):velxhi(2),velxlo(3):velxhi(3))
@@ -175,5 +184,4 @@ subroutine compute_grad(lo, hi, velx, velxlo, velxhi, vely, velylo, velyhi, velz
 end subroutine compute_grad
 #endif
 
-
-
+end module compute_div_and_grad_module
