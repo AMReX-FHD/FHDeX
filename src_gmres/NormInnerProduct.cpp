20e00d2da84561ff539d3115a7a9f96639cf16e3
Andy Nonaka <AJNonaka@lbl.gov>
2018-07-03 13:46:51 -0700

    outline for staginnerprod

---
+++ src_gmres/NormInnerProduct.cpp
@@ -0,0 +1,12 @@
+#include "gmres_functions.H"
+
+using namespace amrex;
+
+void StagInnerProd(const std::array<MultiFab, AMREX_SPACEDIM>& m1,
+                   const int& comp1, 
+                   const std::array<MultiFab, AMREX_SPACEDIM>& m2,
+                   const int& comp2,
+                   Vector<Real> prod_val)
+{
+
+}
