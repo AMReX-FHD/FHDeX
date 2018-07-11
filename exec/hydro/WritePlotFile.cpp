2dc4a4c6c341da75303f7c177f45400e5b2799ef
Andy Nonaka <AJNonaka@lbl.gov>
2018-07-11 10:26:44 -0700

    set up a driver to call GMRES

---
+++ exec/hydro/WritePlotFile.cpp
@@ -0,0 +1,60 @@
+#include "hydro_functions.H"
+
+#include "AMReX_PlotFileUtil.H"
+
+#include "common_functions.H"
+
+#include "common_namespace.H"
+
+using namespace common;
+
+void WritePlotFile(int step,
+                   const amrex::Real time,
+                   const amrex::Geometry geom,
+                   const std::array< MultiFab, AMREX_SPACEDIM >& umac)
+{
+    const std::string plotfilename = Concatenate("plt",step,7);
+
+    BoxArray ba = umac[0].boxArray();
+    DistributionMapping dmap = umac[0].DistributionMap();
+
+    // plot all the velocity variables
+    int nPlot = AMREX_SPACEDIM;
+
+    MultiFab plotfile(ba, dmap, nPlot, 0);
+
+    Vector<std::string> varNames(nPlot);
+
+    // keep a counter for plotfile variables
+    int cnt = 0;
+
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        std::string x = "vel";
+        x += (120+i);
+        varNames[cnt++] = x;
+    }
+
+    // reset plotfile variable counter
+    cnt = 0;
+
+    // average staggered velocities to cell-centers and copy into plotfile
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        AverageFaceToCC(umac[i],0,plotfile,cnt,1);
+        cnt++;
+    }
+
+    // write a plotfile
+    WriteSingleLevelPlotfile(plotfilename,plotfile,varNames,geom,time,step);
+
+    // staggered velocity
+    const std::string plotfilenamex = Concatenate("stagx",step,7);
+    const std::string plotfilenamey = Concatenate("stagy",step,7);
+    const std::string plotfilenamez = Concatenate("stagz",step,7);
+
+    WriteSingleLevelPlotfile(plotfilenamex,umac[0],{"umac"},geom,time,step);
+    WriteSingleLevelPlotfile(plotfilenamey,umac[1],{"vmac"},geom,time,step);
+#if (AMREX_SPACEDIM == 3)
+    WriteSingleLevelPlotfile(plotfilenamez,umac[2],{"wmac"},geom,time,step);
+#endif
+
+}
---
+++ exec/hydro/init_funcs.F90
@@ -0,0 +1,108 @@
+subroutine init_vel(lo, hi, vel, vello, velhi, dx, prob_lo, prob_hi, di, &
+                    reallo, realhi) bind(C, name="init_vel")
+
+  use amrex_fort_module, only : amrex_real
+
+  implicit none
+
+  integer         , intent(in   ) :: lo(3), hi(3), vello(3), velhi(3), di
+  real(amrex_real), intent(inout) :: vel(vello(1):velhi(1),vello(2):velhi(2),vello(3):velhi(3))
+  real(amrex_real), intent(in   ) :: reallo(3), realhi(3)
+  real(amrex_real), intent(in   ) :: prob_lo(3) 
+  real(amrex_real), intent(in   ) :: prob_hi(3)
+  real(amrex_real), intent(in   ) :: dx(3) 
+
+  integer          :: i,j,k
+  double precision :: pos(3),center(3),partdom,itVec(3),relpos(3),rad,zshft
+
+#if (AMREX_SPACEDIM == 2)
+  zshft = 0.0d0
+#elif (AMREX_SPACEDIM == 3)
+  zshft = 0.5d0
+#endif
+
+  center = (realhi - reallo)/2d0;
+  partdom = ((realhi(1) - reallo(1))/4d0)**2;
+
+  if (di .EQ. 0) then
+     do k = lo(3), hi(3)
+        do j = lo(2), hi(2)
+           do i = lo(1), hi(1) + 1
+
+              itVec(1) = dble(i)*dx(1)
+              itVec(2) = (dble(j)+0.5d0)*dx(2)
+              itVec(3) = (dble(k)+zshft)*dx(3)
+
+              pos = reallo + itVec
+              relpos = pos - center
+              rad = DOT_PRODUCT(relpos,relpos)
+
+              ! if (rad .LT. partdom) then
+              !    vel(i,j,k) = -200*exp(-rad/(10*partdom*partdom))*relpos(2)
+              ! else
+              !    vel(i,j,k) = 0d0
+              ! endif
+
+              vel(i,j,k) = 1.d0 + exp(-rad**2/(1.0d0*partdom*partdom))
+
+           end do
+        end do
+     end do
+  endif
+
+  if (di .EQ. 1) then
+     do k = lo(3), hi(3)
+        do j = lo(2), hi(2) + 1
+           do i = lo(1), hi(1)
+
+              itVec(1) = (dble(i)+0.5d0)*dx(1)
+              itVec(2) = dble(j)*dx(2)
+              itVec(3) = (dble(k)+zshft)*dx(3)
+
+              pos = reallo + itVec
+              relpos = pos - center
+              rad = DOT_PRODUCT(relpos,relpos)
+
+              ! if (rad .LT. partdom) then
+              !    vel(i,j,k) = -200*exp(-rad/(10*partdom*partdom))*relpos(1)
+              ! else
+              !    vel(i,j,k) = 0
+              ! endif
+
+              vel(i,j,k) = 1.d0 + exp(-rad**2/(1.0d0*partdom*partdom))
+
+           end do
+        end do
+     end do
+  endif
+
+  if (di .EQ. 2) then
+     do k = lo(3), hi(3) + 1
+        do j = lo(2), hi(2)
+           do i = lo(1), hi(1)
+
+              itVec(1) = (dble(i)+0.5d0)*dx(1)
+              itVec(2) = (dble(j)+0.5d0)*dx(2)
+              itVec(3) = dble(k)*dx(3)
+
+              pos = reallo + itVec
+              relpos = pos - center
+              rad = DOT_PRODUCT(relpos,relpos)
+
+              ! if (rad .LT. partdom) then
+              !    vel(i,j,k) = 0
+              ! else
+              !    vel(i,j,k) = 0
+              ! endif
+
+              vel(i,j,k) = 1.d0 + exp(-rad**2/(1.0d0*partdom*partdom))
+
+           end do
+        end do
+     end do
+  endif
+
+
+end subroutine init_vel
+
+
