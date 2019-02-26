16ea69fab035730856be89ce5fac97a3b60b0525
Daniel Ladiges <drladiges@lbl.gov>
2019-02-26 14:36:55 -0800

    compressible stuff

--- src_compressible/timeStep.cpp
+++ src_compressible/timeStep.cpp
@@ -91,7 +91,7 @@ void RK3step(MultiFab& cu, MultiFab& cup, MultiFab& cup2, MultiFab& cup3, MultiF
     cu.FillBoundary(geom.periodicity());
     prim.FillBoundary(geom.periodicity());
 
-    doMembrane(cu, prim, flux, geom, ZFILL(dx), dt);
+    //doMembrane(cu, prim, flux, geom, ZFILL(dx), dt);
 
     calculateTransportCoeffs(prim, eta, zeta, kappa);
 
