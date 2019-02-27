b0e9015baae1565f0406ecfdfe7f76d9c2b6bebd
Daniel Ladiges <drladiges@lbl.gov>
2019-02-27 14:41:58 -0800

    Added simplified move ions functions.

--- src_particles/src_F90/particle_functions.F90
+++ src_particles/src_F90/particle_functions.F90
@@ -805,3 +805,300 @@ subroutine move_particles_fhd(particles, np, lo, hi, &
   
 end subroutine move_particles_fhd
 
+subroutine move_ions_fhd(particles, np, lo, hi, &
+     cell_part_ids, cell_part_cnt, clo, chi, plo, phi, dx, dt, plof, dxf, &
+                                     velx, velxlo, velxhi, &
+                                     vely, velylo, velyhi, &
+#if (BL_SPACEDIM == 3)
+                                     velz, velzlo, velzhi, &
+#endif
+                                     coordsx, coordsxlo, coordsxhi, &
+                                     coordsy, coordsylo, coordsyhi, &
+#if (BL_SPACEDIM == 3)
+                                     coordsz, coordszlo, coordszhi, &
+#endif
+
+                                     sourcex, sourcexlo, sourcexhi, &
+                                     sourcey, sourceylo, sourceyhi, &
+#if (BL_SPACEDIM == 3)
+                                     sourcez, sourcezlo, sourcezhi, &
+#endif
+                                     surfaces, ns)bind(c,name="move_ions_fhd")
+  use amrex_fort_module, only: amrex_real
+  use iso_c_binding, only: c_ptr, c_int, c_f_pointer
+  use cell_sorted_particle_module, only: particle_t, remove_particle_from_cell
+  use common_namelist_module, only: visc_type, k_B
+  use rng_functions_module
+  use surfaces_module
+  
+  implicit none
+
+  integer,          intent(in   )         :: np, ns, lo(3), hi(3), clo(3), chi(3), velxlo(3), velxhi(3), velylo(3), velyhi(3)
+  integer,          intent(in   )         :: sourcexlo(3), sourcexhi(3), sourceylo(3), sourceyhi(3)
+  integer,          intent(in   )         :: coordsxlo(3), coordsxhi(3), coordsylo(3), coordsyhi(3)
+#if (AMREX_SPACEDIM == 3)
+  integer,          intent(in   )         :: velzlo(3), velzhi(3), sourcezlo(3), sourcezhi(3), coordszlo(3), coordszhi(3)
+#endif
+  type(particle_t), intent(inout), target :: particles(np)
+  type(surface_t),  intent(in),    target :: surfaces(ns)
+
+  double precision, intent(in   )         :: dx(3), dxf(3), dt, plo(3), phi(3), plof(3)
+
+  double precision, intent(in   ) :: velx(velxlo(1):velxhi(1),velxlo(2):velxhi(2),velxlo(3):velxhi(3))
+  double precision, intent(in   ) :: vely(velylo(1):velyhi(1),velylo(2):velyhi(2),velylo(3):velyhi(3))
+#if (AMREX_SPACEDIM == 3)
+  double precision, intent(in   ) :: velz(velzlo(1):velzhi(1),velzlo(2):velzhi(2),velzlo(3):velzhi(3))
+#endif
+
+  double precision, intent(in   ) :: coordsx(coordsxlo(1):coordsxhi(1),coordsxlo(2):coordsxhi(2),coordsxlo(3):coordsxhi(3),1:AMREX_SPACEDIM)
+  double precision, intent(in   ) :: coordsy(coordsylo(1):coordsyhi(1),coordsylo(2):coordsyhi(2),coordsylo(3):coordsyhi(3),1:AMREX_SPACEDIM)
+#if (AMREX_SPACEDIM == 3)
+  double precision, intent(in   ) :: coordsz(coordszlo(1):coordszhi(1),coordszlo(2):coordszhi(2),coordszlo(3):coordszhi(3),1:AMREX_SPACEDIM)
+#endif
+
+  double precision, intent(inout) :: sourcex(sourcexlo(1):sourcexhi(1),sourcexlo(2):sourcexhi(2),sourcexlo(3):sourcexhi(3))
+  double precision, intent(inout) :: sourcey(sourceylo(1):sourceyhi(1),sourceylo(2):sourceyhi(2),sourceylo(3):sourceyhi(3))
+#if (AMREX_SPACEDIM == 3)
+  double precision, intent(inout) :: sourcez(sourcezlo(1):sourcezhi(1),sourcezlo(2):sourcezhi(2),sourcezlo(3):sourcezhi(3))
+#endif
+
+  type(c_ptr),      intent(inout) :: cell_part_ids(clo(1):chi(1), clo(2):chi(2), clo(3):chi(3))
+  integer(c_int),   intent(inout) :: cell_part_cnt(clo(1):chi(1), clo(2):chi(2), clo(3):chi(3))
+  
+  integer :: i, j, k, p, cell_np, new_np, intside, intsurf, push, loopcount, pointcount
+  integer :: ni(3), fi(3)
+  integer(c_int), pointer :: cell_parts(:)
+  type(particle_t), pointer :: part
+  type(surface_t), pointer :: surf
+  real(amrex_real) dxinv(3), dxfinv(3), onemdxf(3), ixf(3), localvel(3), localbeta, bfac(3), deltap(3), std, normalrand(3), nodalp, tempvel(3), intold, inttime, runerr, runtime, adj, adjalt, domsize(3), posalt(3), propvec(3)
+
+  double precision  :: cc(0:7)
+  double precision  :: rr(0:7)
+
+  double precision :: int_weights(-3:3,-3:3,-3:3)
+
+
+  !Zero source terms
+  do k = sourcexlo(3), sourcexhi(3)
+    do j = sourcexlo(2), sourcexhi(2)
+      do i = sourcexlo(1), sourcexhi(1)
+
+        sourcex(i,j,k) = 0;
+
+      enddo
+    enddo
+  enddo
+
+  do k = sourceylo(3), sourceyhi(3)
+    do j = sourceylo(2), sourceyhi(2)
+      do i = sourceylo(1), sourceyhi(1)
+
+        sourcey(i,j,k) = 0;
+
+      enddo
+    enddo
+  enddo
+
+#if (BL_SPACEDIM == 3)
+  do k = sourcezlo(3), sourcezhi(3)
+    do j = sourcezlo(2), sourcezhi(2)
+      do i = sourcezlo(1), sourcezhi(1)
+
+        sourcez(i,j,k) = 0;
+
+      enddo
+    enddo
+  enddo
+#endif
+
+   
+  domsize = phi - plo
+
+  adj = 0.999999
+  adjalt = 2d0*(1d0 - adj)
+
+  dxinv = 1.d0/dx
+
+  dxfinv = 1.d0/dxf
+  onemdxf = 1.d0 - dxf
+  
+  do p = 1, ns
+
+    surf => surfaces(p)  
+
+    surf%fxleft = 0
+    surf%fyleft = 0
+    surf%fzleft = 0
+
+    surf%fxright = 0
+    surf%fyright = 0
+    surf%fzright = 0
+
+  enddo
+
+  !print *, "Starting"        
+
+  do k = lo(3), hi(3)
+     do j = lo(2), hi(2)
+        do i = lo(1), hi(1)
+           cell_np = cell_part_cnt(i,j,k)
+           call c_f_pointer(cell_part_ids(i,j,k), cell_parts, [cell_np])
+
+           new_np = cell_np
+           p = 1
+
+           do while (p <= new_np)
+
+              runtime = dt
+              part => particles(cell_parts(p))
+
+                !find fluid cell
+              fi(1) = floor((part%pos(1) - plof(1))*dxfinv(1))
+              fi(2) = floor((part%pos(2) - plof(2))*dxfinv(2))
+#if (BL_SPACEDIM == 3)
+              fi(3) = floor((part%pos(3) - plof(3))*dxfinv(3))
+#else
+              fi(3) = 0
+#endif
+              !Interpolate fluid fields. ixf is the particle position in local cell coordinates. fi is the fluid cell
+              ixf(1) = (part%pos(1) - coordsx(fi(1),fi(2),fi(3),1))*dxfInv(1)
+              ixf(2) = (part%pos(2) - coordsy(fi(1),fi(2),fi(3),2))*dxfInv(2)
+#if (BL_SPACEDIM == 3)
+              ixf(3) = (part%pos(3) - coordsz(fi(1),fi(2),fi(3),3))*dxfInv(3)
+#endif
+
+              !dxf is the size of the fluid cell
+
+
+              call get_interpolation_weights(cc, rr, ixf, onemdxf)
+
+
+                !Brownian forcing
+
+              call get_particle_normal(normalrand(1))
+              call get_particle_normal(normalrand(2))
+              call get_particle_normal(normalrand(3))
+
+              runerr = 1d0;
+
+              deltap(1) = part%vel(1)
+              deltap(2) = part%vel(1)
+#if (BL_SPACEDIM == 3)
+              deltap(3) = part%vel(1)
+#endif
+
+!this velocity update introduces a timetep error when a boundary intersection occurs. Need to use some kind of iteration to get a consistent intersection time and velocity update.
+
+              std = sqrt(-part%drag_factor*localbeta*k_B*2d0*runtime*293d0)/part%mass
+
+              bfac(1) = std*normalrand(1)
+              bfac(2) = std*normalrand(2)
+              bfac(3) = std*normalrand(3)
+
+              !print *, "brownian: ", bfac, " propusive: ", part%dir*part%propulsion*runtime
+
+                !print *, "Position 1: ", part%pos, " Vel 1: ", part%vel, "localvel: ", localbeta
+
+              part%vel(1) = part%accel_factor*localbeta*(part%vel(1)-localvel(1))*runtime + bfac(1) + part%vel(1)
+              part%vel(2) = part%accel_factor*localbeta*(part%vel(2)-localvel(2))*runtime + bfac(2) + part%vel(2)
+#if (BL_SPACEDIM == 3)
+              part%vel(3) = part%accel_factor*localbeta*(part%vel(3)-localvel(3))*runtime + bfac(3) + part%vel(3)
+#endif
+              call redirect(part)
+  
+              part%vel = part%dir*part%propulsion*runtime + part%vel
+
+              deltap(1) = part%mass*(part%vel(1) - deltap(1))
+              deltap(2) = part%mass*(part%vel(2) - deltap(2))
+#if (BL_SPACEDIM == 3)
+              deltap(3) = part%mass*(part%vel(3) - deltap(3))
+#endif
+
+#if (BL_SPACEDIM == 3)
+              call distribute_momentum(deltap, rr, fi ,sourcex, sourcexlo, sourcexhi, sourcey, sourceylo, sourceyhi, sourcez, sourcezlo, sourcezhi)
+#endif
+#if (BL_SPACEDIM == 2)
+              call distribute_momentum(deltap, rr, fi ,sourcex, sourcexlo, sourcexhi, sourcey, sourceylo, sourceyhi)
+#endif
+
+              do while (runtime .gt. 0)
+
+                !if(part%id .eq. 5) then
+                !  print *, "Starting vel: ", part%vel, " Starting pos: ", part%pos/phi
+                !endif
+
+
+                call find_intersect(part,runtime, surfaces, ns, intsurf, inttime, intside, phi, plo)
+
+                posalt(1) = inttime*part%vel(1)*adjalt
+                posalt(2) = inttime*part%vel(2)*adjalt
+#if (BL_SPACEDIM == 3)
+                posalt(3) = inttime*part%vel(3)*adjalt
+#endif
+
+                ! move the particle in a straight line, adj factor prevents double detection of boundary intersection
+                part%pos(1) = part%pos(1) + inttime*part%vel(1)*adj
+                part%pos(2) = part%pos(2) + inttime*part%vel(2)*adj
+#if (BL_SPACEDIM == 3)
+                part%pos(3) = part%pos(3) + inttime*part%vel(3)*adj
+#endif
+
+                runtime = runtime - inttime
+
+                if(intsurf .gt. 0) then
+
+                  surf => surfaces(intsurf)
+                  !if(intsurf .eq. 7) then
+                 !   print *, part%id, " prevel: ", part%vel
+                  !endif
+                  call apply_bc(surf, part, intside, domsize, push)
+                  !if(intsurf .eq. 7) then
+                 !   print *, part%id, " postvel: ", part%vel
+                 ! endif
+
+
+                    if(push .eq. 1) then
+                      
+                      part%pos(1) = part%pos(1) + posalt(1)
+                      part%pos(2) = part%pos(2) + posalt(2)
+#if (BL_SPACEDIM == 3)
+                      part%pos(3) = part%pos(3) + posalt(3)
+#endif
+                    endif
+                    
+                endif
+
+               ! if(part%id .eq. 5) then
+               !   print *, "Final vel: ", part%vel, " Final pos: ", part%pos/phi
+              !  endif
+
+              enddo
+
+              ! if it has changed cells, remove from vector.
+              ! otherwise continue
+              ni(1) = floor((part%pos(1) - plo(1))*dxinv(1))
+              ni(2) = floor((part%pos(2) - plo(2))*dxinv(2))
+#if (BL_SPACEDIM == 3)
+              ni(3) = floor((part%pos(3) - plo(3))*dxinv(3))
+#else
+              ni(3) = 0
+#endif
+
+              if ((ni(1) /= i) .or. (ni(2) /= j) .or. (ni(3) /= k)) then
+                 part%sorted = 0
+                 call remove_particle_from_cell(cell_parts, cell_np, new_np, p)  
+              else
+                 p = p + 1
+              end if
+           end do
+
+           cell_part_cnt(i,j,k) = new_np
+    
+        end do
+     end do
+  end do
+
+  !print *, "Ending"        
+  
+end subroutine move_ions_fhd
+
