9dc87bf0bbafe7396fad8ae46a12323eb68b86fd
Johannes Blaschke <jpblaschke@lbl.gov>
2019-03-06 22:07:43 -0800

    Re-enable corner cases for BC filling

--- src_common/MultiFABPhysBC.cpp
+++ src_common/MultiFABPhysBC.cpp
@@ -3,17 +3,17 @@
 
 
 
-void MultiFABPhysBC(amrex::MultiFab & pressure, const amrex::Geometry & geom) {
-    MultiFABPhysBC(pressure, amrex::IntVect{AMREX_D_DECL(1, 1, 1)}, geom);
+void MultiFABPhysBC(MultiFab & pressure, const Geometry & geom) {
+    MultiFABPhysBC(pressure, IntVect{AMREX_D_DECL(1, 1, 1)}, geom);
 }
 
 
 
-void MultiFABPhysBC(amrex::MultiFab & pressure, int seq_fill_ghost, const amrex::Geometry & geom) {
+void MultiFABPhysBC(MultiFab & pressure, int seq_fill_ghost, const Geometry & geom) {
 
-    amrex::IntVect fill_ghost{AMREX_D_DECL(1, 1, 1)};
-    // for(int i=0; i<=seq_fill_ghost; i++)
-    //     fill_ghost[i] = 1;
+    IntVect fill_ghost{AMREX_D_DECL(0, 0, 0)};
+    for(int i=0; i<=seq_fill_ghost; i++)
+        fill_ghost[i] = 1;
 
     MultiFABPhysBC(pressure, fill_ghost, geom);
 
@@ -21,14 +21,13 @@ void MultiFABPhysBC(amrex::MultiFab & pressure, int seq_fill_ghost, const amrex:
 
 
 
-void MultiFABPhysBC(amrex::MultiFab & pressure, const amrex::IntVect & dim_fill_ghost,
-                    const amrex::Geometry & geom) {
+void MultiFABPhysBC(MultiFab & pressure, const IntVect & dim_fill_ghost, const Geometry & geom) {
 
-    amrex::Box dom(geom.Domain());
+    Box dom(geom.Domain());
 
-    for (amrex::MFIter mfi(pressure); mfi.isValid(); ++mfi) {
+    for (MFIter mfi(pressure); mfi.isValid(); ++mfi) {
 
-        const amrex::Box & bx = mfi.validbox();
+        const Box & bx = mfi.validbox();
         fab_physbc(BL_TO_FORTRAN_BOX(bx),
                    BL_TO_FORTRAN_BOX(dom),
                    BL_TO_FORTRAN_FAB(pressure[mfi]), pressure.nGrow(),
@@ -38,32 +37,30 @@ void MultiFABPhysBC(amrex::MultiFab & pressure, const amrex::IntVect & dim_fill_
 
 
 
-void MultiFABPhysBCDomainVel(amrex::MultiFab & vel, const amrex::Geometry & geom) {
-    MultiFABPhysBCDomainVel(vel, amrex::IntVect{AMREX_D_DECL(1,1,1)}, geom);
+void MultiFABPhysBCDomainVel(MultiFab & vel, const amrex::Geometry & geom) {
+    MultiFABPhysBCDomainVel(vel, IntVect{AMREX_D_DECL(1,1,1)}, geom);
 }
 
 
 
-void MultiFABPhysBCDomainVel(amrex::MultiFab & vel, int seq_fill_ghost,
-                             const amrex::Geometry & geom) {
+void MultiFABPhysBCDomainVel(MultiFab & vel, int seq_fill_ghost, const Geometry & geom) {
 
-    amrex::IntVect fill_ghost{AMREX_D_DECL(1, 1, 1)};
-    // for(int i=0; i<=seq_fill_ghost; i++)
-    //     fill_ghost[i] = 1;
+    IntVect fill_ghost{AMREX_D_DECL(0, 0, 0)};
+    for(int i=0; i<=seq_fill_ghost; i++)
+        fill_ghost[i] = 1;
 
     MultiFABPhysBCDomainVel(vel, fill_ghost, geom);
 }
 
 
 
-void MultiFABPhysBCDomainVel(amrex::MultiFab & vel, const amrex::IntVect & dim_fill_ghost,
-                             const amrex::Geometry & geom) {
+void MultiFABPhysBCDomainVel(MultiFab & vel, const IntVect & dim_fill_ghost, const Geometry & geom) {
 
-    amrex::Box dom(geom.Domain());
+    Box dom(geom.Domain());
 
-    for (amrex::MFIter mfi(vel); mfi.isValid(); ++mfi) {
+    for (MFIter mfi(vel); mfi.isValid(); ++mfi) {
 
-        const amrex::Box & bx = mfi.validbox();
+        const Box & bx = mfi.validbox();
         fab_physbc_domainvel(BL_TO_FORTRAN_BOX(bx),
                              BL_TO_FORTRAN_BOX(dom),
                              BL_TO_FORTRAN_FAB(vel[mfi]), vel.nGrow(),
@@ -73,32 +70,30 @@ void MultiFABPhysBCDomainVel(amrex::MultiFab & vel, const amrex::IntVect & dim_f
 
 
 
-void MultiFABPhysBCMacVel(amrex::MultiFab & vel, const amrex::Geometry & geom) {
-    MultiFABPhysBCMacVel(vel, amrex::IntVect{AMREX_D_DECL(1,1,1)}, geom);
+void MultiFABPhysBCMacVel(MultiFab & vel, const Geometry & geom) {
+    MultiFABPhysBCMacVel(vel, IntVect{AMREX_D_DECL(1,1,1)}, geom);
 }
 
 
 
-void MultiFABPhysBCMacVel(amrex::MultiFab & vel, int seq_fill_ghost,
-                          const amrex::Geometry & geom) {
+void MultiFABPhysBCMacVel(MultiFab & vel, int seq_fill_ghost, const Geometry & geom) {
 
-    amrex::IntVect fill_ghost{AMREX_D_DECL(1, 1, 1)};
-    // for(int i=0; i<=seq_fill_ghost; i++)
-    //     fill_ghost[i] = 1;
+    IntVect fill_ghost{AMREX_D_DECL(0, 0, 0)};
+    for(int i=0; i<=seq_fill_ghost; i++)
+        fill_ghost[i] = 1;
 
     MultiFABPhysBCMacVel(vel, fill_ghost, geom);
 }
 
 
 
-void MultiFABPhysBCMacVel(amrex::MultiFab & vel, const amrex::IntVect & dim_fill_ghost,
-                          const amrex::Geometry & geom) {
+void MultiFABPhysBCMacVel(MultiFab & vel, const IntVect & dim_fill_ghost, const Geometry & geom) {
 
-    amrex::Box dom(geom.Domain());
+    Box dom(geom.Domain());
 
-    for (amrex::MFIter mfi(vel); mfi.isValid(); ++mfi) {
+    for (MFIter mfi(vel); mfi.isValid(); ++mfi) {
 
-        const amrex::Box & bx = mfi.validbox();
+        const Box & bx = mfi.validbox();
         fab_physbc_macvel(BL_TO_FORTRAN_BOX(bx),
                           BL_TO_FORTRAN_BOX(dom),
                           BL_TO_FORTRAN_FAB(vel[mfi]), vel.nGrow(),
