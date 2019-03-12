6a1b3c8fc4a10223358c100f2418813a02c1779e
Johannes Blaschke <jpblaschke@lbl.gov>
2019-03-12 16:53:36 -0700

    added 2D version for pressure BCs

--- src_hydro/src_F90/hydro_bounds_mod.F90
+++ src_hydro/src_F90/hydro_bounds_mod.F90
@@ -11,6 +11,91 @@ module hydro_bounds_module
 
 contains
 
+#if (AMREX_SPACEDIM == 2)
+
+  subroutine set_pressure_bc(lo,         hi,            & ! dim(lo) == dim(hi) == 3
+       &                     dom_lo,     dom_hi,        &
+       &                     data, d_lo, d_hi, d_ncomp, & ! dim(d_lo) == dim(d_hi) == 3
+       &                     ngc)                       &
+       &                     bind(C, name="set_pressure_bc")
+
+    integer,          intent(in   ) :: lo(3), hi(3), dom_lo(3), dom_hi(3), &
+         &                             d_lo(3), d_hi(3), d_ncomp
+    integer, value,   intent(in   ) :: ngc
+    real(amrex_real), intent(inout) :: data(d_lo(1):d_hi(1), &
+         &                                  d_lo(2):d_hi(2), d_ncomp)
+
+    ! ** loop indices
+    integer :: i,j
+
+
+    !____________________________________________________________________________
+    ! Apply BC to X faces
+
+    if (lo(1) .eq. dom_lo(1)) then ! lower bound
+       if (bc_lo(1) .eq. -2) then ! pressure inflow
+
+          do j = lo(2)-ngc, hi(2)+ngc
+             do i = 1, ngc
+
+                data(lo(1)-i, j, :) = p_lo(1)
+
+             end do
+          end do
+
+       end if
+    end if
+
+    if (hi(1) .eq. dom_hi(1)) then ! upper bound
+       if (bc_hi(1) .eq. -2) then ! pressure inflow
+
+          do j = lo(2)-ngc, hi(2)+ngc
+             do i = 1, ngc
+
+                data(hi(1)+i, j, :) = p_hi(1)
+
+             end do
+          end do
+
+       end if
+    end if
+
+
+    !____________________________________________________________________________
+    ! Apply BC to Y faces
+
+    if (lo(2) .eq. dom_lo(2)) then ! lower bound
+       if (bc_lo(2) .eq. -2) then ! pressure inflow
+
+          do j = 1, ngc
+             do i = lo(1)-ngc, hi(1)+ngc
+
+                data(i, lo(2)-j, :) = p_lo(2)
+
+             end do
+          end do
+
+       end if
+    end if
+
+    if (hi(2) .eq. dom_hi(2)) then ! upper bound
+       if (bc_hi(2) .eq. -2) then ! pressure inflow
+
+          do j = 1, ngc
+             do i = lo(1)-ngc, hi(1)+ngc
+
+                data(i, hi(2)+j, :) = p_hi(2)
+
+             end do
+          end do
+
+       end if
+    end if
+
+  end subroutine set_pressure_bc
+
+#elif (AMREX_SPACEDIM == 3)
+
   subroutine set_pressure_bc(lo,         hi,            & ! dim(lo) == dim(hi) == 3
        &                     dom_lo,     dom_hi,        &
        &                     data, d_lo, d_hi, d_ncomp, & ! dim(d_lo) == dim(d_hi) == 3
@@ -135,7 +220,8 @@ contains
        end if
     end if
 
-
   end subroutine set_pressure_bc
+
+#endif
 
 end module hydro_bounds_module
