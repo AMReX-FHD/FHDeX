f18878c748e8194e4224eb6766d4284f2e2ef6ca
Andy Nonaka <AJNonaka@lbl.gov>
2019-03-27 13:07:20 -0700

    enable plot_base_name

--- exec/DiffuisonTest/WritePlotFile.cpp
+++ exec/DiffuisonTest/WritePlotFile.cpp
@@ -13,7 +13,7 @@ void WritePlotFile(int step,
                    const amrex::Geometry geom,
                    const std::array< MultiFab, AMREX_SPACEDIM >& umac)
 {
-    const std::string plotfilename = Concatenate("plt",step,7);
+    const std::string plotfilename = Concatenate(plot_base_name,step,7);
 
     BoxArray ba = umac[0].boxArray();
     DistributionMapping dmap = umac[0].DistributionMap();
