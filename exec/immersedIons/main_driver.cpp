1991062b5b28d689a42a5ba37c4e8cac65a20f64
Daniel Ladiges <drladiges@lbl.gov>
2019-04-28 18:35:23 -0700

    Particle movement now toggled from input

--- exec/immersedIons/main_driver.cpp
+++ exec/immersedIons/main_driver.cpp
@@ -687,18 +687,20 @@ void main_driver(const char* argv)
     	    advance(umac,pres,stochMfluxdiv,source,alpha_fc,beta,gamma,beta_ed,geom,dt);
         }
 
-        //Calls wet ion interpolation and movement.
-        particles.MoveIons(dt, dx, dxp, geom, umac, efield, RealFaceCoords, source, sourceTemp, surfaceList, surfaceCount, 3 /*this number currently does nothing, but we will use it later*/);
+        if(move_tog==1)
+        {
+            //Calls wet ion interpolation and movement.
+            particles.MoveIons(dt, dx, dxp, geom, umac, efield, RealFaceCoords, source, sourceTemp, surfaceList, surfaceCount, 3 /*this number currently does nothing, but we will use it later*/);
 
-        particles.Redistribute();
-        particles.ReBin();            //We may not need to redist & rebin after seperately for wet & dry moves - check this later
+            particles.Redistribute();
+            particles.ReBin();            //We may not need to redist & rebin after seperately for wet & dry moves - check this later
 
-        //particles.MoveParticlesDry(dt, dx, umac, RealFaceCoords, source, sourceTemp, surfaceList, surfaceCount);
+            //particles.MoveParticlesDry(dt, dx, umac, RealFaceCoords, source, sourceTemp, surfaceList, surfaceCount);
 
-        //These functions reorganise particles between cells and processes
-//Dout  particles.Redistribute();
-//Dout  particles.ReBin();
-
+            //These functions reorganise particles between cells and processes
+    //Dout  particles.Redistribute();
+    //Dout  particles.ReBin();
+        }
 
         //Start collecting statistics after step n_steps_skip
         if(step == n_steps_skip)
