f12281701403ffb70ac54a125ce0833d8664eb02
Daniel Ladiges <drladiges@lbl.gov>
2019-05-07 16:18:02 -0700

    Partial change for 2D compilation

--- src_common/src_F90/multifab_physbc_mod.F90
+++ src_common/src_F90/multifab_physbc_mod.F90
@@ -17,7 +17,7 @@ module multifab_physbc_module
 
 contains
 
-# if (AMREX_SPACEDIM == 2)
+#if (AMREX_SPACEDIM == 2)
 
   pure subroutine fab_physbc(lo,     hi,                  & ! dim(lo) == dim(hi) == 3
        &                     dom_lo, dom_hi,              &
@@ -873,6 +873,104 @@ contains
 
   end subroutine fab_physbc_macvel
 
+  pure subroutine fab_physbc_macstress(lo,     hi,               & ! dim(lo) == dim(hi) == 3
+       &                            dom_lo, dom_hi,           &
+       &                            stress, v_lo, v_hi, v_ncomp, & ! dim(v_lo) == dim(v_hi) == 3
+       &                            ngc, dim_fill_ghost, dd)      &
+       &                            bind(C, name="fab_physbc_macstress")
+
+    integer,          intent(in   ) :: lo(3), hi(3), dom_lo(3), dom_hi(3), v_lo(3), v_hi(3), dd
+    integer,          intent(in   ) :: v_ncomp, dim_fill_ghost(2)
+    integer, value,   intent(in   ) :: ngc
+    real(amrex_real), intent(inout) :: stress(v_lo(1):v_hi(1), &
+         &                                 v_lo(2):v_hi(2), v_ncomp)
+
+    ! ** loop indices
+    integer :: i,j,k
+
+    ! ** number of ghost cells to fill in each dimension
+    integer, dimension(3) :: ngc_eff
+
+    ngc_eff(:) = ngc*dim_fill_ghost(:)
+
+    !____________________________________________________________________________
+    ! Apply BC to X faces
+
+    if(dd .ne.  0) then
+    if(lo(1) .eq. dom_lo(1)) then ! lower bound
+       if(bc_lo(1) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = 1, ngc ! always fill the ghost cells at the bc face
+
+                   stress(lo(1)-1+i, j, k, :) = stress(lo(1)-1+i, j, k, :) - stress(lo(1)-i, j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+
+    if(hi(1) .eq. dom_hi(1)) then ! upper bound
+       if(bc_hi(1) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = 1, ngc ! always fill the ghost cells at the bc face
+
+                   stress(hi(1)+1-i, j, k, :) = stress(hi(1)+1-i, j, k, :) - stress(hi(1)+i, j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+    endif
+
+
+    !____________________________________________________________________________
+    ! Apply BC to Y faces
+    if(dd .ne.  1) then
+    if(lo(2) .eq. dom_lo(2)) then ! lower bound
+       if(bc_lo(2) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = 1, ngc ! always fill the ghost cells at the bc face
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, lo(2)-1+j, k, :) = stress(i, lo(2)-1+j, k, :) - stress(i, lo(2)-j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+
+    if(hi(2) .eq. dom_hi(2)) then ! upper bound
+       if(bc_hi(2) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = 1, ngc ! always fill the ghost cells at the bc face
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, hi(2)+1-j, k, :) = stress(i, hi(2)+1-j, k, :) - stress(i, hi(2)+j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+    endif
+
+    !endif
+
+  end subroutine fab_physbc_macstress
+
 #elif (AMREX_SPACEDIM == 3)
 
   pure subroutine fab_physbc_macvel(lo,     hi,               & ! dim(lo) == dim(hi) == 3
@@ -1008,6 +1106,139 @@ contains
     endif
 
   end subroutine fab_physbc_macvel
+
+  pure subroutine fab_physbc_macstress(lo,     hi,               & ! dim(lo) == dim(hi) == 3
+       &                            dom_lo, dom_hi,           &
+       &                            stress, v_lo, v_hi, v_ncomp, & ! dim(v_lo) == dim(v_hi) == 3
+       &                            ngc, dim_fill_ghost, dd)      &
+       &                            bind(C, name="fab_physbc_macstress")
+
+    integer,          intent(in   ) :: lo(3), hi(3), dom_lo(3), dom_hi(3), v_lo(3), v_hi(3), dd
+    integer,          intent(in   ) :: v_ncomp, dim_fill_ghost(3)
+    integer, value,   intent(in   ) :: ngc
+    real(amrex_real), intent(inout) :: stress(v_lo(1):v_hi(1), &
+         &                                 v_lo(2):v_hi(2), &
+         &                                 v_lo(3):v_hi(3), v_ncomp)
+
+    ! ** loop indices
+    integer :: i,j,k
+
+    ! ** number of ghost cells to fill in each dimension
+    integer, dimension(3) :: ngc_eff
+
+    ngc_eff(:) = ngc*dim_fill_ghost(:)
+
+    !____________________________________________________________________________
+    ! Apply BC to X faces
+
+    if(dd .ne.  0) then
+    if(lo(1) .eq. dom_lo(1)) then ! lower bound
+       if(bc_lo(1) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = 1, ngc ! always fill the ghost cells at the bc face
+
+                   stress(lo(1)-1+i, j, k, :) = stress(lo(1)-1+i, j, k, :) - stress(lo(1)-i, j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+
+    if(hi(1) .eq. dom_hi(1)) then ! upper bound
+       if(bc_hi(1) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = 1, ngc ! always fill the ghost cells at the bc face
+
+                   stress(hi(1)+1-i, j, k, :) = stress(hi(1)+1-i, j, k, :) - stress(hi(1)+i, j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+    endif
+
+
+    !____________________________________________________________________________
+    ! Apply BC to Y faces
+    if(dd .ne.  1) then
+    if(lo(2) .eq. dom_lo(2)) then ! lower bound
+       if(bc_lo(2) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = 1, ngc ! always fill the ghost cells at the bc face
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, lo(2)-1+j, k, :) = stress(i, lo(2)-1+j, k, :) - stress(i, lo(2)-j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+
+    if(hi(2) .eq. dom_hi(2)) then ! upper bound
+       if(bc_hi(2) .eq. 2) then ! no slip thermal
+
+          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
+             do j = 1, ngc ! always fill the ghost cells at the bc face
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, hi(2)+1-j, k, :) = stress(i, hi(2)+1-j, k, :) - stress(i, hi(2)+j, k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+    endif
+
+    !____________________________________________________________________________
+    ! Apply BC to Z faces
+    if(dd .ne.  2) then
+    if(lo(3) .eq. dom_lo(3)) then ! lower bound
+       if(bc_lo(3) .eq. 2) then ! no slip thermal
+
+          do k = 1, ngc ! always fill the ghost cells at the bc face
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, j, lo(3)-1+k, :) = stress(i, j, lo(3)-1+k, :) - stress(i, j, lo(3)-k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+
+    if(hi(3) .eq. dom_hi(3)) then ! upper bound
+       if(bc_hi(3) .eq. 2) then ! no slip thermal
+
+          do k = 1, ngc ! always fill the ghost cells at the bc face
+             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
+                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
+
+                   stress(i, j, hi(3)+1-k, :) = stress(i, j, hi(3)+1-k, :) - stress(i, j, hi(3)+k, :)
+
+                end do
+             end do
+          end do
+
+       end if
+    end if
+    endif
+
+  end subroutine fab_physbc_macstress
 
 #endif
 
@@ -1204,137 +1435,6 @@ contains
 
   end subroutine fab_physbc_domainstress
 
-  pure subroutine fab_physbc_macstress(lo,     hi,               & ! dim(lo) == dim(hi) == 3
-       &                            dom_lo, dom_hi,           &
-       &                            stress, v_lo, v_hi, v_ncomp, & ! dim(v_lo) == dim(v_hi) == 3
-       &                            ngc, dim_fill_ghost, dd)      &
-       &                            bind(C, name="fab_physbc_macstress")
 
-    integer,          intent(in   ) :: lo(3), hi(3), dom_lo(3), dom_hi(3), v_lo(3), v_hi(3), dd
-    integer,          intent(in   ) :: v_ncomp, dim_fill_ghost(3)
-    integer, value,   intent(in   ) :: ngc
-    real(amrex_real), intent(inout) :: stress(v_lo(1):v_hi(1), &
-         &                                 v_lo(2):v_hi(2), &
-         &                                 v_lo(3):v_hi(3), v_ncomp)
-
-    ! ** loop indices
-    integer :: i,j,k
-
-    ! ** number of ghost cells to fill in each dimension
-    integer, dimension(3) :: ngc_eff
-
-    ngc_eff(:) = ngc*dim_fill_ghost(:)
-
-    !____________________________________________________________________________
-    ! Apply BC to X faces
-
-    if(dd .ne.  0) then
-    if(lo(1) .eq. dom_lo(1)) then ! lower bound
-       if(bc_lo(1) .eq. 2) then ! no slip thermal
-
-          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
-             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
-                do i = 1, ngc ! always fill the ghost cells at the bc face
-
-                   stress(lo(1)-1+i, j, k, :) = stress(lo(1)-1+i, j, k, :) - stress(lo(1)-i, j, k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-
-    if(hi(1) .eq. dom_hi(1)) then ! upper bound
-       if(bc_hi(1) .eq. 2) then ! no slip thermal
-
-          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
-             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
-                do i = 1, ngc ! always fill the ghost cells at the bc face
-
-                   stress(hi(1)+1-i, j, k, :) = stress(hi(1)+1-i, j, k, :) - stress(hi(1)+i, j, k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-    endif
-
-
-    !____________________________________________________________________________
-    ! Apply BC to Y faces
-    if(dd .ne.  1) then
-    if(lo(2) .eq. dom_lo(2)) then ! lower bound
-       if(bc_lo(2) .eq. 2) then ! no slip thermal
-
-          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
-             do j = 1, ngc ! always fill the ghost cells at the bc face
-                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
-
-                   stress(i, lo(2)-1+j, k, :) = stress(i, lo(2)-1+j, k, :) - stress(i, lo(2)-j, k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-
-    if(hi(2) .eq. dom_hi(2)) then ! upper bound
-       if(bc_hi(2) .eq. 2) then ! no slip thermal
-
-          do k = lo(3)-ngc_eff(3), hi(3)+ngc_eff(3)
-             do j = 1, ngc ! always fill the ghost cells at the bc face
-                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
-
-                   stress(i, hi(2)+1-j, k, :) = stress(i, hi(2)+1-j, k, :) - stress(i, hi(2)+j, k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-    endif
-
-    !____________________________________________________________________________
-    ! Apply BC to Z faces
-    if(dd .ne.  2) then
-    if(lo(3) .eq. dom_lo(3)) then ! lower bound
-       if(bc_lo(3) .eq. 2) then ! no slip thermal
-
-          do k = 1, ngc ! always fill the ghost cells at the bc face
-             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
-                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
-
-                   stress(i, j, lo(3)-1+k, :) = stress(i, j, lo(3)-1+k, :) - stress(i, j, lo(3)-k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-
-    if(hi(3) .eq. dom_hi(3)) then ! upper bound
-       if(bc_hi(3) .eq. 2) then ! no slip thermal
-
-          do k = 1, ngc ! always fill the ghost cells at the bc face
-             do j = lo(2)-ngc_eff(2), hi(2)+ngc_eff(2)
-                do i = lo(1)-ngc_eff(1), hi(1)+ngc_eff(1)
-
-                   stress(i, j, hi(3)+1-k, :) = stress(i, j, hi(3)+1-k, :) - stress(i, j, hi(3)+k, :)
-
-                end do
-             end do
-          end do
-
-       end if
-    end if
-    endif
-
-  end subroutine fab_physbc_macstress
 
 end module multifab_physbc_module
