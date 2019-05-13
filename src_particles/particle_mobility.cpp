1d52cece6cc810d0443fc125d524b3545498d4ba
Daniel Ladiges <drladiges@lbl.gov>
2019-05-13 15:23:06 -0700

    Added Fortran type for species, added more stuff for dry mobility calculation.

--- src_particles/particle_mobility.cpp
+++ src_particles/particle_mobility.cpp
@@ -6,7 +6,7 @@
 //using namespace amrex;
 
 
-void ComputeDryMobility(MultiFab & dryMobility, const Geometry & Geom)
+void ComputeDryMobility(MultiFab & dryMobility, species* particleInfo, const Geometry & Geom)
 {
 
     const Real* dx = Geom.CellSize();
@@ -17,7 +17,7 @@ void ComputeDryMobility(MultiFab & dryMobility, const Geometry & Geom)
     for ( MFIter mfi(dryMobility); mfi.isValid(); ++mfi ) {
 
         const Box& bx = mfi.validbox();
-        compute_dry_mobility(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()), BL_TO_FORTRAN_3D(dryMobility[mfi]), ZFILL(dx), ZFILL(plo), ZFILL(phi), &ngc);
+        compute_dry_mobility(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()), BL_TO_FORTRAN_3D(dryMobility[mfi]), ZFILL(dx), ZFILL(plo), ZFILL(phi), &ngc, particleInfo);
         
     }
 }
