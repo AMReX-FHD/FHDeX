c9cf44c2fc84e00eda8736c7a9b48e944d607972
Daniel Ladiges <drladiges@lbl.gov>
2019-06-20 09:19:52 -0700

    precheck file change for ldt-dsmc

--- exec/DSMC-LDT/precheck.F90
+++ exec/DSMC-LDT/precheck.F90
@@ -8,93 +8,93 @@ module precheck_module
 
 contains        
 
-!  subroutine precheck(part, surfaces, ns, delt, flag, phi, plo)
+  subroutine precheck(part, surfaces, ns, delt, flag, phi, plo)
 
-!    use iso_c_binding, only: c_int
-!    use cell_sorted_particle_module, only: particle_t
-!    use surfaces_module, only: surface_t
+    use iso_c_binding, only: c_int
+    use cell_sorted_particle_module, only: particle_t
+    use surfaces_module, only: surface_t
 
-!    type(particle_t), intent(inout) :: part
-!    type(surface_t), intent(in), target :: surfaces(ns)
-!    real(amrex_real), intent(in) :: delt, phi(3), plo(3)
-!    integer(c_int), intent(inout) :: flag
-!    integer(c_int), intent(in) :: ns
-!#if (BL_SPACEDIM == 3)
-!    double precision proj(3), box1lo(3), box1hi(3), box2lo(3), box2hi(3)
-!#endif
-!#if (BL_SPACEDIM == 2)
-!    double precision proj(2), box1lo(2), box1hi(2), box2lo(2), box2hi(2)
-!#endif
+    type(particle_t), intent(inout) :: part
+    type(surface_t), intent(in), target :: surfaces(ns)
+    real(amrex_real), intent(in) :: delt, phi(3), plo(3)
+    integer(c_int), intent(inout) :: flag
+    integer(c_int), intent(in) :: ns
+#if (BL_SPACEDIM == 3)
+    double precision proj(3), box1lo(3), box1hi(3), box2lo(3), box2hi(3)
+#endif
+#if (BL_SPACEDIM == 2)
+    double precision proj(2), box1lo(2), box1hi(2), box2lo(2), box2hi(2)
+#endif
 
-!#if (BL_SPACEDIM == 3)
-!    proj = part%pos + part%vel*delt
+#if (BL_SPACEDIM == 3)
+    proj = part%pos + part%vel*delt
+
+    box1lo = plo
+    box1hi = (/ surfaces(7)%x0 , phi(2) , phi(3) /)
+
+    box2lo = (/ surfaces(7)%x0 , plo(2) , plo(3) /)
+    box2hi = phi
+
 
 !    box1lo = plo
-!    box1hi = (/ surfaces(7)%x0 , phi(2) , phi(3) /)
+!    box1hi = (/ phi(1)*0.5 , phi(2) , phi(3) /)
 
-!    box2lo = (/ surfaces(7)%x0 , plo(2) , plo(3) /)
+!    box2lo = (/ phi(1)*0.5 , plo(2) , plo(3) /)
 !    box2hi = phi
+#endif
 
+#if (BL_SPACEDIM == 2)
+    proj(1) = part%pos(1) + part%vel(1)*delt
+    proj(2) = part%pos(2) + part%vel(2)*delt
 
-!!    box1lo = plo
-!!    box1hi = (/ phi(1)*0.5 , phi(2) , phi(3) /)
+    box1lo(1) = plo(1)
+    box1lo(2) = plo(2)
 
-!!    box2lo = (/ phi(1)*0.5 , plo(2) , plo(3) /)
-!!    box2hi = phi
-!#endif
+    box1hi = (/ surfaces(5)%x0 , phi(2) /)
 
-!#if (BL_SPACEDIM == 2)
-!    proj(1) = part%pos(1) + part%vel(1)*delt
-!    proj(2) = part%pos(2) + part%vel(2)*delt
+    box2lo = (/ surfaces(5)%x0 , plo(2) /)
 
-!    box1lo(1) = plo(1)
-!    box1lo(2) = plo(2)
+    box2hi(1) = phi(1)
+    box2hi(2) = phi(2)
+#endif
 
-!    box1hi = (/ surfaces(5)%x0 , phi(2) /)
+#if (BL_SPACEDIM == 3)
+    if(  (part%pos(1) < box1hi(1)) .and. (part%pos(2) < box1hi(2)) .and. (part%pos(3) < box1hi(3)) .and. (part%pos(1) > box1lo(1)) .and. (part%pos(2) > box1lo(2)) .and. (part%pos(3) > box1lo(3))  ) then  !started in box1
 
-!    box2lo = (/ surfaces(5)%x0 , plo(2) /)
+      if(  (proj(1) < box1hi(1)) .and. (proj(2) < box1hi(2)) .and. (proj(3) < box1hi(3)) .and. (proj(1) > box1lo(1)) .and. (proj(2) > box1lo(2)) .and. (proj(3) > box1lo(3)) ) then  !ended in box 1
 
-!    box2hi(1) = phi(1)
-!    box2hi(2) = phi(2)
-!#endif
+        flag = 1
 
-!#if (BL_SPACEDIM == 3)
-!    if(  (part%pos(1) < box1hi(1)) .and. (part%pos(2) < box1hi(2)) .and. (part%pos(3) < box1hi(3)) .and. (part%pos(1) > box1lo(1)) .and. (part%pos(2) > box1lo(2)) .and. (part%pos(3) > box1lo(3))  ) then  !started in box1
+      endif
 
-!      if(  (proj(1) < box1hi(1)) .and. (proj(2) < box1hi(2)) .and. (proj(3) < box1hi(3)) .and. (proj(1) > box1lo(1)) .and. (proj(2) > box1lo(2)) .and. (proj(3) > box1lo(3)) ) then  !ended in box 1
+    else  !started in box 2
 
-!        flag = 1
+      if(  (proj(1) < box2hi(1)) .and. (proj(2) < box2hi(2)) .and. (proj(3) < box2hi(3)) .and. (proj(1) > box2lo(1)) .and. (proj(2) > box2lo(2)) .and. (proj(3) > box2lo(3)) ) then  !ended in box 2
 
-!      endif
+        flag = 1
 
-!    else  !started in box 2
+      endif
+    endif    
+#endif
+#if (BL_SPACEDIM == 2)
+    if(  (part%pos(1) < box1hi(1)) .and. (part%pos(2) < box1hi(2)) .and. (part%pos(1) > box1lo(1)) .and. (part%pos(2) > box1lo(2)) ) then  !started in box1
 
-!      if(  (proj(1) < box2hi(1)) .and. (proj(2) < box2hi(2)) .and. (proj(3) < box2hi(3)) .and. (proj(1) > box2lo(1)) .and. (proj(2) > box2lo(2)) .and. (proj(3) > box2lo(3)) ) then  !ended in box 2
+      if(  (proj(1) < box1hi(1)) .and. (proj(2) < box1hi(2)) .and. (proj(1) > box1lo(1)) .and. (proj(2) > box1lo(2)) ) then  !ended in box 1
 
-!        flag = 1
+        flag = 1
 
-!      endif
-!    endif    
-!#endif
-!#if (BL_SPACEDIM == 2)
-!    if(  (part%pos(1) < box1hi(1)) .and. (part%pos(2) < box1hi(2)) .and. (part%pos(1) > box1lo(1)) .and. (part%pos(2) > box1lo(2)) ) then  !started in box1
+      endif
 
-!      if(  (proj(1) < box1hi(1)) .and. (proj(2) < box1hi(2)) .and. (proj(1) > box1lo(1)) .and. (proj(2) > box1lo(2)) ) then  !ended in box 1
+    else  !started in box 2
 
-!        flag = 1
+      if(  (proj(1) < box2hi(1)) .and. (proj(2) < box2hi(2)) .and. (proj(1) > box2lo(1)) .and. (proj(2) > box2lo(2))) then  !ended in box 2
 
-!      endif
+        flag = 1
 
-!    else  !started in box 2
-
-!      if(  (proj(1) < box2hi(1)) .and. (proj(2) < box2hi(2)) .and. (proj(1) > box2lo(1)) .and. (proj(2) > box2lo(2))) then  !ended in box 2
-
-!        flag = 1
-
-!      endif
-!    endif
-!#endif
-!  end subroutine precheck
+      endif
+    endif
+#endif
+  end subroutine precheck
 
   subroutine precheck(part, surfaces, ns, delt, flag, phi, plo)
 
