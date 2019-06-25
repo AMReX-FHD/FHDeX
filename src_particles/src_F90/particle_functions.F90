fe4f9fe6c0f90c509beea5b20cade91c3e4bad7d
Daniel Ladiges <drladiges@lbl.gov>
2019-06-25 16:32:08 -0700

    LM HYdro debugging

--- src_particles/src_F90/particle_functions.F90
+++ src_particles/src_F90/particle_functions.F90
@@ -2195,7 +2195,7 @@ subroutine move_ions_fhd(particles, np, lo, hi, &
 
            do while (p <= new_np)
 
-              runtime = dt
+              
               part => particles(cell_parts(p))
 
               !Get peskin kernel weights. Weights are stored in 'weights', indicies contains the indicies to which the weights are applied.
@@ -2223,6 +2223,7 @@ subroutine move_ions_fhd(particles, np, lo, hi, &
               if(move_tog .eq. 2) then !mid point time stepping - First step 1/2 a time step then interpolate velocity field
 
                 posold = part%pos
+
                 runtime = dt*0.5
 
                 do while (runtime .gt. 0)
@@ -2282,9 +2283,10 @@ subroutine move_ions_fhd(particles, np, lo, hi, &
                                   velz, velzlo, velzhi, &
 #endif
                                   part, ks, dxf, boundflag, midpoint, rejected)
+
+                part%pos = posold
               endif
 
-              part%pos = posold
               runtime = dt
 
               if (dry_move_tog .eq. 1) then
@@ -2359,9 +2361,11 @@ subroutine move_ions_fhd(particles, np, lo, hi, &
 
 !!!!!!!!!! Mean square displacement measurer.
 
-              !print *, part%pos(1)
+              !print *, "Before: ", part%abspos
 
               part%abspos = part%abspos + dt*part%vel
+
+              !print *, "After: ", part%abspos
 
               dist = sqrt(dot_product(dt*part%vel,dt*part%vel))/part%radius
 
@@ -2374,6 +2378,8 @@ subroutine move_ions_fhd(particles, np, lo, hi, &
 !              distav = distav + dt*sqrt(part%vel(1)**2+part%vel(2)**2+part%vel(3)**2)
 
               part%travel_time = part%travel_time + dt
+
+              !print *, "tt: ", part%travel_time
 
               norm = part%abspos
 
