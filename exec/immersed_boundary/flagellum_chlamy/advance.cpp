6ee2b4a14f0522ac0740ea84a944f031c6758aec
Gang Xu <gangxu@lbl.gov>
2019-08-08 14:32:36 -0700

    updated according to flagellum folder. Not working yet.

--- exec/immersed_boundary/flagellum_chlamy/advance.cpp
+++ exec/immersed_boundary/flagellum_chlamy/advance.cpp
@@ -13,6 +13,8 @@
 
 #include "gmres_namespace.H"
 
+#include <immbdy_namespace.H>
+
 #include <AMReX_ParallelDescriptor.H>
 #include <AMReX_MultiFabUtil.H>
 
@@ -24,6 +26,8 @@ using namespace amrex;
 using namespace common;
 using namespace gmres;
 using namespace immbdy_md;
+using namespace immbdy;
+using namespace ib_flagellum;
 
 
 using ParticleVector = typename IBMarkerContainer::ParticleVector;
@@ -163,22 +167,19 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
     int ib_lev = 0;
 
     // Parameters for spring force calculation
-    Real spr_k = 100000.0 ; // spring constant
-
-    // initial distance btw markers. TODO: Need to update depending on initial
-    // coordinates.
-    Real l_db = 0.025;
+    Real spr_k = 10000.0 ; // spring constant
 
     // Parameters for calling bending force calculation
-    Real driv_k = 100000.0; //bending stiffness
+    Real driv_k = 10000.0; //bending stiffness
     RealVect driv_u = {0, 0, 1};
 
     //Real driv_period = 100;  //This is actually angular frequency =  2*pi/T
-    Real driv_period = 1.33/2.0;  // This is 50 times larger than the actual period
-    Real length_flagellum = 0.5;
-    //Real driv_amp = 15 * std::min(time*10, 1.);
-    Real driv_amp = 15.0 * std::min(time*100, 1.);
-    Print() << "driv_amp = " << driv_amp << std::endl;
+    //Real driv_period = 1.33/2.0;  // This is 50 times larger than the actual period
+    //Real length_flagellum = 0.5;
+
+    // slowly ramp up driving amplitude
+    Real driv_amp = 1.; //std::min(time*10, 1.);
+    //Print() << "driv_amp = " << driv_amp << std::endl;
 
 
     /****************************************************************************
@@ -332,6 +333,15 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
 
             ParticleType & mark = markers[i];
 
+            int i_ib        = mark.idata(IBM_intData::cpu_1);
+            int N           = ib_flagellum::n_marker[i_ib];
+            Real L          = ib_flagellum::length[i_ib];
+            Real wavelength = ib_flagellum::wavelength[i_ib];
+            Real frequency  = ib_flagellum::frequency[i_ib];
+            Real amplitude  = ib_flagellum::amplitude[i_ib];
+            Real l_link     = L/(N-1);
+
+
             // Get previous and next markers connected to current marker (if they exist)
             ParticleType * next_marker = NULL;
             ParticleType * prev_marker = NULL;
@@ -359,7 +369,7 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                 }
 
                 Real lp_m = r_m.vectorLength(),       lp_p = r_p.vectorLength();
-                Real fm_0 = spr_k * (lp_m-l_db)/lp_m, fp_0 = spr_k * (lp_p-l_db)/lp_p;
+                Real fm_0 = spr_k * (lp_m-l_link)/lp_m, fp_0 = spr_k * (lp_p-l_link)/lp_p;
 
                 for (int d=0; d<AMREX_SPACEDIM; ++d) {
                     prev_marker->rdata(IBM_realData::pred_forcex + d) += fm_0 * r_m[d];
@@ -393,8 +403,8 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
 
                 // calling the active bending force calculation
                 // This a simple since wave imposed
-                //Real theta = l_db*driv_amp*sin(driv_period*time
-                //             + 2*M_PI/length_flagellum*mark.idata(IBM_intData::id_1)*l_db);
+                //Real theta = l_link*driv_amp*sin(driv_period*time
+                //             + 2*M_PI/length_flagellum*mark.idata(IBM_intData::id_1)*l_link);
 
 
                 //Fourier series parameters based on normalized axial location (0.06-0.96)
@@ -408,15 +418,15 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
 		Real a3 = -0.2303*s*s*s*s + 0.5034*s*s*s - 0.5192*s*s + 0.268*s - 0.0419;
 		Real b3 = 0.1073*s*s*s*s - 0.1903*s*s*s + 0.1145*s*s + 0.0115*s - 0.022;
 
-                Real w  = 2*M_PI; //for normailized beat period of 1
+                Real w  = 0.; //2*M_PI; //for normailized beat period of 1
 
-                Real T = driv_period;  // T=13.3 ms for 75.2Hz beating
+                //Real T = driv_period;  // T=13.3 ms for 75.2Hz beating
 
-                Real theta_raw = a0 + a1*cos(w*time/T)   + b1*sin(w*time/T)
-                                    + a2*cos(2*w*time/T) + b2*sin(2*w*time/T)
-                                    + a3*cos(3*w*time/T) + b3*sin(3*w*time/T);
+                Real theta_raw = a0 + a1*cos(w*time*frequency)   + b1*sin(w*time*frequency)
+                                    + a2*cos(2*w*time*frequency) + b2*sin(2*w*time*frequency)
+                                    + a3*cos(3*w*time*frequency) + b3*sin(3*w*time*frequency);
 
-                Real theta = l_db*driv_amp*theta_raw;
+                Real theta = l_link*driv_amp*amplitude*theta_raw;
 
 		//std::cout << "predictor theta = " << theta << std::endl;
 
@@ -650,6 +660,15 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
 
             ParticleType & mark = markers[i];
 
+            int i_ib        = mark.idata(IBM_intData::cpu_1);
+            int N           = ib_flagellum::n_marker[i_ib];
+            Real L          = ib_flagellum::length[i_ib];
+            Real wavelength = ib_flagellum::wavelength[i_ib];
+            Real amplitude  = ib_flagellum::amplitude[i_ib];
+            Real frequency  = ib_flagellum::frequency[i_ib];
+            Real l_link     = L/N;
+
+
             // Get previous and next markers connected to current marker (if they exist)
             ParticleType * next_marker = NULL;
             ParticleType * prev_marker = NULL;
@@ -676,7 +695,7 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                 }
 
                 Real lp_m = r_m.vectorLength(),       lp_p = r_p.vectorLength();
-                Real fm_0 = spr_k * (lp_m-l_db)/lp_m, fp_0 = spr_k * (lp_p-l_db)/lp_p;
+                Real fm_0 = spr_k * (lp_m-l_link)/lp_m, fp_0 = spr_k * (lp_p-l_link)/lp_p;
 
                 for (int d=0; d<AMREX_SPACEDIM; ++d) {
                     prev_marker->rdata(IBM_realData::forcex + d) += fm_0 * r_m[d];
@@ -708,8 +727,8 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                 RealVect f_m = RealVect{0., 0., 0.};
 
                 // calling the active bending force calculation
-                //Real theta = l_db*driv_amp*sin(driv_period*time
-                //            + 2*M_PI/length_flagellum*mark.idata(IBM_intData::id_1)*l_db);
+                //Real theta = l_link*driv_amp*sin(driv_period*time
+                //            + 2*M_PI/length_flagellum*mark.idata(IBM_intData::id_1)*l_link);
 
                 //Fourier series parameters based on normalized axial location (0.06-0.96)
                 Real s = mark.idata(IBM_intData::id_1)/20+0.01; //note: id_1 = 0 for first marker created
@@ -722,15 +741,15 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                 Real a3 = -0.2303*s*s*s*s + 0.5034*s*s*s - 0.5192*s*s + 0.268*s - 0.0419;
                 Real b3 = 0.1073*s*s*s*s - 0.1903*s*s*s + 0.1145*s*s + 0.0115*s - 0.022;
 
-                Real w  = 2*M_PI; //for normailized beat period of 1
+                Real w  = 0.; // 2*M_PI; //for normailized beat period of 1
 
-                Real T = driv_period;  // T=13.3 ms for 75.2Hz beating
+                //Real T = driv_period;  // T=13.3 ms for 75.2Hz beating
 
-                Real theta_raw = a0 + a1*cos(w*time/T)   + b1*sin(w*time/T)
-                                    + a2*cos(2*w*time/T) + b2*sin(2*w*time/T)
-                                    + a3*cos(3*w*time/T) + b3*sin(3*w*time/T);
+                Real theta_raw = a0 + a1*cos(w*time*frequency)   + b1*sin(w*time*frequency)
+                                    + a2*cos(2*w*time*frequency) + b2*sin(2*w*time*frequency)
+                                    + a3*cos(3*w*time*frequency) + b3*sin(3*w*time*frequency);
 
-                Real theta = l_db*driv_amp*theta_raw;
+                Real theta = l_link*driv_amp*amplitude*theta_raw;
 
                 driving_f(f, f_p, f_m, r, r_p, r_m, driv_u, theta, driv_k);
 
