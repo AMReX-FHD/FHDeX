3a9d2eb8c75a96cddacff901458603b720ef3403
Andy Nonaka <AJNonaka@lbl.gov>
2019-08-15 09:18:07 -0700

    stag_apply_op: visc_type=-2 on GPU

--- src_gmres/StagApplyOp.cpp
+++ src_gmres/StagApplyOp.cpp
@@ -441,6 +441,204 @@ void stag_applyop_visc_p2 (Box const& tbx,
 
 }
 
+AMREX_GPU_HOST_DEVICE
+inline
+void stag_applyop_visc_m2 (Box const& tbx,
+			   AMREX_D_DECL(Box const& xbx,
+					Box const& ybx,
+					Box const& zbx),
+                           AMREX_D_DECL(Array4<Real const> const& alphax,
+					Array4<Real const> const& alphay,
+					Array4<Real const> const& alphaz),
+                           AMREX_D_DECL(Array4<Real const> const& phix,
+					Array4<Real const> const& phiy,
+					Array4<Real const> const& phiz),
+                           AMREX_D_DECL(Array4<Real> const& Lphix,
+					Array4<Real> const& Lphiy,
+					Array4<Real> const& Lphiz),
+                           Array4<Real const> const& betacc,
+                           Array4<Real const> const& betaxy,
+#if (AMREX_SPACEDIM == 3)
+                           Array4<Real const> const& betaxz,
+                           Array4<Real const> const& betayz,
+#endif
+			   AMREX_D_DECL(bool do_x,
+					bool do_y,
+					bool do_z),
+			   Real bt,  Real gt, int offset,  int color,
+			   const GpuArray<Real, AMREX_SPACEDIM> & dx) noexcept
+{
+    // xbx, ybx, and zbx are the face-centered boxes
+
+    // if running on the host
+    // tlo is the minimal box containins the union of the face-centered grid boxes
+
+    // if running on the gpu, tlo is a box with a single point that comes
+    // from the union of the face-centered grid boxes
+
+    const auto tlo = lbound(tbx);
+    const auto thi = ubound(tbx);
+
+    // if running on the host, x/y/zlo and x/y/zhi are set to
+    // the lower/uppser bounds of x/y/zbx
+
+    // if running on the gpu, x/y/zlo and x/y/zhi are set to
+    // the single point defined by tlo, unless tlo is outside of the union
+    // of the face-centered grid boxes, in which case they are set to
+    // values that make sure the loop is not entered
+
+    AMREX_D_TERM(const auto xlo = amrex::elemwiseMax(tlo, lbound(xbx));,
+                 const auto ylo = amrex::elemwiseMax(tlo, lbound(ybx));,
+                 const auto zlo = amrex::elemwiseMax(tlo, lbound(zbx)););
+
+    AMREX_D_TERM(const auto xhi = amrex::elemwiseMin(thi, ubound(xbx));,
+                 const auto yhi = amrex::elemwiseMin(thi, ubound(ybx));,
+                 const auto zhi = amrex::elemwiseMin(thi, ubound(zbx)););
+
+    int ioff;
+
+    Real dxsqinv = 1./(dx[0]*dx[0]);
+    Real dysqinv = 1./(dx[1]*dx[1]);
+    Real dxdyinv = 1./(dx[0]*dx[1]);
+#if (AMREX_SPACEDIM == 3)
+    Real dzsqinv = 1./(dx[2]*dx[2]);
+    Real dxdzinv = 1./(dx[0]*dx[2]);
+    Real dydzinv = 1./(dx[1]*dx[2]);
+#endif
+
+    if (do_x) {
+
+        for (int k = xlo.z; k <= xhi.z; ++k) {
+        for (int j = xlo.y; j <= xhi.y; ++j) {
+        ioff = 0;
+	if (offset == 2 && (xlo.x+j+k)%2 != (color+1)%2 ) {
+	  ioff = 1;
+	}
+        AMREX_PRAGMA_SIMD
+        for (int i = xlo.x+ioff; i <= xhi.x; i+=offset) {
+                   
+            Lphix(i,j,k) = phix(i,j,k)*
+                ( alphax(i,j,k) +
+                  2.*(betacc(i,j,k)+betacc(i-1,j,k))*dxsqinv
+                  + (betaxy(i,j,k)+betaxy(i,j+1,k))*dysqinv
+#if (AMREX_SPACEDIM == 3)
+                  + (betaxz(i,j,k)+betaxz(i,j,k+1))*dzsqinv
+#endif
+                    )
+                        
+                -2.*phix(i+1,j,k)*betacc(i,j,k)*dxsqinv
+                -2.*phix(i-1,j,k)*betacc(i-1,j,k)*dxsqinv
+                -phix(i,j+1,k)*betaxy(i,j+1,k)*dysqinv
+                -phix(i,j-1,k)*betaxy(i,j,k)*dysqinv
+#if (AMREX_SPACEDIM == 3)
+                -phix(i,j,k+1)*betaxz(i,j,k+1)*dzsqinv
+                -phix(i,j,k-1)*betaxz(i,j,k)*dzsqinv
+#endif
+                        
+                -phiy(i,j+1,k)*betaxy(i,j+1,k)*dxdyinv
+                +phiy(i,j,k)*betaxy(i,j,k)*dxdyinv
+                +phiy(i-1,j+1,k)*betaxy(i,j+1,k)*dxdyinv
+                -phiy(i-1,j,k)*betaxy(i,j,k)*dxdyinv
+                        
+#if (AMREX_SPACEDIM == 3)
+                -phiz(i,j,k+1)*betaxz(i,j,k+1)*dxdzinv
+                +phiz(i,j,k)*betaxz(i,j,k)*dxdzinv
+                +phiz(i-1,j,k+1)*betaxz(i,j,k+1)*dxdzinv
+                -phiz(i-1,j,k)*betaxz(i,j,k)*dxdzinv
+#endif
+                ;            
+        }
+        }
+        }
+    }
+
+    if (do_y) {
+
+        for (int k = ylo.z; k <= yhi.z; ++k) {
+        for (int j = ylo.y; j <= yhi.y; ++j) {
+        ioff = 0;
+        if (offset == 2 && (ylo.x+j+k)%2 != (color+1)%2 ) {
+	  ioff = 1;
+	}
+        AMREX_PRAGMA_SIMD
+        for (int i = ylo.x+ioff; i <= yhi.x; i+=offset) {
+                   
+            Lphiy(i,j,k) = phiy(i,j,k)*
+                ( alphay(i,j,k) +
+                  2.*(betacc(i,j,k)+betacc(i,j-1,k))*dysqinv
+                  + (betaxy(i,j,k)+betaxy(i+1,j,k))*dxsqinv
+#if (AMREX_SPACEDIM == 3)
+                  + (betayz(i,j,k)+betayz(i,j,k+1))*dzsqinv
+#endif
+                    )
+                        
+                -2.*phiy(i,j+1,k)*betacc(i,j,k)*dysqinv
+                -2.*phiy(i,j-1,k)*betacc(i,j-1,k)*dysqinv
+                -phiy(i+1,j,k)*betaxy(i+1,j,k)*dxsqinv
+                -phiy(i-1,j,k)*betaxy(i,j,k)*dxsqinv
+#if (AMREX_SPACEDIM == 3)
+                -phiy(i,j,k+1)*betayz(i,j,k+1)*dzsqinv
+                -phiy(i,j,k-1)*betayz(i,j,k)*dzsqinv
+#endif
+                        
+                -phix(i+1,j,k)*betaxy(i+1,j,k)*dxdyinv
+                +phix(i,j,k)*betaxy(i,j,k)*dxdyinv
+                +phix(i+1,j-1,k)*betaxy(i+1,j,k)*dxdyinv
+                -phix(i,j-1,k)*betaxy(i,j,k)*dxdyinv
+                        
+#if (AMREX_SPACEDIM == 3)
+                -phiz(i,j,k+1)*betayz(i,j,k+1)*dydzinv
+                +phiz(i,j,k)*betayz(i,j,k)*dydzinv
+                +phiz(i,j-1,k+1)*betayz(i,j,k+1)*dydzinv
+                -phiz(i,j-1,k)*betayz(i,j,k)*dydzinv
+#endif
+                ;
+        }
+        }
+        }
+    }
+
+#if (AMREX_SPACEDIM == 3)
+    if (do_z) {
+
+        for (int k = zlo.z; k <= zhi.z; ++k) {
+        for (int j = zlo.y; j <= zhi.y; ++j) {
+        ioff = 0;
+        if (offset == 2 && (zlo.x+j+k)%2 != (color+1)%2 ) {
+	  ioff = 1;
+	}
+        AMREX_PRAGMA_SIMD
+        for (int i = zlo.x+ioff; i <= zhi.x; i+=offset) {
+
+            Lphiz(i,j,k) = phiz(i,j,k)*
+                ( alphaz(i,j,k) +
+                  2.*(betacc(i,j,k)+betacc(i,j,k-1))*dzsqinv
+                  + (betaxz(i,j,k)+betaxz(i+1,j,k))*dxsqinv
+                  + (betayz(i,j,k)+betayz(i,j+1,k))*dysqinv )
+                        
+                -2.*phiz(i,j,k+1)*betacc(i,j,k)*dzsqinv
+                -2.*phiz(i,j,k-1)*betacc(i,j,k-1)*dzsqinv
+                -phiz(i+1,j,k)*betaxz(i+1,j,k)*dxsqinv
+                -phiz(i-1,j,k)*betaxz(i,j,k)*dxsqinv
+                -phiz(i,j+1,k)*betayz(i,j+1,k)*dysqinv
+                -phiz(i,j-1,k)*betayz(i,j,k)*dysqinv
+                        
+                -phix(i+1,j,k)*betaxz(i+1,j,k)*dxdzinv
+                +phix(i,j,k)*betaxz(i,j,k)*dxdzinv
+                +phix(i+1,j,k-1)*betaxz(i+1,j,k)*dxdzinv
+                -phix(i,j,k-1)*betaxz(i,j,k)*dxdzinv
+                        
+                -phiy(i,j+1,k)*betayz(i,j+1,k)*dydzinv
+                +phiy(i,j,k)*betayz(i,j,k)*dydzinv
+                +phiy(i,j+1,k-1)*betayz(i,j+1,k)*dydzinv
+                -phiy(i,j,k-1)*betayz(i,j,k)*dydzinv;            
+        }
+        }
+        }
+    }
+#endif
+}
+
 void StagApplyOp(const MultiFab& beta_cc,
                  const MultiFab& gamma_cc,
                  const std::array<MultiFab, NUM_EDGE>& beta_ed,
@@ -572,33 +770,21 @@ void StagApplyOp(const MultiFab& beta_cc,
                                      bt, gt, offset, color, dx_gpu);
             });
         }
-        else {
-            stag_apply_op(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
-                          BL_TO_FORTRAN_ANYD(beta_cc[mfi]),
-                          BL_TO_FORTRAN_ANYD(gamma_cc[mfi]),
-                          BL_TO_FORTRAN_ANYD(beta_ed[0][mfi]),
+        else if (visc_type == -2) {
+            AMREX_LAUNCH_HOST_DEVICE_LAMBDA(index_bounds, tbx,
+            {
+                stag_applyop_visc_m2(tbx, AMREX_D_DECL(bx_x,bx_y,bx_z),
+                                     AMREX_D_DECL(alphax_fab,alphay_fab,alphaz_fab),
+                                     AMREX_D_DECL(phix_fab,phiy_fab,phiz_fab),
+                                     AMREX_D_DECL(Lphix_fab,Lphiy_fab,Lphiz_fab),
+                                     beta_cc_fab, beta_xy_fab,
 #if (AMREX_SPACEDIM == 3)
-                          BL_TO_FORTRAN_ANYD(beta_ed[1][mfi]),
-                          BL_TO_FORTRAN_ANYD(beta_ed[2][mfi]),
-#endif
-                          BL_TO_FORTRAN_ANYD(phi[0][mfi]),
-                          BL_TO_FORTRAN_ANYD(phi[1][mfi]),
-#if (AMREX_SPACEDIM == 3)
-                          BL_TO_FORTRAN_ANYD(phi[2][mfi]),
-#endif
-                          BL_TO_FORTRAN_ANYD(Lphi[0][mfi]),
-                          BL_TO_FORTRAN_ANYD(Lphi[1][mfi]),
-#if (AMREX_SPACEDIM == 3)
-                          BL_TO_FORTRAN_ANYD(Lphi[2][mfi]),
-#endif
-                          BL_TO_FORTRAN_ANYD(alpha_fc[0][mfi]),
-                          BL_TO_FORTRAN_ANYD(alpha_fc[1][mfi]),
-#if (AMREX_SPACEDIM == 3)
-                          BL_TO_FORTRAN_ANYD(alpha_fc[2][mfi]),
-#endif
-                          dx, &color);
+                                     beta_xz_fab, beta_yz_fab,
+#endif                                 
+                                     AMREX_D_DECL(do_x,do_y,do_z),
+                                     bt, gt, offset, color, dx_gpu);
+            });            
         }
-
     }
 
     // divide alpha by theta_alpha
