f3a39899a7c8856593c0de48e4a7c9f65d7474fb
Johannes Blaschke <jpblaschke@lbl.gov>
2019-08-15 11:20:40 -0700

    searching for why flagellum is moving the wrong way

--- exec/immersed_boundary/flagellum/advance.cpp
+++ exec/immersed_boundary/flagellum/advance.cpp
@@ -336,11 +336,11 @@ void advance(std::array< MultiFab, AMREX_SPACEDIM >& umac,
     RealVect driv_u = {0, 0, 1};
 
     // Slowly ramp up driving amplitude
-    // Real driv_amp = std::min(time*10, 1.);
-    // Print() << "driv_amp = " << driv_amp << std::endl;
-    //
+    Real driv_amp = std::min(time*10, 1.);
+    Print() << "driv_amp = " << driv_amp << std::endl;
+
     // I'm too impatient to wait... -JPB
-    Real driv_amp = 1.;
+    // Real driv_amp = 1.;
 
 
 
