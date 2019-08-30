cab32a9ad89fada73aaf177d444e776b8144cce1
Daniel Ladiges <drladiges@lbl.gov>
2019-08-29 17:46:22 -0700

    ions, membrane, & DSMC now use 'seed' input variable for rngs

--- exec/immersedIons/main_driver.cpp
+++ exec/immersedIons/main_driver.cpp
@@ -66,19 +66,22 @@ void main_driver(const char* argv)
 
     const int n_rngs = 1;
 
-    int fhdSeed = ParallelDescriptor::MyProc() + 1;
-    int particleSeed = 2*ParallelDescriptor::MyProc() + 2;
-    int selectorSeed = 3*ParallelDescriptor::MyProc() + 3;
-    int thetaSeed = 4*ParallelDescriptor::MyProc() + 4;
-    int phiSeed = 5*ParallelDescriptor::MyProc() + 5;
-    int generalSeed = 6*ParallelDescriptor::MyProc() + 6;
+    int fhdSeed = 0;
+    int particleSeed = 0;
+    int selectorSeed = 0;
+    int thetaSeed = 0;
+    int phiSeed = 0;
+    int generalSeed = 0;
 
-//    int fhdSeed = 0;
-//    int particleSeed = 0;
-//    int selectorSeed = 0;
-//    int thetaSeed = 0;
-//    int phiSeed = 0;
-//    int generalSeed = 0;
+    if(seed > 0)
+    {
+        fhdSeed = ParallelDescriptor::MyProc() + 1 + seed;
+        particleSeed = 2*ParallelDescriptor::MyProc() + 2 + seed;
+        selectorSeed = 3*ParallelDescriptor::MyProc() + 3 + seed;
+        thetaSeed = 4*ParallelDescriptor::MyProc() + 4 + seed;
+        phiSeed = 5*ParallelDescriptor::MyProc() + 5 + seed;
+        generalSeed = 6*ParallelDescriptor::MyProc() + 6 + seed;
+    }
 
     //Initialise rngs
     rng_initialize(&fhdSeed,&particleSeed,&selectorSeed,&thetaSeed,&phiSeed,&generalSeed);
