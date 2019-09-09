cdd3ab0efc1022b8413c4121ac77cf7e4026d2c5
Andy Nonaka <AJNonaka@lbl.gov>
2019-09-09 12:56:49 -0700

    more code cleanup

--- src_compressible/src_F90/init_consvar.F90
+++ src_compressible/src_F90/init_consvar.F90
@@ -1,17 +1,17 @@
-subroutine init_consvar(lo, hi, cu, culo, cuhi, pu, pulo, puhi, dx &
-     , reallo, realhi) bind(C, name="init_consvar")
+subroutine init_consvar(lo, hi, cu, culo, cuhi, pu, pulo, puhi, dx, &
+                        reallo, realhi) bind(C, name="init_consvar")
 
   use amrex_fort_module, only : amrex_real
   use common_namelist_module, only : ngc, nvars, nprimvars, nspecies, &
-       n_cells, prob_type, molmass, Runiv, grav, membrane_cell
+                                     n_cells, prob_type, molmass, Runiv, grav, membrane_cell
   use compressible_namelist_module, only : bc_Yk
   use conv_module, only : get_energy, get_pressure_gas
 
   implicit none
 
   integer         , intent(in   ) :: lo(3), hi(3), culo(3), cuhi(3), pulo(3), puhi(3)
-  real(amrex_real), intent(inout) :: cu(culo(1):cuhi(1),culo(2):cuhi(2),culo(3):cuhi(3), nvars)
-  real(amrex_real), intent(in   ) :: pu(pulo(1):puhi(1),pulo(2):puhi(2),pulo(3):puhi(3), nprimvars)
+  real(amrex_real), intent(inout) :: cu(culo(1):cuhi(1),culo(2):cuhi(2),culo(3):cuhi(3),nvars)
+  real(amrex_real), intent(in   ) :: pu(pulo(1):puhi(1),pulo(2):puhi(2),pulo(3):puhi(3),nprimvars)
   real(amrex_real), intent(in   ) :: dx(3)
   real(amrex_real), intent(in   ) :: reallo(3), realhi(3)
 
@@ -21,70 +21,70 @@ subroutine init_consvar(lo, hi, cu, culo, cuhi, pu, pulo, puhi, dx &
   double precision :: massvec(nspecies), intEnergy, pamb, molmix, rgasmix, alpha
   double precision :: Ygrad
 
-  center = (realhi - reallo)/2d0
-  L_hlf = (realhi(1) - reallo(1))/2d0
+  center = (realhi - reallo)/2.d0
+  L_hlf = (realhi(1) - reallo(1))/2.d0
 
   pi = acos(-1.d0)
 
   do k = lo(3), hi(3)
-     do j = lo(2), hi(2)
-        do i = lo(1), hi(1)
+  do j = lo(2), hi(2)
+  do i = lo(1), hi(1)
+     
+     itVec(1) = (dble(i)+0.5)*dx(1)
+     itVec(2) = (dble(j)+0.5)*dx(2)
+     itVec(3) = (dble(k)+0.5)*dx(3)
 
-           itVec(1) = (dble(i)+0.5)*dx(1)
-           itVec(2) = (dble(j)+0.5)*dx(2)
-           itVec(3) = (dble(k)+0.5)*dx(3)
+     pos = reallo + itVec
+     relpos = pos - center
 
-           pos = reallo + itVec
-           relpos = pos - center
+     ! Total density must be pre-set
 
-           ! Total density must be pre-set
-           
-           if (prob_type.eq.2) then ! Rayleigh-Taylor
-              
-              if (relpos(3) .ge. 0) then
-                 massvec = (/0.4, 0.4, 0.1, 0.1/)
-              else
-                 massvec = (/0.1, 0.1, 0.4, 0.4/)
-              endif
-              
-              call get_pressure_gas(pamb, massvec, cu(i,j,k,1), pu(i,j,k,5))
-              molmix = 0.0d0
-              do l = 1, nspecies
-                 molmix = molmix + massvec(l)/molmass(l)
-              enddo
-              molmix = 1.0d0/molmix
-              rgasmix = Runiv/molmix
-              alpha = grav(3)/(rgasmix*pu(i,j,k,5))
-              
-              ! rho = exponential in z-dir to init @ hydrostatic eqm.
-              ! must satisfy system: dP/dz = -rho*g & P = rhogasmix*rho*T
-              ! Assumes temp=const
-              cu(i,j,k,1) = pamb*exp(alpha*pos(3))/(rgasmix*pu(i,j,k,5)) 
-              
-              do l = 1,nspecies
-                 cu(i,j,k,5+l) = cu(i,j,k,1)*massvec(l)
-              enddo
+     if (prob_type .eq. 2) then ! Rayleigh-Taylor
 
-              call get_energy(intEnergy, massvec, pu(i,j,k,5))
-              cu(i,j,k,5) = cu(i,j,k,1)*intEnergy + 0.5*cu(i,j,k,1)*(pu(i,j,k,2)**2 + &
-                   pu(i,j,k,3)**2 + pu(i,j,k,4)**2)
-              
-           elseif (prob_type.eq.3) then ! diffusion barrier
-                                          
-              do l = 1,nspecies
-                 Ygrad = (bc_Yk(2,2,l) - bc_Yk(2,1,l))/(realhi(2) - reallo(2))
-                 massvec(l) = Ygrad*pos(2) + bc_Yk(2,1,l)
-                 cu(i,j,k,5+l) = cu(i,j,k,1)*massvec(l)
-              enddo
+        if (relpos(3) .ge. 0) then
+           massvec = (/0.4, 0.4, 0.1, 0.1/)
+        else
+           massvec = (/0.1, 0.1, 0.4, 0.4/)
+        endif
 
-              call get_energy(intEnergy, massvec, pu(i,j,k,5))
-              cu(i,j,k,5) = cu(i,j,k,1)*intEnergy + 0.5*cu(i,j,k,1)*(pu(i,j,k,2)**2 + &
-                   pu(i,j,k,3)**2 + pu(i,j,k,4)**2)
+        call get_pressure_gas(pamb, massvec, cu(i,j,k,1), pu(i,j,k,5))
+        molmix = 0.0d0
+        do l = 1, nspecies
+           molmix = molmix + massvec(l)/molmass(l)
+        enddo
+        molmix = 1.0d0/molmix
+        rgasmix = Runiv/molmix
+        alpha = grav(3)/(rgasmix*pu(i,j,k,5))
 
-           endif
+        ! rho = exponential in z-dir to init @ hydrostatic eqm.
+        ! must satisfy system: dP/dz = -rho*g & P = rhogasmix*rho*T
+        ! Assumes temp=const
+        cu(i,j,k,1) = pamb*exp(alpha*pos(3))/(rgasmix*pu(i,j,k,5)) 
 
-        end do
-     end do
+        do l = 1,nspecies
+           cu(i,j,k,5+l) = cu(i,j,k,1)*massvec(l)
+        enddo
+
+        call get_energy(intEnergy, massvec, pu(i,j,k,5))
+        cu(i,j,k,5) = cu(i,j,k,1)*intEnergy + 0.5*cu(i,j,k,1)*(pu(i,j,k,2)**2 + &
+             pu(i,j,k,3)**2 + pu(i,j,k,4)**2)
+
+     elseif (prob_type .eq. 3) then ! diffusion barrier
+
+        do l = 1,nspecies
+           Ygrad = (bc_Yk(2,2,l) - bc_Yk(2,1,l))/(realhi(2) - reallo(2))
+           massvec(l) = Ygrad*pos(2) + bc_Yk(2,1,l)
+           cu(i,j,k,5+l) = cu(i,j,k,1)*massvec(l)
+        enddo
+
+        call get_energy(intEnergy, massvec, pu(i,j,k,5))
+        cu(i,j,k,5) = cu(i,j,k,1)*intEnergy + 0.5*cu(i,j,k,1)*(pu(i,j,k,2)**2 + &
+             pu(i,j,k,3)**2 + pu(i,j,k,4)**2)
+
+     endif
+
+  end do
+  end do
   end do
 
 end subroutine init_consvar
--- src_compressible/src_F90/trans_funcs.F90
+++ src_compressible/src_F90/trans_funcs.F90
@@ -14,15 +14,10 @@ contains
   subroutine trans_coeffs(lo,hi, prim, eta, zeta, kappa) bind(C,name="trans_coeffs")
 
       integer         , intent(in   ) :: lo(3),hi(3)
-
-      real(amrex_real), intent(in   ) :: prim(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3), nprimvars)
-
-      real(amrex_real), intent(inout) :: eta(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3))
-      real(amrex_real), intent(inout) :: zeta(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3))
+      real(amrex_real), intent(in   ) :: prim (lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3),nprimvars)
+      real(amrex_real), intent(inout) :: eta  (lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3))
+      real(amrex_real), intent(inout) :: zeta (lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3))
       real(amrex_real), intent(inout) :: kappa(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3))
-
-      ! real(amrex_real), intent(inout) :: chi(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3),nspecies)
-      ! real(amrex_real), intent(inout) :: Dij(lo(1)-ngc(1):hi(1)+ngc(1),lo(2)-ngc(2):hi(2)+ngc(2),lo(3)-ngc(3):hi(3)+ngc(3),nspecies,nspecies)
 
       integer :: i,j,k,l
       real(amrex_real) :: mgrams(MAX_SPECIES)
@@ -36,56 +31,43 @@ contains
       gamma2 = 1.9223
 
       do l = 1, nspecies
-
-        vconst(l) = sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(gamma1/4)*sqrt(1/3.14159265359)
-        tconst(l) = R(l)*sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(5*gamma2/8)*sqrt(1/3.14159265359)
-
+         vconst(l) = sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(gamma1/4)*sqrt(1/3.14159265359)
+         tconst(l) = R(l)*sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(5*gamma2/8)*sqrt(1/3.14159265359)
       enddo
 
-      !Hard sphere for now. Check these defs
-      !Per Sone (Sone Grad-Hilbert, Bird Chapman Enskog? Check this)
-
-      ! print *, "Hack: dims = ", lo(1),hi(1), lo(2),hi(2), lo(3),hi(3)
-
+      ! Hard sphere for now. Check these defs
+      ! Per Sone (Sone Grad-Hilbert, Bird Chapman Enskog? Check this)
       do k = lo(3),hi(3)
-        do j = lo(2),hi(2)
-          do i = lo(1),hi(1)
+      do j = lo(2),hi(2)
+      do i = lo(1),hi(1)
 
-            specaveta = 0
-            specavkappa = 0
+         specaveta = 0
+         specavkappa = 0
 
-            do l = 1, nspecies
-              specaveta = specaveta + prim(i,j,k,6+l)*vconst(l)
-              specavkappa = specavkappa + prim(i,j,k,6+l)*tconst(l)
-            enddo
+         do l = 1, nspecies
+            specaveta = specaveta + prim(i,j,k,6+l)*vconst(l)
+            specavkappa = specavkappa + prim(i,j,k,6+l)*tconst(l)
+         enddo
+         
+         rootT = sqrt(prim(i,j,k,5))
 
-            rootT = sqrt(prim(i,j,k,5))
+         zeta(i,j,k) = 0 !no bulk viscosity for now
 
-            zeta(i,j,k) = 0 !no bulk viscosity for now
+         eta(i,j,k) = rootT*specaveta
+         kappa(i,j,k) = rootT*specavkappa
 
-            eta(i,j,k) = rootT*specaveta
-            kappa(i,j,k) = rootT*specavkappa
-            
-            ! if((i.eq.0).and.(j.eq.0).and.(k.eq.0)) then
-            !    print *, "transcoef: Hack = ", i, j, k, specaveta, specavkappa, rootT, prim(i,j,k,5)
-            ! endif
+         if(kappa(i,j,k) .ne. kappa(i,j,k)) then
+            print *, "NAN! kappa ", i, j, k, prim(i,j,k,5)
+            call exit()
+         endif
 
-            if(kappa(i,j,k) .ne. kappa(i,j,k)) then
-               print *, "NAN! kappa ", i, j, k, prim(i,j,k,5)
-               call exit()
-            endif
-
-            ! print*, "Hack (trans_coeffs): eta = ", eta(i,j,k)
-            ! stop
-
-          enddo
-        enddo
+      enddo
+      enddo
       enddo
 
   end subroutine trans_coeffs
 
   subroutine get_trans_coeff(temp, fracvec, eta, zeta, kappa) bind(C,name="get_trans_coeff")
-
 
       double precision, intent(in   ) :: temp, fracvec(nspecies)
       double precision, intent(inout) :: eta, zeta, kappa
@@ -101,18 +83,16 @@ contains
       gamma2 = 1.9223
 
       do l = 1, nspecies
-
-        vconst(l) = sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(gamma1/4)*sqrt(1/3.14159265359)
-        tconst(l) = R(l)*sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(5*gamma2/8)*sqrt(1/3.14159265359)
-
+         vconst(l) = sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(gamma1/4)*sqrt(1/3.14159265359)
+         tconst(l) = R(l)*sqrt(R(l))*(mgrams(l)/(diameter(l)**2))*(5*gamma2/8)*sqrt(1/3.14159265359)
       enddo
 
       specaveta = 0
       specavkappa = 0
 
       do l = 1, nspecies
-        specaveta = specaveta + fracvec(l)*vconst(l)
-        specavkappa = specavkappa + fracvec(l)*tconst(l)
+         specaveta = specaveta + fracvec(l)*vconst(l)
+         specavkappa = specavkappa + fracvec(l)*tconst(l)
       enddo
 
       rootT = sqrt(temp)
@@ -126,7 +106,6 @@ contains
          print *, "NAN! kappa bc", temp, specavkappa
          call exit()
       endif
-
 
   end subroutine get_trans_coeff
 
