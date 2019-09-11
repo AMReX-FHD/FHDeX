d062bc70addb1afa6aeef036fc7ca6039d6451df
Johannes Blaschke <jpblaschke@lbl.gov>
2019-09-11 14:01:19 -0700

    exampand input file parser to include chlamy data

--- src_immersed-boundary/ib_functions.cpp
+++ src_immersed-boundary/ib_functions.cpp
@@ -33,6 +33,10 @@ void InitializeIBFlagellumNamespace() {
     ib_flagellum::k_spring.resize(n_immbdy);
     ib_flagellum::k_driving.resize(n_immbdy);
 
+    int len_buffer = 0;
+    chlamy_flagellum_datafile_len(& len_buffer);
+    char chlamy_flagellum_datafile[len_buffer+1];
+
     initialize_ib_flagellum_namespace(n_immbdy,
                                       ib_flagellum::n_marker.dataPtr(),
                                       ib_flagellum::offset_0.dataPtr(),
@@ -41,6 +45,12 @@ void InitializeIBFlagellumNamespace() {
                                       ib_flagellum::length.dataPtr(),
                                       ib_flagellum::wavelength.dataPtr(),
                                       ib_flagellum::k_spring.dataPtr(),
-                                      ib_flagellum::k_driving.dataPtr());
+                                      ib_flagellum::k_driving.dataPtr(),
+                                      & ib_flagellum::fourier_coef_len,
+                                      chlamy_flagellum_datafile);
 
+
+
+    ib_flagellum::chlamy_flagellum_datafile = chlamy_flagellum_datafile;
+    Print() << "data_file = " << ib_flagellum::chlamy_flagellum_datafile << std::endl;
 }
--- src_immersed-boundary/src_F90/immbdy_namelist.F90
+++ src_immersed-boundary/src_F90/immbdy_namelist.F90
@@ -1,6 +1,6 @@
 module immbdy_namelist_module
 
-    use iso_c_binding,       only: c_char
+    use iso_c_binding,       only: c_int, c_char, c_null_char
     use amrex_string_module, only: amrex_string_c_to_f, amrex_string_f_to_c
     use amrex_fort_module,   only: amrex_real
 
@@ -22,10 +22,17 @@ module immbdy_namelist_module
     real(amrex_real), allocatable, save :: k_spring(:)
     real(amrex_real), allocatable, save :: k_driving(:)
 
+    integer,                       save :: fourier_coef_len
+    real(amrex_real), allocatable, save :: a_coeff(:)
+    real(amrex_real), allocatable, save :: b_coeff(:)
+    character(:),     allocatable, save :: chlamy_flagellum_datafile
 
+
+    ! most general immersed boundary information:
     namelist /immbdy/ n_immbdy
     namelist /immbdy/ contains_flagellum
 
+    ! information relevant to generic flagellum
     namelist /ib_flagellum/ n_marker
     namelist /ib_flagellum/ offset_0
     namelist /ib_flagellum/ amplitude
@@ -35,9 +42,18 @@ module immbdy_namelist_module
     namelist /ib_flagellum/ k_spring
     namelist /ib_flagellum/ k_driving
 
+    ! general information about chlamy (fourier) data
+    namelist /ib_flagellum/ fourier_coef_len
+    namelist /ib_flagellum/ chlamy_flagellum_datafile
+
+    ! fourier series analysis based on experimenatal data on chlamy
+    namelist /chlamy_flagellum/ a_coeff
+    namelist /chlamy_flagellum/ b_coeff
+
 
 
 contains
+
 
     subroutine read_immbdy_namelist(inputs_file, f_length) &
             bind(C, name="read_immbdy_namelist")
@@ -76,6 +92,10 @@ contains
             k_spring(:)    = 0
             k_driving(:)   = 0
 
+            ! default file name for any additional chlamy data
+            fourier_coef_len = 0
+            chlamy_flagellum_datafile = "none"
+
             ! read in immbdy namelist
             open(unit=100, file=amrex_string_c_to_f(inputs_file), status='old', action='read')
             read(unit=100, nml=ib_flagellum)
@@ -86,11 +106,21 @@ contains
     end subroutine read_immbdy_namelist
 
 
+    subroutine chlamy_flagellum_datafile_len(len_buffer) &
+            bind(C, name="chlamy_flagellum_datafile_len")
+
+        integer(c_int), intent(inout) :: len_buffer
+
+        len_buffer = len_trim(chlamy_flagellum_datafile)
+
+    end subroutine chlamy_flagellum_datafile_len
+
+
     subroutine initialize_immbdy_namespace (n_immbdy_in, contains_flagellum_in) &
             bind(C, name="initialize_immbdy_namespace")
 
-        integer, intent(inout) :: n_immbdy_in
-        integer, intent(inout) :: contains_flagellum_in
+        integer(c_int), intent(inout) :: n_immbdy_in
+        integer(c_int), intent(inout) :: contains_flagellum_in
 
         n_immbdy_in = n_immbdy
 
@@ -102,12 +132,16 @@ contains
 
     subroutine initialize_ib_flagellum_namespace (n_immbdy, n_marker_in, offset_0_in,       &
             &                                     amplitude_in, frequency_in, length_in,    &
-            &                                     wavelength_in, k_spring_in, k_driving_in) &
+            &                                     wavelength_in, k_spring_in, k_driving_in, &
+            &                                     fourier_coef_len_in,                      &
+            &                                     chlamy_flagellum_datafile_in             )&
             bind(C, name="initialize_ib_flagellum_namespace")
 
-        integer, value, intent(in) :: n_immbdy
+        integer :: i, n
 
-        integer,          intent(inout) :: n_marker_in(n_immbdy)
+        integer(c_int), value, intent(in) :: n_immbdy
+
+        integer(c_int),   intent(inout) :: n_marker_in(n_immbdy)
         real(amrex_real), intent(inout) :: offset_0_in(AMREX_SPACEDIM, n_immbdy)
         real(amrex_real), intent(inout) :: amplitude_in(n_immbdy)
         real(amrex_real), intent(inout) :: frequency_in(n_immbdy)
@@ -116,6 +150,12 @@ contains
         real(amrex_real), intent(inout) :: k_spring_in(n_immbdy)
         real(amrex_real), intent(inout) :: k_driving_in(n_immbdy)
 
+        integer(c_int), intent(inout) :: fourier_coef_len_in
+        character(kind=c_char), intent(inout) :: chlamy_flagellum_datafile_in(*)
+
+
+        ! copy general immersed-boundary flagellum parameters
+        !------------------------------------------------------------------------
 
         n_marker_in   = n_marker;
         offset_0_in   = offset_0;
@@ -125,6 +165,21 @@ contains
         wavelength_in = wavelength;
         k_spring_in   = k_spring;
         k_driving_in  = k_driving;
+
+
+        ! extra flagellum data relating to fourier expansion:
+        !------------------------------------------------------------------------
+
+        ! number of fourier modes
+        fourier_coef_len_in = fourier_coef_len
+
+        ! name of data file containing fourier modes
+        n = len(chlamy_flagellum_datafile)
+        do i = 1, n
+            ! you need to "substring" into a fortran string to access a single character:
+            chlamy_flagellum_datafile_in(i) = chlamy_flagellum_datafile(i:i);
+        end do
+        chlamy_flagellum_datafile_in(n+1) = c_null_char
 
     end subroutine initialize_ib_flagellum_namespace
 
