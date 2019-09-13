67fa6a0ba030d0da51b89294a03911b1031848c0
Johannes Blaschke <jpblaschke@lbl.gov>
2019-09-13 14:47:56 -0700

    copy fourier data to c-land

--- src_immersed-boundary/ib_functions.cpp
+++ src_immersed-boundary/ib_functions.cpp
@@ -49,4 +49,52 @@ void InitializeIBFlagellumNamespace() {
                                       chlamy_flagellum_datafile);
 
     ib_flagellum::chlamy_flagellum_datafile = chlamy_flagellum_datafile;
+
+    if (contains_fourier) {
+        chlamy_flagellum::a_coef.resize(n_immbdy);
+        chlamy_flagellum::b_coef.resize(n_immbdy);
+
+        int max_n_markers = 0;
+        flagellum_max_markers(& max_n_markers);
+
+        for (int i=0; i<n_immbdy; ++i) {
+            // NOTE: "last" marker contains no fourier mode
+            chlamy_flagellum::a_coef[i].resize(max_n_markers-1);
+            chlamy_flagellum::b_coef[i].resize(max_n_markers-1);
+
+            for(int j=0; j<max_n_markers-1; ++j) {
+                chlamy_flagellum::a_coef[i][j].resize(ib_flagellum::fourier_coef_len);
+                chlamy_flagellum::b_coef[i][j].resize(ib_flagellum::fourier_coef_len);
+
+                copy_ib_fourier_data(i+1, j+1, // Don't forget: fortran array indices start at 1
+                                     chlamy_flagellum::a_coef[i][j].dataPtr(),
+                                     chlamy_flagellum::b_coef[i][j].dataPtr());
+
+            }
+        }
+
+
+        Print() << "Loaded Fourier Data:" << std::endl;
+        Print() << "a_coef:" << std::endl;
+        for (int i=0; i<n_immbdy; ++i) {
+            for (int j=0; j<max_n_markers-1; ++j) {
+                Print() << i << ", " << j << ":";
+                for (Real elt : chlamy_flagellum::a_coef[i][j])
+                    Print() << " " << elt ;
+                Print() << std::endl;
+            }
+        }
+
+        Print() << "b_coef:" << std::endl;
+        for (int i=0; i<n_immbdy; ++i) {
+            for (int j=0; j<max_n_markers-1; ++j) {
+                Print() << i << ", " << j << ":";
+                for (Real elt : chlamy_flagellum::b_coef[i][j])
+                    Print() << " " << elt ;
+                Print() << std::endl;
+            }
+        }
+
+
+    }
 }
