31f52197441f9d86afcb034b64790909979912ba
Andy Nonaka <AJNonaka@lbl.gov>
2019-10-24 18:03:07 -0700

    FHDeX struct fact test setup

---
+++ exec/structFactTest/main_driver.cpp
@@ -0,0 +1,119 @@
+#include "common_functions.H"
+#include "common_functions_F.H"
+
+#include "common_namespace.H"
+#include "common_namespace_declarations.H"
+
+#include <AMReX_VisMF.H>
+#include <AMReX_PlotFileUtil.H>
+#include <AMReX_ParallelDescriptor.H>
+#include <AMReX_MultiFabUtil.H>
+
+#include "StructFact.H"
+
+using namespace amrex;
+using namespace common;
+
+// argv contains the name of the inputs file entered at the command line
+void main_driver(const char* argv)
+{
+
+    BL_PROFILE_VAR("main_driver()",main_driver);
+
+    // store the current time so we can later compute total run time.
+    Real strt_time = ParallelDescriptor::second();
+
+    std::string inputs_file = argv;
+
+    // read in parameters from inputs file into F90 modules
+    // we use "+1" because of amrex_string_c_to_f expects a null char termination
+    read_common_namelist(inputs_file.c_str(),inputs_file.size()+1);
+
+    // copy contents of F90 modules to C++ namespaces
+    InitializeCommonNamespace();
+
+    // is the problem periodic?
+    Vector<int> is_periodic(AMREX_SPACEDIM,1);  // set to 1 (periodic) by default
+
+    // make BoxArray and Geometry
+    BoxArray ba;
+    Geometry geom;
+    {
+        IntVect dom_lo(AMREX_D_DECL(           0,            0,            0));
+        IntVect dom_hi(AMREX_D_DECL(n_cells[0]-1, n_cells[1]-1, n_cells[2]-1));
+        Box domain(dom_lo, dom_hi);
+
+        // Initialize the boxarray "ba" from the single box "bx"
+        ba.define(domain);
+
+        // Break up boxarray "ba" into chunks no larger than "max_grid_size" along a direction
+        // note we are converting "Vector<int> max_grid_size" to an IntVect
+        ba.maxSize(IntVect(max_grid_size));
+
+       // This defines the physical box, [-1,1] in each direction.
+        RealBox real_box({AMREX_D_DECL(prob_lo[0],prob_lo[1],prob_lo[2])},
+                         {AMREX_D_DECL(prob_hi[0],prob_hi[1],prob_hi[2])});
+
+        // This defines a Geometry object
+        geom.define(domain,&real_box,CoordSys::cartesian,is_periodic.data());
+    }
+
+    const Real* dx = geom.CellSize();
+
+    // we may need to use dVol for scaling
+    Real dVol = dx[0]*dx[1];
+    int tot_n_cells = n_cells[0]*n_cells[1];
+    if (AMREX_SPACEDIM == 2) {
+	dVol *= cell_depth;
+    } else if (AMREX_SPACEDIM == 3) {
+	dVol *= dx[2];
+	tot_n_cells = n_cells[2]*tot_n_cells;
+    }
+    
+    // how boxes are distrubuted among MPI processes
+    DistributionMapping dmap(ba);
+    
+    /////////////////////////////////////////
+
+    Vector< std::string > var_names(2);
+    var_names[0] = "phi1";
+    var_names[1] = "phi2";
+
+    // for the 3 pairs
+    Vector< Real > var_scaling(3);
+    var_scaling[0] = 1.;
+    var_scaling[1] = 1.;
+    var_scaling[2] = 1.;
+    
+    MultiFab struct_in_cc;
+    struct_in_cc.define(ba, dmap, 2, 0);
+
+    // WRITE INIT ROUTINE
+    struct_in_cc.setVal(0.);
+
+    amrex::Vector< int > s_pairA(3);
+    amrex::Vector< int > s_pairB(3);
+
+    // Select which variable pairs to include in structure factor:
+    s_pairA[0] = 0;
+    s_pairB[0] = 0;
+    //
+    s_pairA[1] = 0;
+    s_pairB[1] = 1;
+    //
+    s_pairA[2] = 1;
+    s_pairB[2] = 1;
+
+    StructFact structFact(ba,dmap,var_names,var_scaling,s_pairA,s_pairB);
+    
+    structFact.FortStructure(struct_in_cc,geom);
+      
+    structFact.WritePlotFile(0,0.,geom,"plt_SF");
+
+    // Call the timer again and compute the maximum difference between the start time
+    // and stop time over all processors
+    Real stop_time = ParallelDescriptor::second() - strt_time;
+    ParallelDescriptor::ReduceRealMax(stop_time);
+    amrex::Print() << "Run time = " << stop_time << std::endl;
+
+}
