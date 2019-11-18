a148209434ffa59c74a5bf6ea6f9ea1a9f1879bc
Andy Nonaka <AJNonaka@lbl.gov>
2019-11-18 10:56:39 -0800

    standard deviation calculator

---
+++ exec/immersedIons/postprocessing/std.f90
@@ -0,0 +1,40 @@
+program std
+
+  implicit none
+
+  character(len=80) filename
+
+  integer i, j, npts, nfiles
+  
+  double precision, allocatable :: file(:)
+
+  double precision :: temp1, temp2
+  
+  print*,'enter filename'
+  read(*,*) filename
+
+  print*,'how many points'
+  read(*,*) npts
+
+  allocate(file(npts))
+
+  open(unit=999,file=trim(filename))
+  do i=1,npts
+     read(unit=999, FMT=*) file(i)
+  end do
+
+  temp1 = 0.
+  do i=1,npts
+     temp1 = temp1 + file(i) 
+  end do
+  temp1 = temp1 / npts
+
+  temp2 = 0.
+  do i=1,npts
+     temp2 = temp2 + ( file(i) - temp1 )**2
+  end do
+  temp2 = sqrt(temp2/npts)
+
+  print*,'standard deviation',temp2
+
+end program std
