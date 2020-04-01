f671cd605a5edd7746cf215cbde3c245bcd5b2ab
Andy Nonaka <AJNonaka@lbl.gov>
2020-04-01 16:07:21 -0700

    advance timestep inertial setup

--- exec/multispec/AdvanceTimestepInertial.cpp
+++ exec/multispec/AdvanceTimestepInertial.cpp
@@ -16,19 +16,83 @@
 void AdvanceTimestepInertial(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                              MultiFab& rho_old, MultiFab& rho_new,
                              MultiFab& rhotot_old, MultiFab& rhotot_new,
-                             MultiFab& pi, const MultiFab& eta, 
-                             const std::array< MultiFab, NUM_EDGE >&  eta_ed,
-                             const MultiFab& kappa, const MultiFab& Temp,
-                             const std::array< MultiFab, NUM_EDGE >& Temp_ed,
+                             MultiFab& pi, MultiFab& eta, 
+                             std::array< MultiFab, NUM_EDGE >&  eta_ed,
+                             MultiFab& kappa, MultiFab& Temp,
+                             std::array< MultiFab, NUM_EDGE >& Temp_ed,
                              MultiFab& diff_mass_fluxdiv,
-                             MultiFab& stoch_mass_fluxdiv_mass_fluxdiv,
+                             MultiFab& stoch_mass_fluxdiv,
                              std::array< MultiFab, AMREX_SPACEDIM >& stoch_mass_flux,
                              const Real& dt, const Real& time, const int& istep,
                              const Geometry& geom)
 {
   
-  BL_PROFILE_VAR("AdvanceTimestepInertial()",AdvanceTimestepInertial);
+    BL_PROFILE_VAR("AdvanceTimestepInertial()",AdvanceTimestepInertial);
 
-  Real theta_alpha = 1./dt;
+    BoxArray ba = rho_old.boxArray();
+    DistributionMapping dmap = rho_old.DistributionMap();
 
+    Vector< amrex::Real > weights;
+    weights = {1.0};
+    
+    Real theta_alpha = 1./dt;
+
+    MultiFab rho_update (ba,dmap,nspecies,0);
+    MultiFab gmres_rhs_p(ba,dmap,       1,0);
+    MultiFab dpi        (ba,dmap,       1,1);
+
+    std::array< MultiFab, AMREX_SPACEDIM > mold;
+    std::array< MultiFab, AMREX_SPACEDIM > mtemp;
+    std::array< MultiFab, AMREX_SPACEDIM > adv_mom_fluxdiv;
+    std::array< MultiFab, AMREX_SPACEDIM > diff_mom_fluxdiv;
+    std::array< MultiFab, AMREX_SPACEDIM > gmres_rhs_v;
+    std::array< MultiFab, AMREX_SPACEDIM > dumac;
+    std::array< MultiFab, AMREX_SPACEDIM > gradpi;
+    std::array< MultiFab, AMREX_SPACEDIM > rhotot_fc_old;
+    std::array< MultiFab, AMREX_SPACEDIM > rhotot_fc_new;
+    std::array< MultiFab, AMREX_SPACEDIM > rho_fc;
+    std::array< MultiFab, AMREX_SPACEDIM > diff_mass_flux;
+    std::array< MultiFab, AMREX_SPACEDIM > total_mass_flux;
+    std::array< MultiFab, AMREX_SPACEDIM > stoch_mom_fluxdiv;
+
+    for (int d=0; d<AMREX_SPACEDIM; ++d) {
+        mold[d]             .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 1);
+        mtemp[d]            .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 1);
+        adv_mom_fluxdiv[d]  .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 0);
+        diff_mom_fluxdiv[d] .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 0);
+        gmres_rhs_v[d]      .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 0);
+        dumac[d]            .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 1);
+        gradpi[d]           .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 0);
+        rhotot_fc_old[d]    .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 1);
+        rhotot_fc_new[d]    .define(convert(ba,nodal_flag_dir[d]), dmap,        1, 1);
+        rho_fc[d]           .define(convert(ba,nodal_flag_dir[d]), dmap, nspecies, 0);
+        diff_mass_flux[d]   .define(convert(ba,nodal_flag_dir[d]), dmap, nspecies, 0);
+        total_mass_flux[d]  .define(convert(ba,nodal_flag_dir[d]), dmap, nspecies, 0);
+        stoch_mom_fluxdiv[d].define(convert(ba,nodal_flag_dir[d]), dmap, nspecies, 0);
+    }
+
+    // make copies of old quantities
+    // copy umac into umac_tmp if using bds
+
+    //////////////////////////////////////////////
+    /// Step 1 - Calculate Predictor Diffusive and Stochastic Fluxes
+    //////////////////////////////////////////////
+
+    // diff/stoch_mass_fluxdiv already contain F_i
+    // this was already done in Step 0 (initialization) or Step 6 from the previous time step
+    
+    //////////////////////////////////////////////
+    // Step 2 - Predictor Euler Step
+    //////////////////////////////////////////////
+
+    /*
+    if (use_charged_fluid) {
+        // compute old Lorentz force
+    }
+    */
+
+    // average rho_old and rhotot_old to faces
+    
+    
+    
 }
