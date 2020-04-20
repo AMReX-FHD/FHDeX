930a7f2ae70899d2056af91c9d92f79773be8bcb
Andy Nonaka <AJNonaka@lbl.gov>
2020-04-20 11:37:30 -0700

    remove print statement

--- src_multispec/MassFluxUtil.cpp
+++ src_multispec/MassFluxUtil.cpp
@@ -109,8 +109,6 @@ void ComputeSqrtLonsagerFC(const MultiFab& rho, const MultiFab& rhotot,
 
     // for GPU later
     // const GpuArray<Real, AMREX_SPACEDIM> dx = geom.CellSizeArray();
-
-    Print() << "HERE\n";
     
     // Loop over boxes
     for (MFIter mfi(rho); mfi.isValid(); ++mfi) {
