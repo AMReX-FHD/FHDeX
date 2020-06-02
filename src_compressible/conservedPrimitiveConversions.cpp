459be241b3f1c80678679201c340cd69255236c8
Andy Nonaka <AJNonaka@lbl.gov>
2020-06-02 16:58:20 -0700

    WIP (doesn't compile) but has Weiqun's fixes commented in

--- src_compressible/conservedPrimitiveConversions.cpp
+++ src_compressible/conservedPrimitiveConversions.cpp
@@ -5,6 +5,34 @@ void conservedToPrimitive(MultiFab& prim_in, const MultiFab& cons_in)
 {
     BL_PROFILE_VAR("conservedToPrimitive()",conservedToPrimitive);
 
+    // from namelist
+    int nspecies_gpu = nspecies;
+
+    // from namelist
+    Real Runiv_gpu = Runiv;
+
+    // from namelist
+    amrex::Vector<amrex::Real> hcv_vect_host(nspecies);
+    for (int n=0; n<nspecies; ++n) {
+        hcv_vect_host[n] = hcv[n];
+    }
+    
+    amrex::Gpu::DeviceVector<amrex::Real> hcv_vect(nspecies);
+    Real const * const AMREX_RESTRICT hcv_gpu = hcv_vect.dataPtr();
+    amrex::Gpu::copy(amrex::Gpu::HostToDevice,hcv_vect_host.begin(),hcv_vect_host.end(),
+                     hcv_vect.begin());
+
+
+    // FIX
+    /*
+    // from namelist
+    amrex::Gpu::DeviceVector<amrex::Real> molmass_vect(nspecies);
+    for (int n=0; n<nspecies; ++n) {
+        molmass_vect[n] = molmass[n];
+    }
+    Real const * const AMREX_RESTRICT molmass_gpu = molmass_vect.dataPtr();
+    */
+    
     // Loop over boxes
     for ( MFIter mfi(prim_in); mfi.isValid(); ++mfi) {
         
@@ -19,6 +47,8 @@ void conservedToPrimitive(MultiFab& prim_in, const MultiFab& cons_in)
         const Array4<const Real>& cons = cons_in.array(mfi);
         const Array4<      Real>& prim = prim_in.array(mfi);
 
+        // FIX
+        /*
         amrex::Gpu::ManagedVector<amrex::Real> Yk_vect(nspecies);
         amrex::Gpu::ManagedVector<amrex::Real> Yk_fixed_vect(nspecies);
         amrex::Gpu::ManagedVector<amrex::Real> Xk_vect(nspecies);
@@ -26,24 +56,19 @@ void conservedToPrimitive(MultiFab& prim_in, const MultiFab& cons_in)
         Real * const AMREX_RESTRICT Yk = Yk_vect.dataPtr();
         Real * const AMREX_RESTRICT Yk_fixed = Yk_fixed_vect.dataPtr();
         Real * const AMREX_RESTRICT Xk = Xk_vect.dataPtr();
+        */
 
-        int nspecies_gpu = nspecies;
-        Real Runiv_gpu = Runiv;
-
-        amrex::Gpu::ManagedVector<amrex::Real> molmass_vect(nspecies);
-        for (int n=0; n<nspecies; ++n) {
-            molmass_vect[n] = molmass[n];
-        }
-        Real const * const AMREX_RESTRICT molmass_gpu = molmass_vect.dataPtr();
-
-        amrex::Gpu::ManagedVector<amrex::Real> hcv_vect(nspecies);
-        for (int n=0; n<nspecies; ++n) {
-            hcv_vect[n] = hcv[n];
-        }
-        Real const * const AMREX_RESTRICT hcv_gpu = hcv_vect.dataPtr();
-                
+        // this is allocated on the DEVICE (no page faults)
+        FArrayBox Yk_fab(bx,nspecies);
+        const Array4<Real>& Yk = Yk_fab.array(mfi);
+        // make sure Yk_fab doesn't go out of scope once the CPU finishes and GPU isn't done
+        auto Yk_eli = Yk_fab.elixir();
+        
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
+            // option if MAX_SPECIES is a compile-time constant
+//            Real Yk[MAX_SPECIES];
+            
             prim(i,j,k,0) = cons(i,j,k,0);
             prim(i,j,k,1) = cons(i,j,k,1)/cons(i,j,k,0);
             prim(i,j,k,2) = cons(i,j,k,2)/cons(i,j,k,0);
@@ -54,8 +79,8 @@ void conservedToPrimitive(MultiFab& prim_in, const MultiFab& cons_in)
 
             Real sumYk = 0.;
             for (int n=0; n<nspecies_gpu; ++n) {
-                Yk[n] = cons(i,j,k,5+n)/cons(i,j,k,0);
-                Yk_fixed[n] = std::max(0.,std::min(1.,Yk[n]));
+                Yk(i,j,k,n) = cons(i,j,k,5+n)/cons(i,j,k,0);
+                Yk_fixed[n] = std::max(0.,std::min(1.,Yk(i,j,k,n)));
                 sumYk += Yk_fixed[n];
             }
             
@@ -64,19 +89,19 @@ void conservedToPrimitive(MultiFab& prim_in, const MultiFab& cons_in)
             }
 
             // update temperature in-place using internal energy
-            GetTemperature(intenergy,Yk_fixed,prim(i,j,k,4),nspecies_gpu,hcv_gpu);
+            GetTemperature(i,j,k,intenergy,Yk_fixed,prim(i,j,k,4),nspecies_gpu,hcv_gpu);
 
             // compute mole fractions from mass fractions
             GetMolfrac(Yk,Xk,nspecies_gpu,molmass_gpu);
 
             // mass fractions
             for (int n=0; n<nspecies_gpu; ++n) {
-                prim(i,j,k,6+n) = Yk[n];
+                prim(i,j,k,6+n) = Yk(i,j,k,n);
                 prim(i,j,k,6+nspecies_gpu+n) = Xk[n];
             }
 
             GetPressureGas(prim(i,j,k,5),Yk,prim(i,j,k,0),prim(i,j,k,4),nspecies_gpu,Runiv_gpu,molmass_gpu);
             
         });
-    }
+    } // end MFIter
 }
