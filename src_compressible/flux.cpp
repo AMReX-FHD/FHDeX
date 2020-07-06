1b082ea49fc0e1e62b7b568021a1091e47d0553b
Andy Nonaka <AJNonaka@lbl.gov>
2020-07-06 13:49:56 -0700

    flux conversion WIP

--- src_compressible/flux.cpp
+++ src_compressible/flux.cpp
@@ -92,6 +92,7 @@ void calculateFlux(const MultiFab& cons_in, const MultiFab& prim_in,
     int nspecies_gpu = nspecies;
     int algorithm_type_gpu = algorithm_type;
     int nvars_gpu = nvars;
+    int nprimvars_gpu = nprimvars;
     Real Runiv_gpu = Runiv;
     
     // from namelist
@@ -121,171 +122,266 @@ void calculateFlux(const MultiFab& cons_in, const MultiFab& prim_in,
         const Box& tbz = mfi.nodaltilebox(2);
 
         if (advection_type == 1) {
+            
+            // Loop over the cells and compute fluxes
+            amrex::ParallelFor(tbx, tby, tbz,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+            
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
+                    
+                for (int l=0; l<nprimvars_gpu; ++l) {
+                    primitive[l] = wgt1*(prim(i,j,k,l)+prim(i-1,j,k,l)) - wgt2*(prim(i-2,j,k,l)+prim(i+1,j,k,l));
+                }
 
-#if 0
+                Real temp = primitive[4];
+                Real pt = primitive[5];
+                Real rho = primitive[0];
+                conserved[0] = rho;
 
-#else
-            const Box& bx = mfi.tilebox();
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = primitive[6+n];
+                }
 
-            hyp_flux_prim(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
-                          cons_in[mfi].dataPtr(),  
-                          prim_in[mfi].dataPtr(),    
-                          flux_in[0][mfi].dataPtr(),
-                          flux_in[1][mfi].dataPtr(),
-#if (AMREX_SPACEDIM == 3)
-                          flux_in[2][mfi].dataPtr(),
-#endif
-                          ZFILL(dx));
-#endif            
+                Real intenergy;
+                GetEnergy(intenergy, Yk, temp, hcv_gpu, nspecies_gpu);
 
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+
+                conserved[4] = rho*intenergy + 0.5*rho*vsqr;
+
+                xflux(i,j,k,0) += conserved[0]*primitive[1];
+                xflux(i,j,k,1) += conserved[0]*(primitive[1]*primitive[1])+primitive[5];
+                xflux(i,j,k,2) += conserved[0]*primitive[1]*primitive[2];
+                xflux(i,j,k,3) += conserved[0]*primitive[1]*primitive[3];
+                
+                xflux(i,j,k,4) += primitive[1]*conserved[4] + primitive[5]*primitive[1];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
+                    for (int n=0; n<nspecies_gpu; ++n) {
+                        xflux(i,j,k,5+n) += rho*primitive[6+n]*primitive[1];
+                    }
+                }
+            },
+
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
+                    
+                for (int l=0; l<nprimvars_gpu; ++l) {
+                    primitive[l] = wgt1*(prim(i,j,k,l)+prim(i,j-1,k,l)) - wgt2*(prim(i,j-2,k,l)+prim(i,j+1,k,l));
+                }
+
+                Real temp = primitive[4];
+                Real pt = primitive[5];
+                Real rho = primitive[0];
+                conserved[0] = rho;
+
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = primitive[6+n];
+                }
+
+                Real intenergy;
+                GetEnergy(intenergy, Yk, temp, hcv_gpu, nspecies_gpu);
+
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+
+                conserved[4] = rho*intenergy + 0.5*rho*vsqr;
+
+                yflux(i,j,k,0) += conserved[0]*primitive[2];
+                yflux(i,j,k,1) += conserved[0]*primitive[1]*primitive[2];
+                yflux(i,j,k,2) += conserved[0]*primitive[2]*primitive[2]+primitive[5];
+                yflux(i,j,k,3) += conserved[0]*primitive[3]*primitive[2];
+
+                yflux(i,j,k,4) += primitive[2]*conserved[4] + primitive[5]*primitive[2];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
+                    for (int n=0; n<nspecies_gpu; ++n) {
+                        yflux(i,j,k,5+n) += rho*primitive[6+n]*primitive[2];
+                    }
+                }
+            },
+
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+                
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
+                    
+                for (int l=0; l<nprimvars_gpu; ++l) {
+                    primitive[l] = wgt1*(prim(i,j,k,l)+prim(i,j,k-1,l)) - wgt2*(prim(i,j,k-2,l)+prim(i,j,k+1,l));
+                }
+
+                Real temp = primitive[4];
+                Real pt = primitive[5];
+                Real rho = primitive[0];
+                conserved[0] = rho;
+
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = primitive[6+n];
+                }
+
+                Real intenergy;
+                GetEnergy(intenergy, Yk, temp, hcv_gpu, nspecies_gpu);
+
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+
+                conserved[4] = rho*intenergy + 0.5*rho*vsqr;
+
+                zflux(i,j,k,0) += conserved[0]*primitive[3];
+                zflux(i,j,k,1) += conserved[0]*primitive[1]*primitive[3];
+                zflux(i,j,k,2) += conserved[0]*primitive[2]*primitive[3];
+                zflux(i,j,k,3) += conserved[0]*primitive[3]*primitive[3]+primitive[5];
+
+                zflux(i,j,k,4) += primitive[3]*conserved[4] + primitive[5]*primitive[3];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
+                    for (int n=0; n<nspecies_gpu; ++n) {
+                        zflux(i,j,k,5+n) += rho*primitive[6+n]*primitive[3];
+                    }
+                }
+
+            });
+            
         } else if (advection_type == 2) {
 
-#if 1
-            // Loop over the cells and compute flux_ines
+            // Loop over the cells and compute fluxes
             amrex::ParallelFor(tbx, tby, tbz,
-                [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
 
-                    GpuArray<Real,MAX_SPECIES> conserved;
-                    GpuArray<Real,MAX_SPECIES> primitive;
-                    GpuArray<Real,MAX_SPECIES> Yk;
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
     
-                    // interpolate conserved quantities to faces
-                    for (int l=0; l<nvars_gpu; ++l) {
-                        conserved[l] = wgt1*(cons(i,j,k,l)+cons(i-1,j,k,l)) - wgt2*(cons(i-2,j,k,l)+cons(i+1,j,k,l));
-                    }
+                // interpolate conserved quantities to faces
+                for (int l=0; l<nvars_gpu; ++l) {
+                    conserved[l] = wgt1*(cons(i,j,k,l)+cons(i-1,j,k,l)) - wgt2*(cons(i-2,j,k,l)+cons(i+1,j,k,l));
+                }
 
-                    // compute velocities
-                    for (int l=1; l<4; ++l) {
-                        primitive[l] = conserved[l]/conserved[0];
-                    }
+                // compute velocities
+                for (int l=1; l<4; ++l) {
+                    primitive[l] = conserved[l]/conserved[0];
+                }
 
-                    // want sum of specden == rho
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = conserved[5+n]/conserved[0];
+                }
+
+                // compute temperature
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+                Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
+                GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
+
+                // compute pressure
+                GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
+
+                xflux(i,j,k,0) += conserved[0]*primitive[1];
+                xflux(i,j,k,1) += conserved[0]*(primitive[1]*primitive[1])+primitive[5];
+                xflux(i,j,k,2) += conserved[0]*primitive[1]*primitive[2];
+                xflux(i,j,k,3) += conserved[0]*primitive[1]*primitive[3];
+                xflux(i,j,k,4) += primitive[1]*conserved[4] + primitive[5]*primitive[1];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
                     for (int n=0; n<nspecies_gpu; ++n) {
-                        Yk[n] = conserved[5+n]/conserved[0];
+                        xflux(i,j,k,5+n) += conserved[5+n]*primitive[1];
                     }
+                }
+            },
 
-                    // compute temperature
-                    Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
-                    Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
-                    GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
 
-                    // compute pressure
-                    GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
-
-                    xflux(i,j,k,1) += conserved[0]*primitive[1];
-                    xflux(i,j,k,2) += conserved[0]*(primitive[1]*primitive[1])+primitive[5];
-                    xflux(i,j,k,3) += conserved[0]*primitive[1]*primitive[2];
-                    xflux(i,j,k,4) += conserved[0]*primitive[1]*primitive[3];
-                    xflux(i,j,k,5) += primitive[1]*conserved[4] + primitive[5]*primitive[1];
-
-                    if (algorithm_type_gpu == 2) { // Add advection of concentration
-                        for (int n=0; n<nspecies_gpu; ++n) {
-                            xflux(i,j,k,5+n) += conserved[5+n]*primitive[1];
-                        }
-                    }
-                    
-                },
-
-                [=] AMREX_GPU_DEVICE (int i, int j, int k) {
-
-                    GpuArray<Real,MAX_SPECIES> conserved;
-                    GpuArray<Real,MAX_SPECIES> primitive;
-                    GpuArray<Real,MAX_SPECIES> Yk;
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
     
-                    // interpolate conserved quantities to faces
-                    for (int l=0; l<nvars_gpu; ++l) {
-                        conserved[l] = wgt1*(cons(i,j,k,l)+cons(i,j-1,k,l)) - wgt2*(cons(i,j-2,k,l)+cons(i,j+1,k,l));
-                    }
+                // interpolate conserved quantities to faces
+                for (int l=0; l<nvars_gpu; ++l) {
+                    conserved[l] = wgt1*(cons(i,j,k,l)+cons(i,j-1,k,l)) - wgt2*(cons(i,j-2,k,l)+cons(i,j+1,k,l));
+                }
 
-                    // compute velocities
-                    for (int l=1; l<4; ++l) {
-                        primitive[l] = conserved[l]/conserved[0];
-                    }
+                // compute velocities
+                for (int l=1; l<4; ++l) {
+                    primitive[l] = conserved[l]/conserved[0];
+                }
 
-                    // want sum of specden == rho
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = conserved[5+n]/conserved[0];
+                }
+
+                // compute temperature
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+                Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
+                GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
+
+                // compute pressure
+                GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
+
+                yflux(i,j,k,1) += conserved[0]*primitive[2];
+                yflux(i,j,k,2) += conserved[0]*primitive[1]*primitive[2];
+                yflux(i,j,k,3) += conserved[0]*primitive[2]*primitive[2]+primitive[5];
+                yflux(i,j,k,4) += conserved[0]*primitive[3]*primitive[2]  ;
+           
+                yflux(i,j,k,5) += primitive[2]*conserved[4] + primitive[5]*primitive[2];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
                     for (int n=0; n<nspecies_gpu; ++n) {
-                        Yk[n] = conserved[5+n]/conserved[0];
+                        yflux(i,j,k,5+n) += conserved[5+n]*primitive[2];
                     }
-
-                    // compute temperature
-                    Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
-                    Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
-                    GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
-
-                    // compute pressure
-                    GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
-
-                    yflux(i,j,k,1) += conserved[0]*primitive[1];
-                    yflux(i,j,k,2) += conserved[0]*(primitive[1]*primitive[1])+primitive[5];
-                    yflux(i,j,k,3) += conserved[0]*primitive[1]*primitive[2];
-                    yflux(i,j,k,4) += conserved[0]*primitive[1]*primitive[3];
-                    yflux(i,j,k,5) += primitive[1]*conserved[4] + primitive[5]*primitive[1];
-
-                    if (algorithm_type_gpu == 2) { // Add advection of concentration
-                        for (int n=0; n<nspecies_gpu; ++n) {
-                            yflux(i,j,k,5+n) += conserved[5+n]*primitive[1];
-                        }
-                    }
-                },
+                }
+            },
                 
-                [=] AMREX_GPU_DEVICE (int i, int j, int k) {
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) {
 
-                    GpuArray<Real,MAX_SPECIES> conserved;
-                    GpuArray<Real,MAX_SPECIES> primitive;
-                    GpuArray<Real,MAX_SPECIES> Yk;
+                GpuArray<Real,MAX_SPECIES> conserved;
+                GpuArray<Real,MAX_SPECIES> primitive;
+                GpuArray<Real,MAX_SPECIES> Yk;
     
-                    // interpolate conserved quantities to faces
-                    for (int l=0; l<nvars_gpu; ++l) {
-                        conserved[l] = wgt1*(cons(i,j,k,l)+cons(i,j,k-1,l)) - wgt2*(cons(i,j,k-2,l)+cons(i,j,k+1,l));
-                    }
+                // interpolate conserved quantities to faces
+                for (int l=0; l<nvars_gpu; ++l) {
+                    conserved[l] = wgt1*(cons(i,j,k,l)+cons(i,j,k-1,l)) - wgt2*(cons(i,j,k-2,l)+cons(i,j,k+1,l));
+                }
 
-                    // compute velocities
-                    for (int l=1; l<4; ++l) {
-                        primitive[l] = conserved[l]/conserved[0];
-                    }
+                // compute velocities
+                for (int l=1; l<4; ++l) {
+                    primitive[l] = conserved[l]/conserved[0];
+                }
 
-                    // want sum of specden == rho
+                // want sum of specden == rho
+                for (int n=0; n<nspecies_gpu; ++n) {
+                    Yk[n] = conserved[5+n]/conserved[0];
+                }
+
+                // compute temperature
+                Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
+                Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
+                GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
+
+                // compute pressure
+                GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
+
+
+                zflux(i,j,k,0) += conserved[0]*primitive[3];
+                zflux(i,j,k,1) += conserved[0]*primitive[1]*primitive[3];
+                zflux(i,j,k,2) += conserved[0]*primitive[2]*primitive[3];
+                zflux(i,j,k,3) += conserved[0]*primitive[3]*primitive[3]+primitive[5];
+
+                zflux(i,j,k,4) += primitive[3]*conserved[4] + primitive[5]*primitive[3];
+
+                if (algorithm_type_gpu == 2) { // Add advection of concentration
                     for (int n=0; n<nspecies_gpu; ++n) {
-                        Yk[n] = conserved[5+n]/conserved[0];
+                        zflux(i,j,k,5+n) += conserved[5+n]*primitive[3];
                     }
-
-                    // compute temperature
-                    Real vsqr = primitive[1]*primitive[1] + primitive[2]*primitive[2] + primitive[3]*primitive[3];
-                    Real intenergy = conserved[4]/conserved[0] - 0.5*vsqr;
-                    GetTemperature(intenergy, Yk, primitive[4], nspecies_gpu, hcv_gpu);
-
-                    // compute pressure
-                    GetPressureGas(primitive[5], Yk, conserved[0], primitive[4], nspecies_gpu, Runiv_gpu, molmass_gpu);
-
-                    zflux(i,j,k,1) += conserved[0]*primitive[1];
-                    zflux(i,j,k,2) += conserved[0]*(primitive[1]*primitive[1])+primitive[5];
-                    zflux(i,j,k,3) += conserved[0]*primitive[1]*primitive[2];
-                    zflux(i,j,k,4) += conserved[0]*primitive[1]*primitive[3];
-                    zflux(i,j,k,5) += primitive[1]*conserved[4] + primitive[5]*primitive[1];
-
-                    if (algorithm_type_gpu == 2) { // Add advection of concentration
-                        for (int n=0; n<nspecies_gpu; ++n) {
-                            zflux(i,j,k,5+n) += conserved[5+n]*primitive[1];
-                        }
-                    }
-                });
-
-#else
-            const Box& bx = mfi.tilebox();
+                }
+            });
             
-            hyp_flux_cons(ARLIM_3D(bx.loVect()), ARLIM_3D(bx.hiVect()),
-                          cons_in[mfi].dataPtr(),  
-                          prim_in[mfi].dataPtr(),    
-                          flux_in[0][mfi].dataPtr(),
-                          flux_in[1][mfi].dataPtr(),
-#if (AMREX_SPACEDIM == 3)
-                          flux_in[2][mfi].dataPtr(),
-#endif
-                          ZFILL(dx));
-#endif
-                
-            }
         }
-    
-    
+    }
 }
