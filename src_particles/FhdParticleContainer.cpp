b58fa5ff0dea66a2db85b9a4277cbf24805df73e
Daniel Ladiges <drladiges@lbl.gov>
2020-07-10 14:54:03 -0700

    Porting particle force calculation to main branch

--- src_particles/FhdParticleContainer.cpp
+++ src_particles/FhdParticleContainer.cpp
@@ -257,13 +257,13 @@ void FhdParticleContainer::computeForcesNL(const MultiFab& charge, const MultiFa
 
         if(sr_tog==1) 
         {
-                amrex_compute_forces_nl(particles.data(), &Np, 
+                compute_forces_nl_gpu(particles.data(), &Np, 
                                         neighbors[lev][index].dataPtr(), &Nn,
                                         neighbor_list[lev][index].dataPtr(), &size, &rcount);
         }
         if(es_tog==3)
         {
-                amrex_compute_p3m_sr_correction_nl(particles.data(), &Np, 
+                compute_p3m_sr_correction_nl_gpu(particles.data(), &Np, 
                                         neighbors[lev][index].dataPtr(), &Nn,
                                         neighbor_list[lev][index].dataPtr(), &size, &rcount,
                                         BL_TO_FORTRAN_3D(charge[pti]),BL_TO_FORTRAN_3D(coords[pti]), ARLIM_3D(tile_box.loVect()), ARLIM_3D(tile_box.hiVect()), ZFILL(dx)); 
