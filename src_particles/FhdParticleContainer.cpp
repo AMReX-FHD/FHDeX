e996f99622a8fec6c19a35fea71249b39a3a4cda
Daniel Ladiges <drladiges@lbl.gov>
2020-08-10 11:14:05 -0700

    DISCOS boundary intersection detection now C++

--- src_particles/FhdParticleContainer.cpp
+++ src_particles/FhdParticleContainer.cpp
@@ -527,7 +527,8 @@ void FhdParticleContainer::MoveIonsGPU1(const Real dt, const Real* dxFluid, cons
 
                 while(runtime > 0)
                 {
-                    find_inter(&part, &runtime, paramPlaneList, &paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
+                    //find_inter(&part, &runtime, paramPlaneList, &paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
+                    find_inter_gpu(part, runtime, paramPlaneList, paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
                     
                     for (int d=0; d<AMREX_SPACEDIM; ++d)
                     {
@@ -541,12 +542,14 @@ void FhdParticleContainer::MoveIonsGPU1(const Real dt, const Real* dxFluid, cons
                     if(intsurf > 0)
                     {
 
-                        const paramPlane& surf = paramPlaneList[intsurf-1];
+                        const paramPlane& surf = paramPlaneList[intsurf]; //intsurf-1 for fortran find_inter, intsurf for c++ find_inter_gpu
 
                         if(surf.periodicity == 0)
                         {
                            Real dummy = 1;
+                           //std::cout << "Pre: " << part.rdata(FHD_realData::velx) << ", " << part.rdata(FHD_realData::vely) << ", " << part.rdata(FHD_realData::velz) << ", " << intside << "\n";
                            app_bc(&surf, &part, &intside, domsize, &push, &dummy, &dummy);
+                           //std::cout << "Post: " << part.rdata(FHD_realData::velx) << ", " << part.rdata(FHD_realData::vely) << ", " << part.rdata(FHD_realData::velz) << ", " << intside << "\n";
 
                            runtime = runtime - inttime;
 
@@ -589,7 +592,7 @@ void FhdParticleContainer::MoveIonsGPU1(const Real dt, const Real* dxFluid, cons
                 
         }
 
-
+//cin.get();
  
         //Need to add midpoint rejecting feature here.
         InterpolateMarkersGpu(0, dxFluid, umac, RealFaceCoords, check);
@@ -687,7 +690,8 @@ void FhdParticleContainer::MoveIonsGPU1(const Real dt, const Real* dxFluid, cons
 
             while(runtime > 0)
             {
-                find_inter(&part, &runtime, paramPlaneList, &paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
+                //find_inter(&part, &runtime, paramPlaneList, &paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
+                find_inter_gpu(part, runtime, paramPlaneList, paramPlaneCount, &intsurf, &inttime, &intside, ZFILL(plo), ZFILL(phi));
                 
                 for (int d=0; d<AMREX_SPACEDIM; ++d)
                 {
@@ -702,7 +706,7 @@ void FhdParticleContainer::MoveIonsGPU1(const Real dt, const Real* dxFluid, cons
                 runtime = runtime - inttime;
                 if(intsurf > 0)
                 {
-                    const paramPlane& surf = paramPlaneList[intsurf-1];
+                    const paramPlane& surf = paramPlaneList[intsurf]; //intsurf-1 for fortran find_inter, intsurf for c++ find_inter_gpu
 
                     Real dummy = 1;
                     app_bc(&surf, &part, &intside, domsize, &push, &dummy, &dummy);
