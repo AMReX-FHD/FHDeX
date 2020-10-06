b6048389423b25b06747d8f95aa70cc0ea0ffc9b
Johannes Blaschke <jpblaschke@lbl.gov>
2020-10-05 18:54:00 -0700

    make verbose

--- exec/lib_hydro/cmake/test/build.fish
+++ exec/lib_hydro/cmake/test/build.fish
@@ -3,5 +3,5 @@
 mkdir -p dist
 pushd dist
 cmake .. -DAMReX_ROOT=/Users/blaschke/Science/amrex/dist -DCMAKE_C_COMPILER=(which gcc-9) -DCMAKE_CXX_COMPILER=(which g++-9) -DCMAKE_Fortran_COMPILER=(which gfortran-9)
-make -j
+make -j VERBOSE=TRUE
 popd
