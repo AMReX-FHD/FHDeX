ed29885bd55a90bb069a036b2ae8f5072cd8e303
Changho Kim <changhokim@gigan.lbl.gov>
2021-01-10 05:08:05 -0800

    exec/compressible_mui/RUN_COadsPt111_coverage: coverage.sh

--- exec/compressible_mui/RUN_COadsPt111_coverage/coverage.sh
+++ exec/compressible_mui/RUN_COadsPt111_coverage/coverage.sh
@@ -11,5 +11,5 @@ fi
 
 grep -B1 "Loop" $log --no-group-separator | awk 'NR % 2 == 1{printf("%e\t%d\t%d\n",$1,$6,$7,$8)}' > $res
 
-gnuplot -persist -e 'plot "res.coverage" u 1:3 w l'
-#gnuplot -persist -e 'plot [0:1e-7] "res.coverage" u 1:3 w l,1.519198e-01*(1-exp(-3.451071e+07*x))*1200*1200'
+#gnuplot -persist -e 'plot "res.coverage" u 1:3 w l'
+gnuplot -persist -e 'plot "res.coverage" u 1:3 w p,1.519198e-01*(1-exp(-3.451071e+07*x))*1200*1200'
