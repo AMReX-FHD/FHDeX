20177324b96986a37dd7180f3eb8f1060cd7025d
Changho Kim <changhokim@gigan.lbl.gov>
2021-01-09 22:52:58 -0800

    exec/compressible_mui/RUN_COadsPt111_coverage: params.sh, coverage.sh

--- exec/compressible_mui/RUN_COadsPt111_coverage/params.py
+++ exec/compressible_mui/RUN_COadsPt111_coverage/params.py
@@ -107,7 +107,8 @@ print "- temp in K = %e" % temp
 print "- k1des = %e" % k1des
 
 theta1 = k1ads/(k1ads+k1des)
-print "- eq coverage of spec1: %e\n" % theta1
+print "- eq coverage of spec1: %e" % theta1
+print "- theta(t) = %e*(1-exp(-%e*t))  for theta(0)=0\n" % (theta1,k1ads+k1des)
 
 ##########
 
