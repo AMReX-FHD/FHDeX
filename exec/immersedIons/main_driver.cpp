ee7dbab8ec60e519d06a97b14b4b235f022fa6c1
jgalenwang <jgalenwang@lbl.gov>
2021-03-18 03:00:21 -0700

    fixing bugs

--- exec/immersedIons/main_driver.cpp
+++ exec/immersedIons/main_driver.cpp
@@ -420,10 +420,10 @@ void main_driver(const char* argv)
                 << " wet radius: " << wetRad[i] << "\n"
                 << " dry radius: " << (k_B*T_init[0])/(6*3.14159265359*(ionParticle[i].dryDiff)*visc_coef) << "\n";
 
-        if (ionParticle[i].dryDiff < 0) {
-            Print() << "Negative dry diffusion in species " << i << "\n";
-            Abort();
-        }
+        //if (ionParticle[i].dryDiff < 0) {
+        //    Print() << "Negative dry diffusion in species " << i << "\n";
+        //    Abort();
+        //}
 
         ionParticle[i].Neff = particle_neff; // From DSMC, this will be set to 1 for electolyte calcs
         ionParticle[i].R = k_B/ionParticle[i].m; //used a lot in kinetic stats cals, bu not otherwise necessary for electrolytes
@@ -871,11 +871,11 @@ void main_driver(const char* argv)
         }
 
 
-//        if(istep == 1)
-//        {
-//            particles.SetPosition(1, prob_hi[0]*0.25, prob_hi[1]*0.25, prob_hi[2]*0.5);
-//            particles.SetPosition(2, prob_hi[0]*0.25, prob_hi[1]*0.25+2*dx[0], prob_hi[2]*0.5);
-//        }
+        if(istep == 1)
+        {
+            particles.SetPosition(1, prob_hi[0]*0.25, prob_hi[1]*0.25, prob_hi[2]*0.5);
+            particles.SetPosition(2, prob_hi[0]*0.25, prob_hi[1]*0.25+2*dx[1], prob_hi[2]*0.5);
+        }
 
     
         //Most of these functions are sensitive to the order of execution. We can fix this, but for now leave them in this order.
@@ -1182,7 +1182,7 @@ void main_driver(const char* argv)
                             potential, potentialM, potentialV);
         }
 
-        //particles.PrintParticles();
+        particles.PrintParticles();
 
         // timer for time step
         Real time2 = ParallelDescriptor::second() - time1;
