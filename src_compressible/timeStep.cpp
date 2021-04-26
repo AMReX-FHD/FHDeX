894ba54b44c9f727a87b5f739738708b7f585f29
isriva <ishan66@gmail.com>
2021-04-26 13:34:01 -0700

    species flux set to 0 at the walls

--- src_compressible/timeStep.cpp
+++ src_compressible/timeStep.cpp
@@ -105,6 +105,9 @@ void RK3step(MultiFab& cu, MultiFab& cup, MultiFab& cup2, MultiFab& cup3,
     calculateFlux(cu, prim, eta, zeta, kappa, chi, D, flux, stochFlux, cornx, corny, cornz,
                   visccorn, rancorn, geom, stoch_weights, dt);
 
+    // Set species flux to zero at the walls
+    BCWallSpeciesFlux(flux,geom);
+
     for ( MFIter mfi(cu,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
         
         const Box& bx = mfi.tilebox();
@@ -199,6 +202,9 @@ void RK3step(MultiFab& cu, MultiFab& cup, MultiFab& cup2, MultiFab& cup3,
     calculateFlux(cup, prim, eta, zeta, kappa, chi, D, flux, stochFlux, cornx, corny, cornz,
                   visccorn, rancorn, geom, stoch_weights, dt);
 
+    // Set species flux to zero at the walls
+    BCWallSpeciesFlux(flux,geom);
+
     for ( MFIter mfi(cu,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
         
         const Box& bx = mfi.tilebox();
@@ -292,6 +298,9 @@ void RK3step(MultiFab& cu, MultiFab& cup, MultiFab& cup2, MultiFab& cup3,
 
     calculateFlux(cup2, prim, eta, zeta, kappa, chi, D, flux, stochFlux, cornx, corny, cornz,
                   visccorn, rancorn, geom, stoch_weights, dt);
+
+    // Set species flux to zero at the walls
+    BCWallSpeciesFlux(flux,geom);
 
     for ( MFIter mfi(cu,TilingIfNotGPU()); mfi.isValid(); ++mfi) {
         
