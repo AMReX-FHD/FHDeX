c75ff0d6bd146bb8d0d9b638df5ebe3e6183e435
isriva <ishan66@gmail.com>
2021-05-20 09:23:44 -0700

    small changes to the SF routine

--- exec/compressible_stag/main_driver.cpp
+++ exec/compressible_stag/main_driver.cpp
@@ -398,38 +398,38 @@ void main_driver(const char* argv)
             MultiFab prim_temp;
             prim_temp.define(ba,dmap,nprimvars,ngc);
             prim_temp.setVal(0.0);
-            ComputeVerticalAverage(prim_temp, primVertAvg, geom, project_dir, 0, structVarsPrim);
+            ComputeVerticalAverage(prim_temp, primVertAvg, geom, project_dir, 0, nprimvars);
             MultiFab primVertAvgRot = RotateFlattenedMF(primVertAvg);
             BoxArray ba_flat = primVertAvgRot.boxArray();
             const DistributionMapping& dmap_flat = primVertAvg.DistributionMap();
             {
-                IntVect dom_lo(AMREX_D_DECL(0,0,0));
-                IntVect dom_hi;
+                IntVect dom_lo_flat(AMREX_D_DECL(0,0,0));
+                IntVect dom_hi_flat;
 #if (AMREX_SPACEDIM == 2)
                 if (project_dir == 0) {
-                    dom_hi[0] = n_cells[1]-1;
-                    dom_hi[1] = 0;
+                    dom_hi_flat[0] = n_cells[1]-1;
+                    dom_hi_flat[1] = 0;
                 }
                 else if (project_dir == 1) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = 0;
                 }
 #elif (AMREX_SPACEDIM == 3)
                 if (project_dir == 0) {
-                    dom_hi[0] = n_cells[1]-1;
-                    dom_hi[1] = n_cells[2]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[1]-1;
+                    dom_hi_flat[1] = n_cells[2]-1;
+                    dom_hi_flat[2] = 0;
                 } else if (project_dir == 1) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = n_cells[2]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = n_cells[2]-1;
+                    dom_hi_flat[2] = 0;
                 } else if (project_dir == 2) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = n_cells[1]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = n_cells[1]-1;
+                    dom_hi_flat[2] = 0;
                 }
 #endif
-                Box domain(dom_lo, dom_hi);
+                Box domain_flat(dom_lo_flat, dom_hi_flat);
 
                 // This defines the physical box
                 Vector<Real> projected_hi(AMREX_SPACEDIM);
@@ -451,16 +451,19 @@ void main_driver(const char* argv)
         
                 projected_hi[AMREX_SPACEDIM-1] = prob_hi[project_dir] / n_cells[project_dir];
 
-                RealBox real_box({AMREX_D_DECL(     prob_lo[0],     prob_lo[1],     prob_lo[2])},
+                RealBox real_box_flat({AMREX_D_DECL(     prob_lo[0],     prob_lo[1],     prob_lo[2])},
                                  {AMREX_D_DECL(projected_hi[0],projected_hi[1],projected_hi[2])});
           
                 // This defines a Geometry object
-                geom_flat.define(domain,&real_box,CoordSys::cartesian,is_periodic.data());
+                geom_flat.define(domain_flat,&real_box_flat,CoordSys::cartesian,is_periodic.data());
+                amrex::Print() << "nx, ny, nz:\t" << ba_flat[0].size()[0] << ", " << ba_flat[0].size()[1]  << ", " << ba_flat[0].size()[2] << std::endl;
+                amrex::Print() << "Lx, Ly, Lz:\t" << domain_flat.length(0)  << ", " << domain_flat.length(1)  << ", " << domain_flat.length(2) << std::endl;
+                amrex::Print() << "nbx, nby, nbz:\t" << domain_flat.length(0)/ba_flat[0].size()[0] << ", " << domain_flat.length(1)/ba_flat[0].size()[1]  << ", " << domain_flat.length(2)/ba_flat[0].size()[2] << std::endl;
             }
 
             if (do_slab_sf == 0) {
-                structFactPrimVerticalAverage.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim);
-                structFactConsVerticalAverage.define(ba_flat,dmap_flat,cons_var_names,var_scaling_cons);
+                structFactPrimVerticalAverage.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim,2);
+                structFactConsVerticalAverage.define(ba_flat,dmap_flat,cons_var_names,var_scaling_cons,2);
             }
             else {
                 structFactPrimVerticalAverage0.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim);
@@ -597,38 +600,38 @@ void main_driver(const char* argv)
         if(project_dir >= 0){
             MultiFab primVertAvg;  // flattened multifab defined below
             prim.setVal(0.0);
-            ComputeVerticalAverage(prim, primVertAvg, geom, project_dir, 0, structVarsPrim);
+            ComputeVerticalAverage(prim, primVertAvg, geom, project_dir, 0, nprimvars);
             MultiFab primVertAvgRot = RotateFlattenedMF(primVertAvg);
             BoxArray ba_flat = primVertAvgRot.boxArray();
             const DistributionMapping& dmap_flat = primVertAvg.DistributionMap();
             {
-                IntVect dom_lo(AMREX_D_DECL(0,0,0));
-                IntVect dom_hi;
+                IntVect dom_lo_flat(AMREX_D_DECL(0,0,0));
+                IntVect dom_hi_flat;
 #if (AMREX_SPACEDIM == 2)
                 if (project_dir == 0) {
-                    dom_hi[0] = n_cells[1]-1;
-                    dom_hi[1] = 0;
+                    dom_hi_flat[0] = n_cells[1]-1;
+                    dom_hi_flat[1] = 0;
                 }
                 else if (project_dir == 1) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = 0;
                 }
 #elif (AMREX_SPACEDIM == 3)
                 if (project_dir == 0) {
-                    dom_hi[0] = n_cells[1]-1;
-                    dom_hi[1] = n_cells[2]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[1]-1;
+                    dom_hi_flat[1] = n_cells[2]-1;
+                    dom_hi_flat[2] = 0;
                 } else if (project_dir == 1) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = n_cells[2]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = n_cells[2]-1;
+                    dom_hi_flat[2] = 0;
                 } else if (project_dir == 2) {
-                    dom_hi[0] = n_cells[0]-1;
-                    dom_hi[1] = n_cells[1]-1;
-                    dom_hi[2] = 0;
+                    dom_hi_flat[0] = n_cells[0]-1;
+                    dom_hi_flat[1] = n_cells[1]-1;
+                    dom_hi_flat[2] = 0;
                 }
 #endif
-                Box domain(dom_lo, dom_hi);
+                Box domain_flat(dom_lo_flat, dom_hi_flat);
 
                 // This defines the physical box
                 Vector<Real> projected_hi(AMREX_SPACEDIM);
@@ -650,16 +653,16 @@ void main_driver(const char* argv)
         
                 projected_hi[AMREX_SPACEDIM-1] = prob_hi[project_dir] / n_cells[project_dir];
 
-                RealBox real_box({AMREX_D_DECL(     prob_lo[0],     prob_lo[1],     prob_lo[2])},
+                RealBox real_box_flat({AMREX_D_DECL(     prob_lo[0],     prob_lo[1],     prob_lo[2])},
                                  {AMREX_D_DECL(projected_hi[0],projected_hi[1],projected_hi[2])});
           
                 // This defines a Geometry object
-                geom_flat.define(domain,&real_box,CoordSys::cartesian,is_periodic.data());
+                geom_flat.define(domain_flat,&real_box_flat,CoordSys::cartesian,is_periodic.data());
             }
 
             if (do_slab_sf == 0) {
-                structFactPrimVerticalAverage.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim);
-                structFactConsVerticalAverage.define(ba_flat,dmap_flat,cons_var_names,var_scaling_cons);
+                structFactPrimVerticalAverage.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim,2);
+                structFactConsVerticalAverage.define(ba_flat,dmap_flat,cons_var_names,var_scaling_cons,2);
             }
             else {
                 structFactPrimVerticalAverage0.define(ba_flat,dmap_flat,prim_var_names,var_scaling_prim);
@@ -923,8 +926,13 @@ void main_driver(const char* argv)
                     ComputeVerticalAverage(structFactConsMF, consVertAvg, geom, project_dir, 0, structVarsCons);
                     MultiFab primVertAvgRot = RotateFlattenedMF(primVertAvg);
                     MultiFab consVertAvgRot = RotateFlattenedMF(consVertAvg);
-                    structFactPrimVerticalAverage.FortStructure(primVertAvg,geom_flat);
-                    structFactConsVerticalAverage.FortStructure(consVertAvg,geom_flat);
+                    amrex::Print() << "entering stage 1" << std::endl;
+                    amrex::Print() << geom_flat << std::endl;
+                    amrex::Print() << "entering stage 1a" << std::endl;
+                    structFactPrimVerticalAverage.FortStructure(primVertAvgRot,geom_flat);
+                    amrex::Print() << "exiting stage 1" << std::endl;
+                    structFactConsVerticalAverage.FortStructure(consVertAvgRot,geom_flat);
+                    amrex::Print() << "exiting stage 2" << std::endl;
                 }
                 else {
                     MultiFab primVertAvg0;  // flattened multifab defined below
@@ -939,10 +947,10 @@ void main_driver(const char* argv)
                     MultiFab primVertAvgRot1 = RotateFlattenedMF(primVertAvg1);
                     MultiFab consVertAvgRot0 = RotateFlattenedMF(consVertAvg0);
                     MultiFab consVertAvgRot1 = RotateFlattenedMF(consVertAvg1);
-                    structFactPrimVerticalAverage0.FortStructure(primVertAvg0,geom_flat);
-                    structFactPrimVerticalAverage1.FortStructure(primVertAvg1,geom_flat);
-                    structFactConsVerticalAverage0.FortStructure(consVertAvg0,geom_flat);
-                    structFactConsVerticalAverage1.FortStructure(consVertAvg1,geom_flat);
+                    structFactPrimVerticalAverage0.FortStructure(primVertAvgRot0,geom_flat);
+                    structFactPrimVerticalAverage1.FortStructure(primVertAvgRot1,geom_flat);
+                    structFactConsVerticalAverage0.FortStructure(consVertAvgRot0,geom_flat);
+                    structFactConsVerticalAverage1.FortStructure(consVertAvgRot1,geom_flat);
                 }
             }
         }
