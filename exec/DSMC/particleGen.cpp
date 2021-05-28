aa4389cf3bde6366d885b21395fb4c0cff1b87e6
Daniel Ladiges <drladiges@lbl.gov>
2021-05-28 11:54:04 -0700

    DSMC framework

---
+++ exec/DSMC/particleGen.cpp
@@ -0,0 +1,82 @@
+#include "INS_functions.H"
+#include "common_functions.H"
+#include "DsmcParticleContainer.H"
+#include <sstream>
+#include <string>
+#include <fstream>
+
+void FhdParticleContainer::InitParticles()
+{
+    const int lev = 0;
+    const Geometry& geom = Geom(lev);
+
+    int pcount = 0;
+
+    bool proc0_enter = true;
+        
+    for (MFIter mfi = MakeMFIter(lev, true); mfi.isValid(); ++mfi) {
+        
+        const Box& tile_box  = mfi.tilebox();
+        const RealBox tile_realbox{tile_box, geom.CellSize(), geom.ProbLo()};
+        const int grid_id = mfi.index();
+        const int tile_id = mfi.LocalTileIndex();
+        auto& particle_tile = GetParticles(lev)[std::make_pair(grid_id,tile_id)];
+
+        //Assuming tile=box for now, i.e. no tiling.
+        IntVect smallEnd = tile_box.smallEnd();
+        IntVect bigEnd = tile_box.bigEnd();       
+
+        if(ParallelDescriptor::MyProc() == 0 && mfi.LocalTileIndex() == 0 && proc0_enter) {
+
+            proc0_enter = false;
+
+            std::ifstream particleFile("particles.dat");
+ //           Print() << "SPEC TOTAL: " << particleInfo[0].total << "\n";
+            for(int i_spec=0; i_spec < nspecies; i_spec++) {
+                for (int i_part=0; i_part<properties[i_spec].total;i_part++) {
+                    ParticleType p;
+                    p.id()  = ParticleType::NextID();
+ //                   std::cout << "ID: " << p.id() << "\n";
+                    p.cpu() = ParallelDescriptor::MyProc();
+                    p.idata(FHD_intData::sorted) = 0;
+
+                    if(particle_placement == 1)
+                    {
+                        particleFile >> p.pos(0);                       
+                        particleFile >> p.pos(1);
+                        particleFile >> p.pos(2);
+
+                    }else
+                    {
+
+                        p.pos(0) = prob_lo[0] + amrex::Random()*(prob_hi[0]-prob_lo[0]);
+                        p.pos(1) = prob_lo[1] + amrex::Random()*(prob_hi[1]-prob_lo[1]);
+                        p.pos(2) = prob_lo[2] + amrex::Random()*(prob_hi[2]-prob_lo[2]);
+
+                    }
+
+                    p.rdata(FHD_realData::velx) = sqrt(properties[i_spec].R*properties[i_spec].T)*amrex::RandomNormal(0.,1.);
+                    p.rdata(FHD_realData::vely) = sqrt(properties[i_spec].R*properties[i_spec].T)*amrex::RandomNormal(0.,1.);
+                    p.rdata(FHD_realData::velz) = sqrt(properties[i_spec].R*properties[i_spec].T)*amrex::RandomNormal(0.,1.);
+
+                    p.rdata(FHD_realData::boostx) = 0;
+                    p.rdata(FHD_realData::boosty) = 0;
+                    p.rdata(FHD_realData::boostz) = 0;
+
+                    p.rdata(FHD_realData::R) = properties[i_spec].R;
+
+                    particle_tile.push_back(p);
+
+                    pcount++;
+                }
+            }
+
+            particleFile.close();
+        }
+    }
+
+    Redistribute();
+}
+
+
+
--- src_particles/DsmcParticleContainer.cpp
+++ src_particles/DsmcParticleContainer.cpp
@@ -1,17 +1,64 @@
 #include "DsmcParticleContainer.H"
 
-#include "particle_functions_K.H"
+//#include "particle_functions_K.H"
 #include "paramplane_functions_K.H"
 #include <math.h>
 
 
-DsmcParticleContainer::DsmcParticleContainer(const Geometry & geom,
+FhdParticleContainer::FhdParticleContainer(const Geometry & geom,
                               const DistributionMapping & dmap,
                               const BoxArray            & ba,
                               int ncells)
-    : NeighborParticleContainer<Dsmc_realData::count, Dsmc_intData::count> (geom, dmap, ba, ncells)
+    : NeighborParticleContainer<FHD_realData::count, FHD_intData::count> (geom, dmap, ba, ncells)
 {
-    BL_PROFILE_VAR("DsmcParticleContainer()",DsmcParticleContainer);
+    BL_PROFILE_VAR("FhdParticleContainer()",FhdParticleContainer);
+
+    realParticles = 0;
+    simParticles = 0;
+
+    for(int i=0;i<nspecies;i++) {
+        
+        properties[i].mass = mass[i];
+        properties[i].radius = diameter[i]/2.0;
+        properties[i].Neff = particle_neff; // From DSMC, this will be set to 1 for electolyte calcs
+        properties[i].R = k_B/properties[i].mass; //used a lot in kinetic stats cals, bu not otherwise necessary for electrolytes
+        properties[i].T = T_init[i];
+
+        if (particle_count[i] >= 0) {
+
+            properties[i].total = particle_count[i];
+            properties[i].n0 = particle_neff*properties[i].total/domainVol;
+            
+            Print() << "Species " << i << " count " << properties[i].total << "\n";
+            Print() << "Species " << i << " n0 " << properties[i].total << "\n";
+        }
+        else {
+            properties[i].total = (int)amrex::Math::ceil(particle_n0[i]*domainVol/particle_neff);
+            properties[i].n0 = properties[i].total/domainVol;
+
+            Print() << "Species " << i << " count " << properties[i].total << "\n";
+            Print() << "Species " << i << " n0 adjusted to " << properties[i].n0 << "\n";
+        }
+
+        Print() << "Species " << i << "\n";
+        Print() << "Mass " << properties[i].mass << "\n";
+        Print() << "Radius " << properties[i].radius << "\n";
+
+        realParticles = realParticles + properties[i].total*particle_neff;
+        simParticles = simParticles + properties[i].total;
+
+    }
+
+    totalCollisionCells = n_cells[0]*n_cells[1]*n_cells[2];
+    domainVol = (prob_hi[0] - prob_lo[0])*(prob_hi[1] - prob_lo[1])*(prob_hi[2] - prob_lo[2]);
+
+    Print() << "Total real particles: " << realParticles << "\n";
+    Print() << "Total sim particles: " << simParticles << "\n";
+
+    Print() << "Collision cells: " << totalCollisionCells << "\n";
+    Print() << "Sim particles per cell: " << simParticles/totalCollisionCells << "\n";
+
+
 }
 
 
