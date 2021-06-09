b6708ca4df397da4ed45ba5f52169ba51a053c24
Andy Nonaka <AJNonaka@lbl.gov>
2021-06-09 13:55:15 -0700

    PDF WIP

--- exec/hydro/PDF/PDF.cpp
+++ exec/hydro/PDF/PDF.cpp
@@ -16,7 +16,9 @@ PrintUsage (const char* progName)
             << "This utility computes... " << std::endl << std::endl;
 
     Print() << "Usage:" << '\n';
-    Print() << progName << " infile=plotFileName " << '\n' << '\n';
+    Print() << progName << " <inputs>" << std::endl
+            << "OR" << std::endl
+            << progName << " infile=plotFileName ..." << '\n' << '\n';
 
     exit(1);
 }
@@ -31,19 +33,22 @@ main (int   argc,
         PrintUsage(argv[0]);
     }
 
-    // plotfile names for the coarse, fine, and subtracted output
+    // plotfile name
     std::string iFile;
 
-    // read in parameters from inputs file
+    // how many 2nd-derivatives to take
+    int nderivs;
+
+    // read in parameters from inputs file or command line
     ParmParse pp;
 
-    // coarse MultiFab
-    pp.query("infile", iFile);
-    if (iFile.empty())
-        amrex::Abort("You must specify `infile'");
+    // MultiFab
+    pp.get("infile", iFile);
 
-    // single-level for now
-    // AMR comes later, where we iterate over each level in isolation
+    // how many 2nd derivatives
+    pp.get("nderivs", nderivs);
+
+    // single-level only
 
     // for the Header
     std::string iFile2 = iFile;
@@ -53,7 +58,7 @@ main (int   argc,
     ifstream x;
     x.open(iFile2.c_str(), ios::in);
 
-    // read in first line of header
+    // read in first line of header (typically "HyperCLaw-V1.1" or similar)
     string str;
     x >> str;
 
@@ -76,6 +81,23 @@ main (int   argc,
         Abort();
     }
 
+    // read in time
+    Real time;
+    x >> time;
+
+    // read in finest level
+    int finest_level;
+    x >> finest_level;
+
+    // read in prob_lo and prob_hi
+    amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> prob_lo, prob_hi;
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        x >> prob_lo[i];        
+    }
+    for (int i=0; i<AMREX_SPACEDIM; ++i) {
+        x >> prob_hi[i];        
+    }
+    
     // now read in the plotfile data
     // check to see whether the user pointed to the plotfile base directory
     // or the data itself
@@ -93,50 +115,67 @@ main (int   argc,
     // note: fine could be the same resolution as coarse
     VisMF::Read(mf, iFile);
 
-    ncomp = mf.nComp();
-    Print() << "ncomp = " << ncomp << std::endl;
-
-    // check nodality
-    IntVect c_nodality = mf.ixType().toIntVect();
-    Print() << "nodality " << c_nodality << std::endl;
-
     // get boxArray
     BoxArray ba = mf.boxArray();
+    DistributionMapping dmap = mf.DistributionMap();
 
-    // minimalBox() computes a single box to enclose all the boxes
-    // enclosedCells() converts it to a cell-centered Box
-    Box bx_onegrid = ba.minimalBox().enclosedCells();
+    RealBox real_box({AMREX_D_DECL(prob_lo[0],prob_lo[1],prob_lo[2])},
+                     {AMREX_D_DECL(prob_hi[0],prob_hi[1],prob_hi[2])});
+            
+    Box domain = ba.minimalBox().enclosedCells();
 
-    // number of cells in the coarse domain
-    Print() << "npts in coarse domain = " << bx_onegrid.numPts() << std::endl;
-    long npts_coarsedomain = bx_onegrid.numPts();
+    Vector<int> is_periodic(AMREX_SPACEDIM,1);  // set to 1 (periodic)
+    
+    Geometry geom(domain,&real_box,CoordSys::cartesian,is_periodic.data());
+    
+    const Real* dx = geom.CellSize();
+  
+    MultiFab mf_grown(ba,dmap,AMREX_SPACEDIM,1);
+    MultiFab laplacian(ba,dmap,AMREX_SPACEDIM,1);
 
-    // BoxArray, DistributionMapping, and MultiFab with one grid
-    BoxArray ba_onegrid(bx_onegrid);
-    DistributionMapping dmap_onegrid(ba_onegrid);
-    MultiFab mf_onegrid(ba_onegrid,dmap_onegrid,ncomp,0);
+    // copy shifted velocity components from mf into mf_grown
+    Copy(mf_grown,mf,AMREX_SPACEDIM,0,AMREX_SPACEDIM,0);
 
-    // copy data into MultiFab with one grid
-    mf_onegrid.ParallelCopy(mf,0,0,ncomp,0,0);
+    // fill ghost cells of mf_grown
+    mf_grown.FillBoundary(geom.periodicity());
 
-    for ( MFIter mfi(mf_onegrid,false); mfi.isValid(); ++mfi ) {
+    for (int i=0; i<nderivs; ++i) {
+    
+    
+        for ( MFIter mfi(mf_grown,false); mfi.isValid(); ++mfi ) {
 
-        const Box& bx = mfi.validbox();
-        const auto lo = amrex::lbound(bx);
-        const auto hi = amrex::ubound(bx);
+            const Box& bx = mfi.validbox();
+            const auto lo = amrex::lbound(bx);
+            const auto hi = amrex::ubound(bx);
 
-        const Array4<Real>& mfdata = mf_onegrid.array(mfi);
+            const Array4<Real>& vel = mf_grown.array(mfi);
+            const Array4<Real>& lap = laplacian.array(mfi);
 
-        for (auto n=0; n<ncomp; ++n) {
-        for (auto k = lo.z; k <= hi.z; ++k) {
-        for (auto j = lo.y; j <= hi.y; ++j) {
-        for (auto i = lo.x; i <= hi.x; ++i) {
-            std::cout << i << " " << j << " " << k << " " << n << " " << mfdata(i,j,k,n) << "\n";
-        }
-        }
-        }
-        }
-        
+            for (auto n=0; n<AMREX_SPACEDIM; ++n) {
+            for (auto k = lo.z; k <= hi.z; ++k) {
+            for (auto j = lo.y; j <= hi.y; ++j) {
+            for (auto i = lo.x; i <= hi.x; ++i) {
+
+                lap(i,j,k,n) =  (vel(i+1,j,k,n) - 2.*vel(i,j,k,n) + vel(i-1,j,k,n)) / (dx[0]*dx[0])
+                               +(vel(i,j+1,k,n) - 2.*vel(i,j,k,n) + vel(i,j-1,k,n)) / (dx[1]*dx[1])
+#if (AMREX_SPACEDIM == 3)
+                               +(vel(i,j,k+1,n) - 2.*vel(i,j,k,n) + vel(i,j,k+1,n)) / (dx[2]*dx[2])
+#endif
+                    ;
+            }
+            }
+            }
+            }
+
     } // end MFIter
+
+    // copy lap into mf_grown
+    Copy(mf_grown,laplacian,0,0,AMREX_SPACEDIM,0);
+
+    // fill ghost cells of mf_grown
+    mf_grown.FillBoundary(geom.periodicity());
+    
+    
+    }
 
 }
