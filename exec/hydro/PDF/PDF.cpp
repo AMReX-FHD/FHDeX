36ea39a17205aee1d116d48e396f5c62c06d6607
Andy Nonaka <AJNonaka@lbl.gov>
2021-06-09 14:05:30 -0700

    PDF WIP

--- exec/hydro/PDF/PDF.cpp
+++ exec/hydro/PDF/PDF.cpp
@@ -76,8 +76,8 @@ main (int   argc,
     x >> dim;
 
     if (dim != AMREX_SPACEDIM) {
-        Print() << "\nError: you are using a " << AMREX_SPACEDIM << "D build to open a "
-                << dim << "D plotfile\n\n";
+        Print() << "\nError: you are using a " << AMREX_SPACEDIM
+                << "D build to open a " << dim << "D plotfile\n\n";
         Abort();
     }
 
@@ -111,20 +111,22 @@ main (int   argc,
     // storage for the input coarse and fine MultiFabs
     MultiFab mf;
 
-    // read in plotfiles, 'coarse' and 'fine' to MultiFabs
-    // note: fine could be the same resolution as coarse
+    // read in plotfile mf to MultiFab
     VisMF::Read(mf, iFile);
 
-    // get boxArray
+    // get BoxArray and DistributionMapping
     BoxArray ba = mf.boxArray();
     DistributionMapping dmap = mf.DistributionMap();
 
+    // physical dimensions of problem
     RealBox real_box({AMREX_D_DECL(prob_lo[0],prob_lo[1],prob_lo[2])},
                      {AMREX_D_DECL(prob_hi[0],prob_hi[1],prob_hi[2])});
-            
+
+    // single box with the enire domain
     Box domain = ba.minimalBox().enclosedCells();
 
-    Vector<int> is_periodic(AMREX_SPACEDIM,1);  // set to 1 (periodic)
+    // set to 1 (periodic)
+    Vector<int> is_periodic(AMREX_SPACEDIM,1);
     
     Geometry geom(domain,&real_box,CoordSys::cartesian,is_periodic.data());
     
@@ -139,8 +141,7 @@ main (int   argc,
     // fill ghost cells of mf_grown
     mf_grown.FillBoundary(geom.periodicity());
 
-    for (int i=0; i<nderivs; ++i) {
-    
+    for (int i=0; i<nderivs; ++i) {    
     
         for ( MFIter mfi(mf_grown,false); mfi.isValid(); ++mfi ) {
 
@@ -167,15 +168,77 @@ main (int   argc,
             }
             }
 
+        } // end MFIter
+
+        // copy lap into mf_grown
+        Copy(mf_grown,laplacian,0,0,AMREX_SPACEDIM,0);
+
+        // fill ghost cells of mf_grown
+        mf_grown.FillBoundary(geom.periodicity());
+        
+    } // end loop over nderivs
+        
+    // store sum of velocity components
+    MultiFab sum_laplacian(ba,dmap,0,0);
+    sum_laplacian.setVal(0.);
+
+    // compute sum over velocity components
+    for ( MFIter mfi(mf_grown,false); mfi.isValid(); ++mfi ) {
+
+        const Box& bx = mfi.validbox();
+        const auto lo = amrex::lbound(bx);
+        const auto hi = amrex::ubound(bx);
+        
+        const Array4<Real>& lap = laplacian.array(mfi);
+        const Array4<Real>& sum_lap = sum_laplacian.array(mfi);
+
+        for (auto n=0; n<AMREX_SPACEDIM; ++n) {
+        for (auto k = lo.z; k <= hi.z; ++k) {
+        for (auto j = lo.y; j <= hi.y; ++j) {
+        for (auto i = lo.x; i <= hi.x; ++i) {
+
+                sum_lap(i,j,k) += lap(i,j,k,n);
+                
+        }
+        }
+        }
+        }
+
     } // end MFIter
 
-    // copy lap into mf_grown
-    Copy(mf_grown,laplacian,0,0,AMREX_SPACEDIM,0);
+    int nbins = 100;
 
-    // fill ghost cells of mf_grown
-    mf_grown.FillBoundary(geom.periodicity());
-    
-    
+    Vector<Real> bins(nbins,0.);
+
+    // compute sum over velocity components
+    for ( MFIter mfi(mf_grown,false); mfi.isValid(); ++mfi ) {
+
+        const Box& bx = mfi.validbox();
+        const auto lo = amrex::lbound(bx);
+        const auto hi = amrex::ubound(bx);
+        
+        const Array4<Real>& sum_lap = sum_laplacian.array(mfi);
+
+        for (auto k = lo.z; k <= hi.z; ++k) {
+        for (auto j = lo.y; j <= hi.y; ++j) {
+        for (auto i = lo.x; i <= hi.x; ++i) {
+
+            // how to fill in the bins?
+            
+                
+        }
+        }
+        }
+
+    } // end MFIter
+
+    ParallelDescriptor::ReduceRealSum(bins.dataPtr(),nbins);
+
+    // print out contents of bins to the screen
+    for (int i=0; i<nbins; ++i) {
+        Print() << i << " " << bins[i] << std::endl;
     }
 
+    
+                 
 }
