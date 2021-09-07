ecad774149e63350928b62525137dda2fd3ed7db
Andy Nonaka <AJNonaka@lbl.gov>
2021-09-07 15:23:04 -0700

    modify inputs files and source code for C++ namespace

--- exec/chemistry_testing/main.cpp
+++ exec/chemistry_testing/main.cpp
@@ -27,9 +27,6 @@ void main_main(const char* argv)
 
     std::string inputs_file = argv;
     
-    // read in parameters from inputs file into F90 modules
-    // we use "+1" because of amrex_string_c_to_f expects a null char termination
-    read_common_namelist(inputs_file.c_str(),inputs_file.size()+1);
 
     // copy contents of F90 modules to C++ namespaces
     InitializeCommonNamespace();
