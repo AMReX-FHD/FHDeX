c4ae54958ffa5f0d1caa1f28a271bb01d70b3b56
Changho Kim <changhokim@gigan.lbl.gov>
2022-04-10 18:30:20 -0700

    src_MFsurfchem: surfcov0 added

--- src_MFsurfchem/MFsurfchem_functions.cpp
+++ src_MFsurfchem/MFsurfchem_functions.cpp
@@ -2,6 +2,7 @@
 #include "AMReX_ParmParse.H"
 
 AMREX_GPU_MANAGED int MFsurfchem::ads_spec;
+AMREX_GPU_MANAGED amrex::Real MFsurfchem::surfcov0;
 
 AMREX_GPU_MANAGED amrex::Real MFsurfchem::ads_rate_const;
 AMREX_GPU_MANAGED amrex::Real MFsurfchem::des_rate;
@@ -17,6 +18,10 @@ void InitializeMFSurfchemNamespace()
 
     // if ads_spec is set to -1 or not defined in the inputs file, quit the routine
     if (ads_spec==-1) return;
+
+    surfcov0 = 0.;
+    // get initial surface coverage
+    pp.query("surfcov0",surfcov0);
 
     ads_rate_const = 0.;
     // get adsorption rate const 
