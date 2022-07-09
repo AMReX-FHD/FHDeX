afe4f030dec83e69b2b333b2b690a5b46da846bd
Changho Kim <ckim103@ucmerced.edu>
2022-07-09 10:03:14 -0700

    exec/compressible_stag_mui/test_COAr_eq_slurm: updated

--- exec/compressible_stag_mui/test_COAr_eq_slurm/job_script.sh
+++ exec/compressible_stag_mui/test_COAr_eq_slurm/job_script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -l
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=56
 #SBATCH --partition test
 #SBATCH --time=0-00:30:00
 
 # COMMANDS HERE
 
-srun -n 5 -l --multi-prog mpmd.conf
+srun -n 56 -l --multi-prog mpmd.conf
