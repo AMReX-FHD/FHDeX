bc8a2eacd7aa99ad7241a360e867b4b696d219e8
Changho Kim <ckim103@ucmerced.edu>
2022-07-31 16:43:50 -0700

    exec/compressible_stag_mui/test_COAr_depletion: depletion setup for compressible_stag_mui

---
+++ exec/compressible_stag_mui/test_COAr_depletion/diff.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR1=./
+DIR2=../test_COAr_depletion
+FILES="run.sh inputs_fhd_stag in.kmc zavg.sh ads.sh mass_cons.py mass_cons.sh theta_eq.py theta_eq.sh clean.sh diff.sh"
+
+for file in $FILES
+do
+  echo "** $file"
+  diff $DIR1/$file $DIR2/$file
+done
