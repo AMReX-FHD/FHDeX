ed8baafd0a11a0c568bd2aaaede5f5bb7740ec62
John Bell <jbb@gigan.lbl.gov>
2023-06-06 10:10:12 -0700

    minor tweaks

--- exec/multispec/Init.cpp
+++ exec/multispec/Init.cpp
@@ -77,6 +77,50 @@ void InitRhoUmac(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                 }
                 
             });
+
+	} else if (prob_type == 5) {
+
+            /*
+              bubble with radius = 1/4 of domain in x
+              c=c_init_1(:) inside, c=c_init_2(:) outside
+              can be discontinous or smooth depending on smoothing_width
+            */
+            
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            {
+                Real x,y,z;
+                AMREX_D_TERM(x = prob_lo[0] + (i+0.5)*dx[0] - center[0];,
+                             y = prob_lo[1] + (j+0.5)*dx[1] - center[1];,
+                             z = prob_lo[2] + (k+0.5)*dx[2] - center[2];);
+
+
+                if (smoothing_width == 0.) {
+                        for (int n=0; n<nspecies; ++n) {
+                            c(i,j,k,n) = 0.01;
+                        }
+
+                    // discontinuous interface
+                    if (x< 0. && y < 0.) {
+		           c(i,j,k,0) = 0.97;
+                    } else if ( x > 0. && y < 0.){
+                           c(i,j,k,1) = 0.97;
+                    } else if ( x < 0. && y > 0.){
+                           c(i,j,k,2) = 0.97;
+		    } else {
+                           c(i,j,k,3) = 0.97;
+	            }
+
+		           
+//                    
+//                } else {
+//                    // smooth interface
+//                    for (int n=0; n<nspecies; ++n) {
+//                        c(i,j,k,n) = c_init_1[n] + (c_init_2[n]-c_init_1[n]) *
+//                            0.5*(1. + std::tanh((r-rad)/(smoothing_width*dx[0])));
+//                    }
+                }
+                
+            });
         } else if (prob_type == 6) {
 
             /*
@@ -227,6 +271,8 @@ void InitRhoUmac(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                }
             });
 
+#if (AMREX_SPACEDIM == 3)
+
 
             const Array4<Real> & wmac = (umac[2]).array(mfi);
             Box bx_wmac = mfi.tilebox(nodal_flag_z);
@@ -266,6 +312,8 @@ void InitRhoUmac(std::array< MultiFab, AMREX_SPACEDIM >& umac,
                    wmac(i,j,k) = wmac(i,j,k)/(factor*factor);
 
             });
+
+#endif
 
         } else if (prob_type == 9) {
 
