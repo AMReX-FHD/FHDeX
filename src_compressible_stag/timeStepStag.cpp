e6c6e8018e3449dbace5c07c766bcafad5094c30
isriva <ishan66@gmail.com>
2023-08-20 21:43:16 -0700

    cleanup comments

--- src_compressible_stag/timeStepStag.cpp
+++ src_compressible_stag/timeStepStag.cpp
@@ -167,10 +167,6 @@ void RK3stepStag(MultiFab& cu,
 
     // fill random numbers (can skip density component 0)
     if (do_1D) { // 1D need only for x- face 
-        //for(int i=1;i<nvars;i++) {
-        //    MultiFabFillRandom(stochface_A[0], i, 1.0, geom);
-        //    MultiFabFillRandom(stochface_B[0], i, 1.0, geom);
-        //}
         FillRandomNormal(stochface_A[0], 0, nvars, 0.0, 1.0);
         FillRandomNormal(stochface_B[0], 0, nvars, 0.0, 1.0);
         stochface_A[0].OverrideSync(geom.periodicity());
@@ -179,12 +175,6 @@ void RK3stepStag(MultiFab& cu,
         stochface_B[0].FillBoundary(geom.periodicity());
     }
     else if (do_2D) { // 2D need only for x- and y- faces
-        //for(int i=1;i<nvars;i++) {
-        //    MultiFabFillRandom(stochface_A[0], i, 1.0, geom);
-        //    MultiFabFillRandom(stochface_B[0], i, 1.0, geom);
-        //    MultiFabFillRandom(stochface_A[1], i, 1.0, geom);
-        //    MultiFabFillRandom(stochface_B[1], i, 1.0, geom);
-        //}
         FillRandomNormal(stochface_A[0], 0, nvars, 0.0, 1.0);
         FillRandomNormal(stochface_B[0], 0, nvars, 0.0, 1.0);
         FillRandomNormal(stochface_A[1], 0, nvars, 0.0, 1.0);
@@ -200,10 +190,6 @@ void RK3stepStag(MultiFab& cu,
     }
     else { // 3D
         for(int d=0;d<AMREX_SPACEDIM;d++) {
-            //for(int i=1;i<nvars;i++) {
-            //    MultiFabFillRandom(stochface_A[d], i, 1.0, geom);
-            //    MultiFabFillRandom(stochface_B[d], i, 1.0, geom);
-            //}
             FillRandomNormal(stochface_A[d], 0, nvars, 0.0, 1.0);
             FillRandomNormal(stochface_B[d], 0, nvars, 0.0, 1.0);
             stochface_A[d].OverrideSync(geom.periodicity());
@@ -216,10 +202,6 @@ void RK3stepStag(MultiFab& cu,
     if (do_1D) { // 1D no transverse shear fluxes
     }
     else if (do_2D) { // 2D only xy-shear
-        //MultiFabFillRandom(stochedge_x_A[0], 0, 1.0, geom);
-        //MultiFabFillRandom(stochedge_x_B[0], 0, 1.0, geom);
-        //MultiFabFillRandom(stochedge_y_A[0], 0, 1.0, geom);
-        //MultiFabFillRandom(stochedge_y_B[0], 0, 1.0, geom);
         FillRandomNormal(stochedge_x_A[0], 0, 1, 0.0, 1.0);
         FillRandomNormal(stochedge_x_B[0], 0, 1, 0.0, 1.0);
         FillRandomNormal(stochedge_y_A[0], 0, 1, 0.0, 1.0);
@@ -235,12 +217,6 @@ void RK3stepStag(MultiFab& cu,
     }
     else { // 3D
         for (int i=0; i<2; i++) {
-            //MultiFabFillRandom(stochedge_x_A[i], 0, 1.0, geom);
-            //MultiFabFillRandom(stochedge_x_B[i], 0, 1.0, geom);
-            //MultiFabFillRandom(stochedge_y_A[i], 0, 1.0, geom);
-            //MultiFabFillRandom(stochedge_y_B[i], 0, 1.0, geom);
-            //MultiFabFillRandom(stochedge_z_A[i], 0, 1.0, geom);
-            //MultiFabFillRandom(stochedge_z_B[i], 0, 1.0, geom);
             FillRandomNormal(stochedge_x_A[i], 0, 1, 0.0, 1.0);
             FillRandomNormal(stochedge_x_B[i], 0, 1, 0.0, 1.0);
             FillRandomNormal(stochedge_y_A[i], 0, 1, 0.0, 1.0);
@@ -263,8 +239,6 @@ void RK3stepStag(MultiFab& cu,
     }
 
     if (do_1D) { // 1D no v_x and w_z stochastic terms
-        //MultiFabFillRandom(stochcen_A[0], 0, 1.0, geom, 1);
-        //MultiFabFillRandom(stochcen_B[0], 0, 1.0, geom, 1);
         FillRandomNormal(stochcen_A[0], 0, 1, 0.0, 1.0);
         FillRandomNormal(stochcen_B[0], 0, 1, 0.0, 1.0);
         stochcen_A[0].OverrideSync(geom.periodicity());
@@ -273,10 +247,6 @@ void RK3stepStag(MultiFab& cu,
         stochcen_B[0].FillBoundary(geom.periodicity());
     }
     else if (do_2D) { // 2D simulation no w_z stochastic term
-        //MultiFabFillRandom(stochcen_A[0], 0, 2.0, geom, 1);
-        //MultiFabFillRandom(stochcen_B[0], 0, 2.0, geom, 1);
-        //MultiFabFillRandom(stochcen_A[1], 0, 2.0, geom, 1);
-        //MultiFabFillRandom(stochcen_B[1], 0, 2.0, geom, 1);
         FillRandomNormal(stochcen_A[0], 0, 1, 0.0, sqrt(2.0));
         FillRandomNormal(stochcen_B[0], 0, 1, 0.0, sqrt(2.0));
         FillRandomNormal(stochcen_A[1], 0, 1, 0.0, sqrt(2.0));
@@ -292,8 +262,6 @@ void RK3stepStag(MultiFab& cu,
     }
     else { // 3D
         for (int i=0; i<3; i++) {
-            //MultiFabFillRandom(stochcen_A[i], 0, 2.0, geom, 1);
-            //MultiFabFillRandom(stochcen_B[i], 0, 2.0, geom, 1);
             FillRandomNormal(stochcen_A[i], 0, 1, 0.0, sqrt(2.0));
             FillRandomNormal(stochcen_B[i], 0, 1, 0.0, sqrt(2.0));
             stochcen_A[i].OverrideSync(geom.periodicity());
