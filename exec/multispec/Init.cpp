2bd05e1598b893429680911166d0533b4db4c298
jbb <jbbell@lbl.gov>
2024-02-15 14:28:55 -0800

    tweaked input

--- exec/multispec/Init.cpp
+++ exec/multispec/Init.cpp
@@ -59,8 +59,9 @@ void InitRhoUmac(std::array< MultiFab, AMREX_SPACEDIM >& umac,
 //                             y = prob_lo[1] + (j+0.5)*dx[1] - center[1];,
 //                             z = prob_lo[2] + (k+0.5)*dx[2] - center[2];);
                 AMREX_D_TERM(x = prob_lo[0] + (i+0.5)*dx[0] - center[0];,
-                             y = prob_lo[1] + (j+0.5)*dx[1] - rad*std::cos(alpha) ;,
+                             y = prob_lo[1] + (j+0.5)*dx[1] - center[1] ;,
                              z = prob_lo[2] + (k+0.5)*dx[2] - center[2];);
+                             //y = prob_lo[1] + (j+0.5)*dx[1] - rad*std::cos(alpha) ;,
 
                 Real r = (AMREX_SPACEDIM == 1) ? std::sqrt(x*x+y*y) : std::sqrt(x*x+y*y+z*z);
 
