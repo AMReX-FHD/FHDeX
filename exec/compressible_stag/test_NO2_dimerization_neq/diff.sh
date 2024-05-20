f331272cedf2adff742eb99cff94f695945a8a54
Changho Kim <ckim103@ucmerced.edu>
2024-05-20 06:02:29 -0700

    test_NO2_dimerization_neq: simple noneq simulation example directory updated

--- exec/compressible_stag/test_NO2_dimerization_neq/diff.sh
+++ exec/compressible_stag/test_NO2_dimerization_neq/diff.sh
@@ -2,7 +2,7 @@
 
 DIR1=./
 DIR2=../test_NO2_dimerization_neq
-FILES="submit_job.sh job_script.sh inputs_NO2_dimerization_neq params_NO2_dimerization_rate_const.py avg.sh clean.sh diff.sh README"
+FILES="submit_job.sh job_script.sh inputs_NO2_dimerization_neq0 inputs_NO2_dimerization_neq1 avg.sh clean.sh diff.sh README"
 
 for file in $FILES
 do
