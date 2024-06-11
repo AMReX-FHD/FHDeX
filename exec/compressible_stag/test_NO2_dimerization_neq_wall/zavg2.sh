09284b94c3d24cfadabb6bf9abb64a6327f50532
Changho Kim <ckim103@ucmerced.edu>
2024-06-11 03:52:59 -0700

    test_NO2_dimerization_neq_wall: new steady-state example setup

---
+++ exec/compressible_stag/test_NO2_dimerization_neq_wall/zavg2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+exec=../../../../FBoxLib/Tools/Postprocessing/F_Src/faverage.Linux.gfortran.exe
+
+if [ "$#" -eq 0 ]
+then 
+    pltfiles=(`ls -d plt0*`)
+    pltfile=${pltfiles[-1]}
+else
+    pltfile=$1
+fi
+
+$exec -p $pltfile -o res.zavg -v 8 tMean rhoMean rhoYkMean_0 rhoYkMean_1 TVarDirect rhoVar rhoYkVar_0 rhoYkVar_1
+
+gnuplot zavg2.plt
+
+mv res.zavg res.zavg2_${pltfile}
+mv zavg.png zavg2_${pltfile}.png
+eog zavg2_${pltfile}.png
