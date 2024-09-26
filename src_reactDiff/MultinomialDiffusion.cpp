0252d1039109ae7ec73b502751e41d4d76b7298b
Andy Nonaka <AJNonaka@lbl.gov>
2024-09-26 08:49:03 -0700

    brute force binomial/multinomial (slow but seems to work)

--- src_reactDiff/MultinomialDiffusion.cpp
+++ src_reactDiff/MultinomialDiffusion.cpp
@@ -40,24 +40,24 @@ void MultinomialDiffusion(MultiFab& n_old,
                      const Array4<const Real> & diffy = diff_coef_face[1].array(mfi);,
                      const Array4<const Real> & diffz = diff_coef_face[2].array(mfi););
 
-        amrex::ParallelFor(bx, nspecies, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
+        amrex::ParallelForRNG(bx, nspecies, [=] AMREX_GPU_DEVICE (int i, int j, int k, int n, amrex::RandomEngine const& engine) noexcept
         {
 
             GpuArray<Real,2*AMREX_SPACEDIM> p;
             GpuArray<Real,2*AMREX_SPACEDIM> fluxes;
 
-            p[0] = diffx(i,j,k,n)*dt/(dx[0]*dx[0]);
-            p[1] = diffx(i+1,j,k,n)*dt/(dx[0]*dx[0]);
-            p[2] = diffy(i,j,k,n)*dt/(dx[1]*dx[1]);
-            p[3] = diffy(i,j+1,k,n)*dt/(dx[1]*dx[1]);
+            p[0] = diffx(i  ,j  ,k,n)*dt/(dx[0]*dx[0]);
+            p[1] = diffx(i+1,j  ,k,n)*dt/(dx[0]*dx[0]);
+            p[2] = diffy(i  ,j  ,k,n)*dt/(dx[1]*dx[1]);
+            p[3] = diffy(i  ,j+1,k,n)*dt/(dx[1]*dx[1]);
 #if (AMREX_SPACEDIM == 3)
-            p[4] = diffz(i,j,k,n)*dt/(dx[2]*dx[2]);
-            p[5] = diffz(i,j,k+1,n)*dt/(dx[2]*dx[2]);
+            p[4] = diffz(i  ,j  ,k  ,n)*dt/(dx[2]*dx[2]);
+            p[5] = diffz(i  ,j  ,k+1,n)*dt/(dx[2]*dx[2]);
 #endif
 
             int N = std::max(0., std::round(n_arr(i,j,k,n)*dv));
             
-            multinomial_rng(fluxes, N, p);
+            multinomial_rng(fluxes, N, p, engine);
 
             // lo-x face
             update(i  ,j,k,n) -= fluxes[0];
@@ -108,34 +108,47 @@ void MultinomialDiffusion(MultiFab& n_old,
 
 AMREX_GPU_HOST_DEVICE void multinomial_rng(GpuArray<Real,2*AMREX_SPACEDIM>& samples,
                                            const int& N,
-                                           GpuArray<Real,2*AMREX_SPACEDIM>& p)
+                                           GpuArray<Real,2*AMREX_SPACEDIM>& p,
+                                           const amrex::RandomEngine& engine)
 {
-#if (AMREX_USE_CUDA)
-    Abort("MultinomialRNG not supported for CUDA");
-#else
-
     Real sum_p = 0;
     for (int sample=0; sample<2*AMREX_SPACEDIM; ++sample) {
         sum_p += p[sample];
     }
     if (sum_p > 1.) {
-        Print() << "sum_p = " << sum_p << std::endl;
+        printf("sum_p = %f",sum_p);
         Abort("multinomial_rng: probabilities must sum to 1 or less");
     }
 
+#if 0
+    // not sure why std:: binomial_distribition gives grid artifacts
     std::default_random_engine generator;
     generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
+#endif
 
     sum_p = 0.;
     int sum_n = 0;
     
     for (int sample=0; sample<2*AMREX_SPACEDIM; ++sample) {
+#if 0
+        // not sure why std:: binomial_distribition gives grid artifacts
+#if (AMREX_USE_CUDA)
+    Abort("std::binomial_distribution not supported for CUDA");
+#endif
         std::binomial_distribution<int> distribution(N-sum_n, p[sample]/(1.-sum_p));
         samples[sample] = distribution(generator);
+#else
+        // brute force binomial distribution
+        int success = 0;
+        Real prob = p[sample]/(1.-sum_p);
+        for (int n=0; n<N-sum_n; ++n) {
+            Real x = amrex::Random(engine);
+            if (x <= prob) success++;
+        }
+        samples[sample] = success;
+#endif
 
         sum_n += samples[sample];
         sum_p += p[sample];
     }
-
-#endif
 }
