ce96feb668f1d597f0d504655b0fd9666258fb00
Ishan Srivastava <isriva@login03.frontier.olcf.ornl.gov>
2024-11-01 13:25:38 -0400

    modified buld script for Frontier

--- exec/compressible_stag/build_frontier_101324.sh
+++ exec/compressible_stag/build_frontier_101324.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/bash
 
 ## load necessary modules 
-module load craype-accel-amd-gfx90a
-module load rocm
-module load cray-mpich
-module load cce  # must be loaded after rocm
-module load cray-fftw
+module load rocm/5.7.1
 
 # GPU-aware MPI
 export MPICH_GPU_SUPPORT_ENABLED=1
@@ -21,4 +17,4 @@ export CFLAGS="-I${ROCM_PATH}/include"
 export CXXFLAGS="-I${ROCM_PATH}/include -Wno-pass-failed"
 export LDFLAGS="-L${ROCM_PATH}/lib -lamdhip64 ${PE_MPICH_GTL_DIR_amd_gfx90a} -lmpi_gtl_hsa"
 
-make -j8 USE_HIP=TRUE DO_TURB=TRUE MAX_SPEC=2 USE_HEFFTE_ROCFFT=FALSE USE_ASSERTION=TRUE
+make -j8 USE_HIP=TRUE DO_TURB=TRUE MAX_SPEC=2 USE_FFT=TRUE USE_ASSERTION=TRUE
