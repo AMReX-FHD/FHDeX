94dd07cc60b9e47871073f8e92a2b28f73d9340d
Matthew Blomquist <mblomquist@rclogin01.cluster>
2024-11-16 10:53:02 -0800

    Update and add binning option.

--- exec/reactDiff/WriteSFToASCII/WriteSFToASCII.cpp
+++ exec/reactDiff/WriteSFToASCII/WriteSFToASCII.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 #include <AMReX_ParmParse.H>
 #include <AMReX_PlotFileUtil.H>
@@ -18,18 +19,27 @@ int main (int argc, char* argv[]) {
 
     // Parse command-line arguments
     std::string plt_file; 
+    std::string out_name;
     int n_bins; 
 
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-i" && i + 1 < argc) {
-            plt_file= argv[++i]; 
+            plt_file = argv[++i]; 
         } 
 
         if (arg == "-b" && i + 1 < argc) {
             n_bins = std::stoi(argv[++i]);
-        } 
+        }
+        
+        if (arg == "-o" && i + 1 < argc) {
+            out_name = argv[++i];
+        }
     }
+
+    if (out_name == "") {
+        out_name += plt_file + "_sf_raw.csv";
+    } 
 
     // Append Level_0/Cell for mf read
     plt_file += "/Level_0/Cell";
@@ -37,10 +47,10 @@ int main (int argc, char* argv[]) {
      
     // read in plotfile to MultiFab
     VisMF::Read(mf, plt_file);
-    //VisMF::Read(mf, "plt_SF_mag000200000/Level_0/Cell");
 
     std::vector<std::pair<double, double>> sf_flat;
-        
+       
+    // TODO: need to figure out how to know the center from the level_0 data 
     const int center_x = 32;
     const int center_y = 32;
 
@@ -51,7 +61,6 @@ int main (int argc, char* argv[]) {
         const auto hi = amrex::ubound(bx);
         const Array4<Real>& mfdata = mf.array(mfi);
 
-        // assume everything is 2d for now
         for (auto n=0; n<1; ++n) {
             for (auto k = lo.z; k <= hi.z; ++k) {
                 for (auto j = lo.y; j <= hi.y; ++j) {
@@ -61,7 +70,6 @@ int main (int argc, char* argv[]) {
                         auto dk = sqrt(dk_x*dk_x + dk_y*dk_y);
 
                         sf_flat.push_back(std::make_pair(dk, mfdata(i,j,k,n)));
-                        //std::cout << dk << ", " << mfdata(i,j,0,n) << "\n";
                     } 
                 } 
             } 
@@ -69,11 +77,75 @@ int main (int argc, char* argv[]) {
 
     } // end MFIter
 
-    // sort and bin data
+    // sort 
     std::sort(sf_flat.begin(), sf_flat.end(), [](const auto& a, const auto& b) {
         return a.first < b.first;
     });
 
+    // if no bins specified, print all the data out
+    if (n_bins == 0)
+    {
+        std::ofstream out_file(out_name);
+
+        if (!out_file.is_open()) {
+            std::cerr << "Error: Could not open file " << out_name << " for writing." << std::endl;
+            return 1; 
+        }
+
+        out_file << "dk, sf\n";
+
+        for (const auto& data : sf_flat)
+        {
+            out_file << data.first << ", " << data.second << "\n";
+        }
+    }
+    else 
+    {
+        // simple way to make sure there are no empty bins
+        if (n_bins > 2*center_x || n_bins > 2*center_y) {
+            n_bins = 2*center_x;
+            std::cout << "The number of bins exceeds the number of cells in one direction!\n";
+            std::cout << "Setting n_bins = " << n_bins << "\n";
+        }
+
+        // bin ranges
+        auto max_dk = std::max_element(sf_flat.begin(), sf_flat.end(),
+                [](const auto& a, const auto& b) {
+                return a.first < b.first;
+                });
+
+        auto bin_r = max_dk->first / double(n_bins);
+
+        // bins<bin_index<sum_values, n_values>>
+        std::map<int, std::pair<double, int>> bins;
+        
+        for (const auto& data : sf_flat)
+        {
+            size_t bin_i = static_cast<size_t>(std::floor(data.first / bin_r));
+            bins[bin_i].first += data.second;
+            bins[bin_i].second += 1; 
+        }
+
+        std::ofstream out_file(out_name);
+
+        if (!out_file.is_open()) {
+            std::cerr << "Error: Could not open file " << out_name << " for writing." << std::endl;
+            return 1; 
+        }
+
+        out_file << "dk, sf\n";
+
+        for (const auto& [bin_i, bin_d] : bins)
+        {
+            auto bin_center = bin_i * bin_r + 0.5*bin_r;
+            auto bin_average = double(bin_d.first / bin_d.second);
+
+            out_file << bin_center << ", " << bin_average << "\n";
+        }
+
+    }
+    
+    
     std::cout << "... and we are done!\n\n";
 
 }
