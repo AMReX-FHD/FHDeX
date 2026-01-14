0a9fecdd6448a47e34cc5a203ae4f546030fe930
Andy Nonaka <AJNonaka@lbl.gov>
2026-01-14 14:10:09 -0800

    add beta back into initial momentum sample

--- exec/FPU_chain/FPU.cpp
+++ exec/FPU_chain/FPU.cpp
@@ -67,7 +67,7 @@ void init(MultiFab& state,
             for (auto i = lo.x; i <= hi.x; ++i) {
                 for (int kkk=0; kkk<1000; kkk++) {
                     Real grad_U = (a*r + b*r*r + c*r*r*r + pressure);
-                    r += -step_size * grad_U + sqrt_2dt * amrex::RandomNormal(0.,1.);
+                    r += -step_size * beta * grad_U + sqrt_2dt * amrex::RandomNormal(0.,1.);
                 }
                 state_fab(i,j,k,0) = r;
             }
